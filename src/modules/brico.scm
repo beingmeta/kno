684dabebdafe0a9bdcf8123c4315ca2f5b8bae0c
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2006-06-16 22:28:49 +0000

    Added extensible concept-frequency to the brico.scm module

--- src/modules/brico.scm
+++ src/modules/brico.scm
@@ -12,12 +12,8 @@
    members members* member-of member-of*
    ingredients ingredients* ingredient-of ingredient-of*
    isa inverse =is= disjoint implies implies*
-   get-gloss
-   get-short-gloss
-   get-expstring
-   gloss
-   basic-concept-frequency
-   index-name})
+   get-gloss get-short-gloss get-expstring index-name gloss
+   basic-concept-frequency concept-frequency use-corpus-frequency})
 
 (define bricosource #f)
 (define brico-pool {})
@@ -153,10 +149,65 @@
 
 ;;; Getting concept frequency information
 
-(define (basic-concept-frequency concept (term #f) (language #f))
-  (let ((sum 1) (language (or language english)))
+(define (basic-concept-frequency concept language term)
+  (let ((sum 0) (language (or language english)))
     (do-choices (wf (if term (?? 'of concept 'word term 'language language)
 			(?? 'of concept)))
       (set! sum (+ sum (try (get wf 'freq) 0))))
+    ;; If there is no frequency data, use the native language occurence
+    ;;  as a single instance.
+    (if (zero? sum) 
+	(if (test concept language term) 1 0)
+	sum)))
+
+;; This is a list of functions to get concept/term frequency information.
+;;  Each item is a car of a name (which is an arbitrary value) and a function
+;;  of three arguments (concept, language, term) that returns a count of
+;;  either co-occurences of term in language with concept or the absolute
+;;  frequency of the concept if language and term are false.
+(define freqfns (list (cons 'basic basic-concept-frequency)))
+
+(define (concept-frequency concept (language #f) (term #f))
+  (let ((sum 0))
+    (dolist (method freqfns)
+      (let ((freq ((cdr method) concept language term)))
+	(when (and (exists? freq) (number? freq))
+	  (set! sum (+ sum freq)))))
     sum))
+
+;;; Configuring freqfns
+
+(define (edit-freqfns name fn scan)
+  (if (null? scan) (fail)
+      (if (and name (equal? (car (car scan)) name))
+	  (cons (cons name fn) (cdr scan))
+	  (cons (car scan) (edit-freqfns name fn (cdr scan))))))
+
+(define (freqfns-config var (val))
+  (if (bound? val)
+      (if (pair? val)
+	  (set! freqfns
+		(try (edit-freqfns (car val) (cdr val) freqfns)
+		     (cons val freqfns)))
+	  (set! freqfns (cons (cons #f val) freqfns)))
+      disambigfns))
+(config-def! 'freqfns freqfns-config)
+
+;; This returns a method for using indexed corpora to provided
+;; absolute or contingent concept frequency information.
+(define use-corpus-frequency
+  (ambda (corpus conceptslot wordslot (langslot #f))
+    (cons
+     (vector (qc corpus) conceptslot wordslot)
+     (lambda (concept language word)
+       (if (not language)
+	   (choice-size (find-frames corpus conceptslot concept))
+	   (if langslot
+	       (choice-size (find-frames corpus
+			      conceptslot concept
+			      wordslot word
+			      langslot language))
+	       (choice-size (find-frames corpus
+			      conceptslot concept
+			      wordslot word))))))))
 
