88fb30cb6e8f85ba8bc188aa79cdaaa25e5d85f3
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2006-07-20 17:42:02 +0000

    Added an xbrico module

---
+++ src/modules/xbrico.scm
@@ -0,0 +1,36 @@
+(in-module 'brico)
+
+(module-export!
+ '{xbrico-pool
+   xbrico-index
+   names-pool
+   places-pool})
+
+(define xbricosource #f)
+
+(define xbrico-pool #f)
+(define names-pool #f)
+(define places-pool #f)
+
+(define xbrico-index #f)
+(define names-index #f)
+(define places-index #f)
+
+(define xbricosource-config
+  (slambda (var (val unbound))
+    (cond ((eq? val 'unbound) xbricosource)
+	  ((equal? val xbricosource)
+	   xbricosource)
+	  ((exists? xbrico-pool)
+	   (message "Redundant configuration of XBRICOSOURCE "
+		    "which is already provided from " xbricosource)
+	   #f)
+	  (else
+	   (set! xbricosource val)
+	   (use-pool val)
+	   (set! xbrico-index (use-index val))
+	   (set! xbrico-pool (name->pool "xbrico.beingmeta.com"))
+	   (set! names-pool (name->pool "namedb.beingmeta.com"))
+	   (set! places-pool (name->pool "placedb.beingmeta.com"))))))
+(config-def! 'xbricosource xbricosource-config)
+
