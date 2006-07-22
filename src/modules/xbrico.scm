839335922eafead97cdd4e3696b7741c198731e1
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2006-07-22 19:01:02 +0000

    Various chnages to xbrico module

--- src/modules/xbrico.scm
+++ src/modules/xbrico.scm
@@ -1,4 +1,4 @@
-(in-module 'brico)
+(in-module 'xbrico)
 
 (module-export!
  '{xbrico-pool
@@ -26,8 +26,9 @@
 		    "which is already provided from " xbricosource)
 	   #f)
 	  (else
-	   (set! xbricosource val)
-	   (use-pool val)
+	   (unless (exists? (name->pool "xbrico.beingmeta.com"))
+	     (set! xbricosource val)
+	     (use-pool val))
 	   (set! xbrico-index (use-index val))
 	   (set! xbrico-pool (name->pool "xbrico.beingmeta.com"))
 	   (set! names-pool (name->pool "namedb.beingmeta.com"))
