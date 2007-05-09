0c76e4715b7749c3bb7510295e92085c7689ad52
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2007-05-09 17:26:46 +0000

    Added usedb to get package pools and indices together

---
+++ src/modules/usedb.scm
@@ -0,0 +1,21 @@
+(in-module 'usedb)
+
+(module-export! 'usedb)
+
+(define (usedb name)
+  (let ((dbname (cond ((file-exists? name) name)
+		      ((file-exists? (stringout name ".db"))
+		       (stringout name ".db"))
+		      (else (error "No such database " name)))))
+    (let ((dbdata (file->dtype dbname)))
+      (do-choices (pool (get dbdata 'pools))
+	(cond ((not (string? pool)))
+	      ((position #\@ pool) (use-pool pool))
+	      ((has-prefix pool "/") (use-pool pool))
+	      (else (use-pool (get-component pool dbname)))))
+      (for-choices (index (get dbdata 'indices))
+	(cond ((not (string? index)))
+	      ((position #\@ index) (use-index index))
+	      ((has-prefix pool "/") (use-index index))
+	      (else (use-index (get-component index dbname))))))))
+
