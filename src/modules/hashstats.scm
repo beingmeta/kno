1263e2a6ba3e22671d2720c4475ac4f01d6b31c6
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2007-05-09 20:21:40 +0000

    Hashstat fixes

--- src/modules/hashstats.scm
+++ src/modules/hashstats.scm
@@ -24,7 +24,8 @@
 	(hashfn (elt report 3))
 	(size (elt report 4))
 	(sum 0) (squaresum 0) (n 0) (highest 0)
-	(max 0) (maxkey #f) (direct 0) (empty 0))
+	(max 0) (maxkey #f) (direct 0) (empty 0)
+	(missing 0))
     (do-choices (key (getkeys bucketsizes))
       (when (> key highest) (set! highest key))
       (let ((nkeys (get bucketsizes key)))
@@ -35,10 +36,12 @@
 	  (set! max nkeys) (set! maxkey key))
 	(cond ((zero? nkeys) (set! empty (+ empty 1)))
 	      ((= nkeys 1) (set! direct (+ direct 1))))))
+    (set! missing (- (or size highest)
+		     (choice-size (getkeys bucketsizes))))
     (printout "Storing " n-keys " keys using " hashfn
 	      (if size (printout " across " size " buckets")) ".\n"
 	      "Averaging " (/~ sum n) " keys per bucket over "
-	      n " buckets, " empty " empty, " direct " direct, "
+	      n " buckets, " (+ empty missing) " empty, " direct " direct, "
 	      (- n direct empty) " collisions\n"
 	      "The highest bucket used was " highest
 	      " and the most loaded bucket, " maxkey ", had "
