74aba5c01e79f71c414472965b21c383ca4dde48
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2007-07-23 12:34:06 +0000

    More fixes/improvements on the new audit methods

--- src/modules/brico.scm
+++ src/modules/brico.scm
@@ -187,7 +187,7 @@
 		    'PARTOF)
 		   ((%test f 'hypernym) 'GENLS)
 		   ((%test f genls) 'GENLS)
-		   ((%test f ISA) 'ISA)
+		   ((%test f implies) 'ISA)
 		   ((%test f partof) 'PARTOF)
 		   (else 'TOP))
 	    ,@(map get-norm
@@ -196,7 +196,7 @@
 				(%get f partof))
 			 (%get f 'hypernym)
 			 (%get f genls)
-			 (%get f isa)
+			 (%get f implies)
 			 (%get f partof))))))))
 (define (make%id! f (lang default-language))
   (store! f '%id (make%id f lang)))
@@ -506,7 +506,7 @@
   (index-frame* index concept ingredientof* ingredientof ingredients*))
 
 (define (index-implies index concept slotid)
-  (index-frame index concept slotid (get (get concept slotid) @?implies*)))
+  (index-frame index concept slotid (get (get concept slotid) implies*)))
 
 (define (indexer index concept (slotids) (values))
   (if (bound? slotids)
@@ -564,6 +564,49 @@
    indexer-lattice-prefetch
    indexer-slotid-prefetch
    prefetch-expansions})
+
+;;; Auditing
+
+(define auditor #f)
+
+(define auditor-config
+  (slambda (var (val))
+    (if (bound? val)
+	(if (and (string? val) (position #\@ val))
+	    (set! auditor val)
+	    (error TYPE "Invalid auditor, must be email address: " val))
+	auditor)))
+(config-def! 'auditor auditor-config)
+
+(define audit+!
+  (ambda (frame slotid value)
+    (if (not auditor) (error NOCONFIG "No auditor configured"))
+    (do-choices (frame frame)
+      (do-choices (slotid slotid)
+	(assert! frame slotid value)
+	(add! frame '%adds
+	      (list slotid (qc value)
+		    auditor (timestamp) (config 'sessionid)))))))
+(define audit-!
+  (ambda (frame slotid value)
+    (if (not auditor) (error NOCONFIG "No auditor configured"))
+    (do-choices (frame frame)
+      (do-choices (slotid slotid)
+	(retract! frame slotid value)
+	(add! frame '%drops
+	      (list slotid (qc value)
+		    auditor (timestamp) (config 'sessionid)))))))
+
+(define audit!
+  (ambda (frame slotid arg3 (arg4))
+    (if (not auditor) (error NOCONFIG "No auditor configured"))
+    (if (and (eq? arg3 'not) (bound? arg4))
+	(audit-! frame slotid arg4)
+	(if (not (bound? arg4))
+	    (audit+! frame slotid arg3)
+	    (error SYNTAX "Bad AUDIT! call")))))
+
+(module-export! '{audit+! audit-! audit!})
 
 ;;; Displaying glosses
 
