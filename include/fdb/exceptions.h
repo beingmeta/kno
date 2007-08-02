96db5286589e2e977857768fce7297608e95fb4c
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2007-08-02 03:10:50 +0000

    Updated some design notes for the new/old exception system

--- include/fdb/exceptions.h
+++ include/fdb/exceptions.h
@@ -8,10 +8,14 @@
     Use a thread var to store the current dynamic context with
      operations for pushing and popping the dynamic context
     External modules can extend the dynamic context structure.
+    The libu8 structure might also have queues for malloc'd blocks,
+     locked mutexes, and maybe some generic cleanup functions.
     
     Exception state consists of an exception, a details string, 
      a void pointer, and a saved exception stack.  Largely compatible
-     with the current model.
+     with the current model.  There might be an extra int indicating
+     some type information about the void pointer with new ints being
+     allocated as needed.
 
     Macros for exception catching and unwind protect as well as a very
      simple set of U8_EXCALLn macros which do function calls in a
