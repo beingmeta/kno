e53788c9f670f9f85b4aba2aecaca45a6db5cf8d
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2007-08-22 12:11:13 +0000

    Fixes to dtermcache

--- src/modules/brico/dtermcache.scm
+++ src/modules/brico/dtermcache.scm
@@ -1,8 +1,20 @@
 (in-module 'brico/dtermcache)
 
+;;; This provides a dterm cache which is automatically filled by
+;;; background threads.  This keeps threads from blocking on dterm
+;;; computation, which can take a while for some concepts.
+
+;;; When a dterm is requested, the cache is checked; if there is a
+;;; value there, it is returned (and a #f is converted to a {}).  If
+;;; there isn't a value in the cache, #f is stored in the table and
+;;; the request (a pair of a concept and a language) is pushed into a
+;;; fifo.  Some number of background threads (configured by
+;;; DTERMTHREADS) are reading from this FIFO, computing dterms
+;;; and storing them back in the cache.
+
 (use-module '{brico brico/dterms fifo})
 
-(module-export! '{cached-dterm lanuch-dtermdaemons})
+(module-export! '{cached-dterm launch-dtermdaemons})
 
 ;;;; The dterm cache
 
@@ -18,9 +30,14 @@
   (if (singleton? (?? language (get-norm concept language)))
       (get-norm concept language)
       (or (try (get dterm-cache (cons concept language))
-	       (begin (store! dterm-cache (cons concept language) #f)
-		      (fifo-push dterm-fifo (cons concept language))
-		      (fail))))))
+	       (cache-compute-dterm concept language))
+	  (fail))))
+
+(defslambda (cache-compute-dterm concept language)
+  (try (get dterm-cache (cons concept language))
+       (begin (store! dterm-cache (cons concept language) #f)
+	      (fifo-push dterm-fifo (cons concept language))
+	      (fail))))
 
 (define (ignore x) (fail))
 
