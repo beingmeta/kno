4862a24eb4e957eb0630425e36505a303c522373
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-03-05 21:56:31 +0000

    Added stringfmts module

---
+++ src/modules/safe/stringfmts.scm
@@ -0,0 +1,80 @@
+;;; -*- Mode: Scheme; Character-Encoding: utf-8; -*-
+
+(in-module 'stringfmts)
+
+;;; Generation of strings from various other kinds of values
+
+(define version "$Id$")
+
+(module-export! '{get% interval-string short-interval-string
+		       printnum numstring})
+
+;; Percentages
+
+(define (get% num den (prec 2))
+  (inexact->string (/ (* num 100.0) den) prec))
+
+;; Temporal intervals
+
+(define (interval-string secs (precise #t))
+  (let* ((days (inexact->exact (floor (/ secs (* 3600 24)))))
+	 (hours (inexact->exact
+		 (floor (/ (- secs (* days 3600 24))
+			   3600))))
+	 (minutes (inexact->exact
+		   (floor (/ (- secs (* days 3600 24) (* hours 3600))
+			     60))))
+	 (secs (- secs (* days 3600 24) (* hours 3600) (* minutes 60))))
+    (stringout
+	(cond ((= days 1) "one day, ")
+	      ((> days 0) (printout days " days, ")))
+      (cond ((= hours 1) "one hour, ")
+	    ((> hours 0) (printout hours " hours, ")))
+      (cond ((= minutes 1) "one minute, ")
+	    ((> minutes 0) (printout minutes " minutes, ")))
+      (cond ((= secs 1) "one second")
+	    ((< secs 1) (printout secs " seconds"))
+	    (else (printout (inexact->string secs 2) " seconds"))))))
+
+(define (short-interval-string secs (precise #t))
+  (if (< secs 180)
+      (stringout (cond ((< secs 0) secs)
+		       ((< secs 10) (inexact->string secs 3))
+		       (else (inexact->string secs 2)))
+	" secs")
+      (let* ((days (inexact->exact (floor (/ secs (* 3600 24)))))
+	     (hours (inexact->exact
+		     (floor (/ (- secs (* days 3600 24))
+			       3600))))
+	     (minutes (inexact->exact
+		       (floor (/ (- secs (* days 3600 24) (* hours 3600))
+				 60))))
+	     (seconds (- secs (* days 3600 24) (* hours 3600) (* minutes 60))))
+	(stringout
+	    (cond ((= days 1) "one day, ")
+		  ((> days 0) (printout days " days, ")))
+	  (when (> hours 0) (printout hours ":"))
+	  (printout 
+	    (if (and (> hours 0) (< minutes 10)) "0")
+	    minutes ":")
+	  (printout (if (< seconds 10) "0")
+	    (cond ((> secs 600) (inexact->exact (round seconds)))
+		  ((>= secs 10) (inexact->string seconds 2))
+		  (else seconds)))))))
+
+;;; Numbers with commas
+
+;; This should probably get this from the locale somehow
+(define printnum-sep ",")
+
+(define (printnum num (pad #f))
+  (cond ((>= num 1000)
+	 (printnum (quotient num 1000) (>= num 1000000))
+	 (printout printnum-sep (printnum (remainder num 1000) #t)))
+	((>= num 100) (printout num))
+	((>= num 10) (printout (if pad "0") num))
+	((>= num 0) (printout (if pad "00") num))
+	(else (printout "-" (printnum (- num))))))
+
+(define (numstring n) (stringout (printnum n)))
+
