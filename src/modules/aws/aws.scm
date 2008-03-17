cfabba52f4599210cd6ec8a16bc502e9479cebaf
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-03-17 01:00:51 +0000

    Got AWS S3 to work

--- src/modules/aws/aws.scm
+++ src/modules/aws/aws.scm
@@ -1,28 +1,61 @@
+(in-module 'aws)
+
+(module-export! 'awsop)
+
 (use-module '{fdweb texttools})
 
-(define accesskeysecret "0Q+UGBUJQYJL3vq85mTJXLnlN2kfEw5xdKmS84c7")
-(define accesskeyid "1QYJ5P9NZE42YRQK5B02")
+;; Default (non-working) values from the Amazon web site
+(define secretawskey
+  (or (getenv "AWS_SECRET_ACCESS_KEY")
+      "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o"))
+(define awskeyid
+  (or (getenv "AWS_ACCESS_KEY_ID")
+      "0PN5J17HBGZHT7JJ3X82"))
+
+(config-def! 'secretawskey
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (set! secretawskey val)
+		   secretawskey)))
+(config-def! 'awskeyid
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (set! awskeyid val)
+		   awskeyid)))
+
+(comment ;; Test case from Amazon website
+ (set! accesskeysecret )
+ (set! accesskeyid "0PN5J17HBGZHT7JJ3X82"))
+
+(define teststring
+  "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg")
+
+(define (hack-date)
+  (let ((t (gmtimestamp)))
+    (stringout (capitalize (symbol->string (get t 'dowid))) ", "
+	       (get t 'date) " " (get t 'month-short) " "
+	       (get t 'year) " " (get t 'hms) " +0000")))
 
 (define (awsop op bucket path subpath (content #f) (ctype "text"))
-  (let* ((date (get (timestamp) 'iso))
+  (let* ((date (hack-date))
 	 (cheaders "")
 	 (cresource (string-append "/" bucket path))
-	 (sig (string-append op "\n"
-			     (if content (packet->base64 (md5 content)) "") "\n"
-			     (if content ctype "") "\n"
-			     date "\n" cheaders cresource))
-	 (authorization (string-append "AWS " accesskeyid ":"
-				       (packet->base64 (hmac-sha1 accesskeysecret sig))))
+	 (sig (string-append
+	       op "\n"
+	       (if content (packet->base64 (md5 content)) "") "\n"
+	       (if content ctype "") "\n"
+	       date "\n" cheaders "/" bucket path))
+	 (authorization
+	  (string-append
+	   "AWS " awskeyid ":"
+	   (packet->base64 (hmac-sha1 secretawskey sig))))
 	 (url (string-append "http://" bucket ".s3.amazonaws.com" path))
 	 (urlparams (frame-create #f)))
-    (%watch sig authorization)
     (when content (add! urlparams 'content-type ctype))
     (add! urlparams 'header (string-append "Date: " date))
-    (add! urlparams 'header (cons 'date date))
-    (add! urlparams 'header (cons 'authorization authorization))
+    (add! urlparams 'header (string-append "Authorization: " authorization))
     (urlget url urlparams)))
 
-
-
-
+(comment
+ (awsop "GET" "data.beingmeta.com" "/brico/brico.db" ""))
 
