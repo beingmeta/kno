7a68b77598f8791e32b8cf3008c93f043581cd36
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-09-08 21:12:56 +0000

    Added docs and better parameter handling to MYSQL module

--- src/cmodules/mysql.c
+++ src/cmodules/mysql.c
@@ -29,12 +29,10 @@ static fdtype callmysqlproc(struct FD_FUNCTION *fn,int n,fdtype *args);
 
 typedef struct FD_MYSQL {
   FD_EXTDB_FIELDS;
-#if FD_THREADS_ENABLED
-  u8_mutex dblock;
-#endif
   MYSQL _db, *db;} FD_MYSQL;
 typedef struct FD_MYSQL *fd_mysql;
 
+/* This is used as a buffer for inbound bindings */
 union BINDBUF { double fval; void *ptr; long lval; long long llval;};
 
 typedef struct FD_MYSQL_PROC {
@@ -50,7 +48,7 @@ typedef struct FD_MYSQL_PROC {
 typedef struct FD_MYSQL_PROC *fd_mysql_proc;
 
 static fd_exception MySQL_Error=_("MySQL Error");
-
+static fd_exception MySQL_NoConvert=_("Can't convert value to SQL");
 static fdtype merge_symbol;
 
 static fdtype intern_upcase(u8_output out,u8_string s)
@@ -82,9 +80,11 @@ static fdtype merge_colinfo(FD_MYSQL *dbp,fdtype colinfo)
 
 /* Opening connections */
 
+/* Everything but the hostname and dbname is optional.
+   In theory, we could have the dbname be optional, but for now we'll
+   require it.  */
 static fdtype open_mysql
-  (fdtype hostname,fdtype dbname,
-   fdtype colinfo,
+  (fdtype hostname,fdtype dbname,fdtype colinfo,
    fdtype user,fdtype password,
    fdtype port,fdtype options)
 {
@@ -92,44 +92,71 @@ static fdtype open_mysql
   char *host, *username, *passwd, *dbstring, *sockname;
   int portno=0, flags=0;
   struct FD_MYSQL *dbp=u8_alloc(struct FD_MYSQL);
+  /* Initialize the cons (does a memset too) */
   FD_INIT_FRESH_CONS(dbp,fd_extdb_type);
+  /* Initialize the MYSQL side of things (after the memset!) */
   dbp->db=mysql_init(&(dbp->_db));
-  mysql_options(&(dbp->_db),MYSQL_READ_DEFAULT_GROUP,u8_appid());
   if ((dbp->db)==NULL) {
     const char *errmsg=mysql_error(&(dbp->_db));
     u8_seterr(MySQL_Error,"open_mysql",u8_strdup(errmsg));
     return FD_ERROR_VALUE;}
+  else if (mysql_set_character_set(dbp->db,"utf8")) {
+    const char *errmsg=mysql_error(&(dbp->_db));
+    u8_seterr(MySQL_Error,"open_mysql",u8_strdup(errmsg));
+    return FD_ERROR_VALUE;}
+
+  /* If the hostname looks like a filename, we assume it's a Unix domain
+     socket. */
   if (strchr(FD_STRDATA(hostname),'/')==NULL) {
     host=FD_STRDATA(hostname); sockname=NULL;}
   else {
     sockname=FD_STRDATA(hostname); host=NULL;}
+  /* Process the other arguments */
   dbstring=((FD_VOIDP(dbname)) ? (NULL) : (FD_STRDATA(dbname)));
   username=((FD_VOIDP(user)) ? (NULL) : (FD_STRDATA(user)));
   passwd=((FD_VOIDP(password)) ? (NULL) : (FD_STRDATA(password)));
   portno=((FD_VOIDP(port)) ? (0) : (fd_getint(port)));
   flags=0;
+
+  /* Try to connect */
   db=mysql_real_connect
     (dbp->db,host,username,passwd,dbstring,portno,sockname,flags);
-  if (db==NULL) {} 
+  if (db==NULL) {
+    const char *errmsg=mysql_error(&(dbp->_db));
+    mysql_close(dbp->db);
+    u8_free(dbp);
+    u8_seterr(MySQL_Error,"open_mysql",u8_strdup(errmsg));
+    return FD_ERROR_VALUE;} 
+
+  /* Initialize the other fields */
   dbp->dbhandler=&mysql_handler;
   dbp->colinfo=fd_incref(colinfo);
   dbp->spec=u8_strdup(FD_STRDATA(hostname));
-  dbp->info=u8_strdup(FD_STRDATA(hostname));
-  u8_init_mutex(&(dbp->dblock));
+  dbp->info=
+    u8_mkstring("%s;client %s",
+		mysql_get_host_info(db),
+		mysql_get_client_info());
+
   return FDTYPE_CONS(dbp);
 }
 
 static void recycle_mysqldb(struct FD_EXTDB *c)
 {
   struct FD_MYSQL *dbp=(struct FD_MYSQL *)c;
+
+  fd_decref(dbp->colinfo);
   u8_free(dbp->spec); u8_free(dbp->info);
+
   mysql_close(dbp->db);
-  u8_destroy_mutex(&(dbp->dblock));
+
   if (FD_MALLOCD_CONSP(c)) u8_free(c);
 }
 
 /* Binding out */
 
+/* This sets up outbound bindings for a statement.
+   It assumes that outval->buffer_type has been initialized
+   from field-specific information. */
 static void outbound_setup(MYSQL_BIND *outval)
 {
   switch (outval->buffer_type) {
@@ -173,6 +200,13 @@ static void outbound_setup(MYSQL_BIND *outval)
   }
 }
 
+/* This returns a LISP pointer for a particular output column
+   for a statement given prepared bindings.  We use a limited typespace and
+   rely on most of the magic happening inside the MySQL client library.
+   The main tricky case here is TEXT/BLOB values (see below) which
+     we wait to retrieve.
+   One potential optimization would be to go ahead and preallocate buffers
+     for those columns when their size is known or constrained.  */
 static fdtype outbound_get(MYSQL_STMT *stmt,MYSQL_BIND *bindings,int column)
 {
   MYSQL_BIND *outval=&(bindings[column]);
@@ -195,6 +229,10 @@ static fdtype outbound_get(MYSQL_STMT *stmt,MYSQL_BIND *bindings,int column)
     double floval=*((double *)(outval->buffer));
     return fd_make_double(floval);}
   case MYSQL_TYPE_STRING: case MYSQL_TYPE_BLOB: {
+    /* This is the only tricky case.  When we do the overall fetch,
+       the TEXT/BLOCK columns are not retrieved, though their sizes
+       are.  We then use mysql_stmt_fetch_column to get the particular
+       value once we have consed a buffer to use. */
     fdtype value; int binary=((outval->buffer_type)==MYSQL_TYPE_BLOB);
     int datalen=(*(outval->length)), buflen=datalen+((binary) ? (0) : (1));
     outval->buffer=u8_alloc_n(buflen,unsigned char);
@@ -213,6 +251,8 @@ static fdtype outbound_get(MYSQL_STMT *stmt,MYSQL_BIND *bindings,int column)
 
 /* Getting outputs from a prepared statement */
 
+/* This gets all the values returned by a statement, using
+   prepared buffers for outbound variables and known column names. */
 static fdtype get_stmt_values
   (MYSQL_STMT *stmt,fdtype colinfo,int n_cols,
    fdtype *colnames,MYSQL_BIND *outbound)
@@ -230,6 +270,7 @@ static fdtype get_stmt_values
     struct FD_KEYVAL *kv=u8_alloc_n(n_cols,struct FD_KEYVAL);
     i=0; while (i<n_cols) {
       fdtype value=outbound_get(stmt,outbound,i);
+      /* Convert outbound variables via colmaps if specified. */
       if (FD_VOIDP(colmaps[i]))
 	kv[i].value=value;
       else if (FD_APPLICABLEP(colmaps[i])) {
@@ -254,6 +295,12 @@ static fdtype get_stmt_values
       else kv[i].value=value;
       kv[i].key=colnames[i];
       i++;}
+    /* How to merge values.  Currently, if MERGEFN is #t
+       and there's only one column, we just use that directly.
+       Otherwise, we apply MERGEFN to the returned slotmap and use
+       that value.  With no MERGEFN we just make a slotmap.
+       It might be cool to do a schemap rather than a slotmap
+       at some point. */
     if ((n_cols==1) && (FD_TRUEP(mergefn))) {
       result=kv[0].value;
       u8_free(kv);}
@@ -280,6 +327,8 @@ static fdtype get_stmt_values
   else return results;
 }
 
+/* This inits the vectors used when converting results from statement
+   execution. */
 static int init_stmt_results
   (MYSQL_STMT *stmt,MYSQL_BIND **outboundptr,fdtype **colnamesptr)
 {
@@ -301,7 +350,12 @@ static int init_stmt_results
     memset(outbound,0,sizeof(MYSQL_BIND)*n_cols);
     while (i<n_cols) {
       colnames[i]=intern_upcase(&out,fields[i].name);
+      /* Synchronize the signed and unsigned fields to get error handling. */
       if ((fields[i].flags)&(UNSIGNED_FLAG)) outbound[i].is_unsigned=1;
+      /* TEXT fields are confusingly labelled with MYSQL_TYPE_BLOB.
+	 We check the BINARY flag and set the corresponding MYSQL_BIND
+	 buffer type to MYSQL_TYPE_STRING or MYSQL_TYPE_BLOB as
+	 appropriate. */
       if (fields[i].type==MYSQL_TYPE_BLOB)
 	if ((fields[i].flags)&(BINARY_FLAG))
 	  outbound[i].buffer_type=fields[i].type;
@@ -321,6 +375,10 @@ static int init_stmt_results
 
 /* Simple execution */
 
+/* Straightforward execution of a single string.  We use prepared
+   statements anyway (rather than mysql_fetch_row) because it lets us
+   use raw C types consistent with prepared statements, rather than
+   using the converted strings from mysql_fetch_row.  */
 static fdtype mysqlexec(struct FD_MYSQL *dbp,fdtype string,fdtype colinfo_arg)
 {
   MYSQL *db=dbp->db;
@@ -374,6 +432,7 @@ static fdtype mysqlmakeproc
   int flags=0, consed_colinfo=0, n_params, n_cols, retval;
   struct FD_MYSQL_PROC *dbproc=u8_alloc(struct FD_MYSQL_PROC);
   FD_INIT_FRESH_CONS(dbproc,fd_extdb_proc_type);
+
   dbproc->stmt=mysql_stmt_init(db);
   if (dbproc->stmt)
     retval=mysql_stmt_prepare(dbproc->stmt,stmt,stmt_len);
@@ -391,14 +450,16 @@ static fdtype mysqlmakeproc
 
   dbproc->colinfo=merge_colinfo(dbp,colinfo);
 
+  /* Set up the vectors we'll use for statement execution. */
   dbproc->n_cols=n_cols=
     init_stmt_results(dbproc->stmt,&(dbproc->outbound),&(dbproc->colnames));
-
   dbproc->n_params=n_params=mysql_stmt_param_count(dbproc->stmt);
   dbproc->inbound=u8_alloc_n(n_params,MYSQL_BIND);
   memset(dbproc->inbound,0,sizeof(MYSQL_BIND)*n_params);
   dbproc->bindbuf=u8_alloc_n(n_params,union BINDBUF);
 
+  /* Set up the other fields */
+  
   dbproc->dbhandler=&mysql_handler;
   dbproc->db=(fdtype)dbp; fd_incref(dbproc->db);
   dbproc->mysqldb=db;
@@ -410,6 +471,8 @@ static fdtype mysqlmakeproc
 
   u8_init_mutex(&(dbproc->lock));
 
+  /* Set up the paramtypes which determine how application arguments
+     are converted to SQL parameters. */
   {
     fdtype *paramtypes=u8_alloc_n(n_params,fdtype);
     int j=0; while (j<n_params) {
@@ -451,6 +514,8 @@ static void recycle_mysqlproc(struct FD_EXTDB_PROC *c)
   
   u8_free(dbp->spec); u8_free(dbp->qtext);
 
+  u8_destroy_mutex(&(dbp->lock));
+
   fd_decref(dbp->db);
   if (FD_MALLOCD_CONSP(c)) u8_free(c);
 }
@@ -462,14 +527,22 @@ static fdtype callmysqlproc(struct FD_FUNCTION *fn,int n,fdtype *args)
   int n_params=dbproc->n_params, retval=1;
   MYSQL_BIND *inbound=dbproc->inbound;
   union BINDBUF *bindbuf=dbproc->bindbuf;
+
+  /* Argbuf stores objects we consed in the process of
+     converting application objects to SQLish values. */
   fdtype _argbuf[4], *argbuf=
     ((n_params<4) ? (_argbuf) : (u8_alloc_n(n_params,fdtype)));
   fdtype *ptypes=dbproc->paramtypes;
   fdtype values=FD_EMPTY_CHOICE;
   int i=0, ret=-1;
+
+  /* DB procs are synchronized. */
   u8_lock_mutex(&(dbproc->lock));
+
+  /* Initialize the input parameters from the arguments. */
   while (i<n_params) {
     fdtype arg=args[i];
+    /* Use the ptypes to map application arguments into SQL. */
     if (FD_VOIDP(ptypes[i])) argbuf[i]=FD_VOID;
     else if ((FD_OIDP(arg)) && (FD_OIDP(ptypes[i]))) {
       FD_OID addr=FD_OID_ADDR(arg);
@@ -478,7 +551,9 @@ static fdtype callmysqlproc(struct FD_FUNCTION *fn,int n,fdtype *args)
       argbuf[i]=arg=FD_INT2DTYPE(offset);}
     else if (FD_APPLICABLEP(ptypes[i])) {
       argbuf[i]=arg=fd_apply(ptypes[i],1,&arg);}
-    else {}
+    else argbuf[i]=FD_VOID;
+
+    /* Now set up the bindings */
     if (FD_FIXNUMP(arg)) {
       inbound[i].is_unsigned=0;
       inbound[i].buffer_type=MYSQL_TYPE_LONG;
@@ -510,27 +585,67 @@ static fdtype callmysqlproc(struct FD_FUNCTION *fn,int n,fdtype *args)
       inbound[i].buffer=FD_PACKET_DATA(arg);
       inbound[i].buffer_length=FD_PACKET_LENGTH(arg);
       inbound[i].length=NULL;}
-    else {/* Should do some kind of error? */}
+    /* This catches cases where the conversion process produces an
+       error. */
+    else if (FD_ABORTP(arg)) {
+      int j=0;
+      u8_unlock_mutex(&(dbproc->lock));
+      while (j<n_params) {fd_decref(argbuf[j]); j++;}
+      if (argbuf!=_argbuf) u8_free(argbuf);
+      return FD_ERROR_VALUE;}
+    else if (FD_TRUEP(ptypes[i])) {
+      /* If the ptype is #t, try to convert it into a string,
+	 and catch it if you have an error. */
+      struct U8_OUTPUT out; U8_INIT_OUTPUT(&out,32);
+      if (fd_unparse(&out,arg)<0) {
+	fd_seterr(MySQL_NoConvert,"callmysqlproc",
+		  u8_strdup(dbproc->qtext),fd_incref(arg));
+	u8_unlock_mutex(&(dbproc->lock));
+	i=0; while (i<n_params) {fd_decref(argbuf[i]); i++;}
+	if (argbuf!=_argbuf) u8_free(argbuf);
+	return FD_ERROR_VALUE;}
+      else {
+	u8_string as_string=out.u8_outbuf;
+	int stringlen=out.u8_outptr-out.u8_outbuf;
+	inbound[i].buffer_type=MYSQL_TYPE_STRING;
+	inbound[i].buffer=as_string;
+	inbound[i].buffer_length=stringlen;
+	inbound[i].length=NULL;
+	/* We put the consed string into the argbug as a Lisp
+	   string so that we'll free it when we're done. */
+	argbuf[i]=fd_init_string(NULL,stringlen,as_string);}}
+    else {
+      /* Finally, if we can't convert the value, we error. */
+      fd_seterr(MySQL_NoConvert,"callmysqlproc",
+		u8_strdup(dbproc->qtext),fd_incref(arg));
+      u8_unlock_mutex(&(dbproc->lock));
+      i=0; while (i<n_params) {fd_decref(argbuf[i]); i++;}
+      if (argbuf!=_argbuf) u8_free(argbuf);
+      return FD_ERROR_VALUE;}
     i++;}
   
   /* Bind and execute */
   retval=mysql_stmt_bind_param(dbproc->stmt,inbound);
   if (retval==0) retval=mysql_stmt_execute(dbproc->stmt);
 
+  /* Handle error cases */
   if (retval) {
     const char *errmsg=mysql_stmt_error(dbproc->stmt);
-    u8_seterr(MySQL_Error,"mysqlproc",u8_strdup(errmsg));
     u8_unlock_mutex(&(dbproc->lock));
+    u8_seterr(MySQL_Error,"mysqlproc",u8_strdup(errmsg));
+    i=0; while (i<n_params) {fd_decref(argbuf[i]); i++;}
+    if (argbuf!=_argbuf) u8_free(argbuf);
     return FD_ERROR_VALUE;}    
 
+  /* Get any values.  This will handle it's own errors. */
   values=get_stmt_values
     (dbproc->stmt,dbproc->colinfo,
      dbproc->n_cols,dbproc->colnames,dbproc->outbound);
 
   u8_unlock_mutex(&(dbproc->lock));
 
-  i=0; while (i<n_params) {
-    fd_decref(argbuf[i]); i++;}
+  /* Clean up */
+  i=0; while (i<n_params) {fd_decref(argbuf[i]); i++;}
   if (argbuf!=_argbuf) u8_free(argbuf);
 
   return values;
