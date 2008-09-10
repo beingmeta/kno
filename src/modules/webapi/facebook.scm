2a07bbac94a1a0db50dec4bdd927db6c788b6eb9
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-09-10 22:19:00 +0000

    Added more facebook API updates

--- src/modules/webapi/facebook.scm
+++ src/modules/webapi/facebook.scm
@@ -1,23 +1,33 @@
 (in-module 'webapi/facebook)
 
+;;; This implements simple access to the Facebook API
+
+(define version "$Id: calltrack.scm 1698 2007-08-24 17:54:19Z haase $")
+
 (use-module '{ezrecords fdweb texttools})
 
+;; Not currently used
 (defrecord facepp appid apikey secretkey)
 
 (define apikey "1ab7fa75d12550c67cf11f3c6be17cb1")
 (define secretkey "1932a9c8a3c12044b8bbd11397da16ed")
 (define appid "24262579118")
 
-(define (config-fb/appinfo var (val))
-  (if (bound? val)
-      (if (and (vector? val)
-	       (= (length val) 3)
-	       (every? string? val))
-	  (begin (set! appid (first val))
-		 (set! apikey (second val))
-		 (set! secretkey (third val))))
-      (vector appid apikey)))
-(config-def! 'fb/appinfo config-fb/appinfo)
+(config-def! 'fb/appid
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (set! appid val)
+		   appid)))
+(config-def! 'fb/apikey
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (set! apikey val)
+		   apikey)))
+(config-def! 'fb/apisecret
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (set! secretkey val)
+		   secretkey)))
 
 (define facebook-uri "https://api.facebook.com/restserver.php")
 
