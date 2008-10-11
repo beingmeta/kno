049a08eb9857799c0bc78d64aca8503547b1c428
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-10-11 11:46:54 +0000

    Added tentative support for SIMPLEDB

--- src/modules/aws/aws.scm
+++ src/modules/aws/aws.scm
@@ -1,14 +1,15 @@
 (in-module 'aws)
 
-(module-export! 'awsop)
+(module-export! '{s3op s3signature s3uri})
 
 (use-module '{fdweb texttools})
 
-;; Default (non-working) values from the Amazon web site
+;; Default (non-working) values from the online documentation
+;;  Helpful for testing requests
 (define secretawskey
   (or (getenv "AWS_SECRET_ACCESS_KEY")
-      "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o"))
-(define awskeyid
+      #"uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o"))
+(define awskey
   (or (getenv "AWS_ACCESS_KEY_ID")
       "0PN5J17HBGZHT7JJ3X82"))
 
@@ -17,45 +18,103 @@
 	       (if (bound? val)
 		   (set! secretawskey val)
 		   secretawskey)))
-(config-def! 'awskeyid
+(config-def! 'awskey
 	     (lambda (var (val))
 	       (if (bound? val)
-		   (set! awskeyid val)
-		   awskeyid)))
-
-(comment ;; Test case from Amazon website
- (set! accesskeysecret )
- (set! accesskeyid "0PN5J17HBGZHT7JJ3X82"))
+		   (set! awskey val)
+		   awskey)))
 
 (define teststring
   "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg")
 
-(define (hack-date)
-  (let ((t (gmtimestamp)))
-    (stringout (capitalize (symbol->string (get t 'dowid))) ", "
-	       (get t 'date) " " (get t 'month-short) " "
-	       (get t 'year) " " (get t 'hms) " +0000")))
-
-(define (awsop op bucket path subpath (content #f) (ctype "text"))
-  (let* ((date (hack-date))
-	 (cheaders "")
-	 (cresource (string-append "/" bucket path))
-	 (sig (string-append
-	       op "\n"
-	       (if content (packet->base64 (md5 content)) "") "\n"
-	       (if content ctype "") "\n"
-	       date "\n" cheaders "/" bucket path))
-	 (authorization
+(define (s3signature op bucket path (date (gmtimestamp)) (cheaders "")
+		     (content-sig "") (content-ctype ""))
+  (let* ((date (if (string? date) date
+		   (if (number? date) (number->string date)
+		       (get date 'rfc822))))
+	 (description
 	  (string-append
-	   "AWS " awskeyid ":"
-	   (packet->base64 (hmac-sha1 secretawskey sig))))
+	   op "\n"
+	   content-sig "\n"
+	   content-ctype "\n"
+	   date "\n" cheaders "/" bucket path)))
+    ;; (message "Description=" (write description))
+    (hmac-sha1 secretawskey description)))
+
+(define (s3op op bucket path (content #f) (ctype "text") (cheaders ""))
+  (let* ((date (gmtimestamp))
+	 (cresource (string-append "/" bucket path))
+	 (sig (s3signature op bucket path date ""
+			   (if content (packet->base64 (md5 content)) "")
+			   (if content ctype "")))
+	 (authorization
+	  (string-append "AWS " awskey ":" (packet->base64 sig)))
 	 (url (string-append "http://" bucket ".s3.amazonaws.com" path))
 	 (urlparams (frame-create #f)))
+    ;; (message "sig=" sig)
+    ;; (message "authorization: " authorization)
     (when content (add! urlparams 'content-type ctype))
-    (add! urlparams 'header (string-append "Date: " date))
+    (add! urlparams 'header (string-append "Date: " (get date 'rfc822)))
     (add! urlparams 'header (string-append "Authorization: " authorization))
     (urlget url urlparams)))
 
+(define (s3uri bucket path expires (op "GET") (cheaders ""))
+  (let* ((expires (if (number? expires) expires (get expires 'tick)))
+	 (sig (s3signature "GET" bucket path expires cheaders)))
+    (string-append
+     "http://" bucket ".s3.amazonaws.com" path "?"
+     "AWSAccessKeyId=" awskey "&"
+     "Expires=" (number->string expires) "&"
+     "Signature=" (packet->base64 sig))))
+
 (comment
- (awsop "GET" "data.beingmeta.com" "/brico/brico.db" ""))
+ (s3op "GET" "data.beingmeta.com" "/brico/brico.db" ""))
+
+;;; Simple DB stuff
+
+(define sdb-common-params
+  '("Version" "2007-11-07" "SignatureVersion" "1"))
+
+(define (simpledb-signature ptable)
+  (let ((desc (stringout
+		(doseq (key (lexsorted (getkeys ptable)))
+		  (printout key
+			    (do-choices (v (get ptable key)) (printout v)))))))
+    (message "desc=" desc)
+    (hmac-sha1 secretawskey desc)))
+(define (simpledb-signature0 action timestamp)
+  (let ((desc (stringout action (get timestamp 'iso))))
+    (message "desc=" desc)
+    (hmac-sha1 secretawskey desc)))
+
+(define (simpledb->uri . params)
+  (let ((timestamp (gmtimestamp 'seconds))
+	(ptable (frame-create #f)))
+    (do ((p sdb-common-params (cddr p)))
+	((null? p))
+      (add! ptable (car p) (cadr p)))
+    (do ((p params (cddr p)))
+	((null? p))
+      (add! ptable (car p) (cadr p)))
+    (add! ptable "Timestamp" (get timestamp 'iso))
+    (add! ptable "AWSAccessKeyId" awskey)
+    (stringout "http://sdb.amazonaws.com/?"
+	       (do ((p params (cddr p)) (first #t #f))
+		   ((null? p) (printout))
+		 (printout (unless first  "&")
+			   (car p) "="
+			   (uriencode (stringout (cadr p)))))
+	       (do ((p sdb-common-params (cddr p)) (first #t #f))
+		   ((null? p) (printout))
+		 (printout "&" (car p) "=" (uriencode (stringout (cadr p)))))
+	       "&Timestamp=" (get timestamp 'iso) 
+	       "&AWSAccessKeyId=" awskey 
+	       "&Signature="
+	       (uriencode (packet->base64 (simpledb-signature ptable))))))
+
+(define (simpledb . params)
+  (let ((uri (apply simpledb->uri params)))
+    (urlget uri)))
+
+(module-export! '{simpledb-signature simpledb->uri simpledb})
 
