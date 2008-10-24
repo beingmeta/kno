facc56aba274a8231c9ff7385df30d046e10785f
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-10-24 00:54:15 +0000

    Added handlers.js

---
+++ src/fdweb/handlers.js
@@ -0,0 +1,152 @@
+/* Various utility handlers */
+
+var msgalert=false;
+
+function fdbmsg(string)
+{
+  if ((console) && (console.log))
+    console.log(string);
+  else if (msgalert)
+    alert(string);
+}
+
+/* INPUT SHOWHELP */
+
+function block_eltp(elt)
+{
+  var name=helptext.tagName;
+  return ((name=='DIV') || (name=='P') || (name=='LI') || (name=='UL'));
+}
+
+function fdb_showhelp_onfocus(evt)
+{
+  var target=evt.target;
+  if ((target.tagName == 'INPUT') && (target.hasAttribute('HELPTEXT'))) {
+    var helptext=document.getElementById(target.getAttribute('HELPTEXT'));
+    if (helptext) {
+      if (block_eltp(helptext))
+	helptext.style.display='block';
+      else helptext.style.display='inline';}}
+}
+
+function fdb_hidehelp_onblur(evt)
+{
+  var target=evt.target;
+  if ((target.tagName == 'INPUT') && (target.hasAttribute('HELPTEXT'))) {
+    var helptext=document.getElementById(target.getAttribute('HELPTEXT'));
+    if (helptext) helptext.style.display='none';}
+}
+
+  /* SHOWHIDE */
+
+function fdb_showhide_onclick(evt)
+{
+  var target=evt.target;
+  while (target)
+    if ((target.hasAttribute('HIDEONCLICK')) || (target.hasAttribute('SHOWONCLICK'))) {
+      var tohide=target.getAttribute('HIDEONCLICK');
+      var toshow=target.getAttribute('SHOWONCLICK');
+      if (tohide) tohide=tohide.split(',');
+      if (toshow) toshow=toshow.split(',');
+      if (tohide) {
+	var i=0; while (i<tohide.length) {
+	  var elt=document.getElementById(tohide[i]); 
+	  if (elt) elt.style.display='none';
+	  i++;}}
+      if (toshow) {
+	var i=0; while (i<toshow.length) {
+	  var elt=document.getElementById(toshow[i]); 
+	  if (elt)
+	    if (block_eltp(elt)) elt.style.display='block';
+	    else elt.style.display='block';
+	  i++;}}
+      return;}
+    else target=target.parentNode;
+}
+
+/* Autoprompt */
+
+function fdb_autoprompt_onfocus(evt)
+{
+  var elt=evt.target;
+  if ((elt) && (elt.hasAttribute('isempty')))
+    elt.value='';
+  fdb_showhelp_onfocus(evt);
+}
+
+function fdb_autoprompt_onblur(evt)
+{
+  var elt=evt.target;
+  if (elt.value=='') {
+    elt.setAttribute('isempty','yes');
+    elt.value=elt.getAttribute('prompt');}
+  else elt.removeAttribute('isempty');
+  fdb_hidehelp_onblur(evt);
+}
+
+function fdb_autoprompt_setup()
+{
+  var elements=document.getElementsByTagName('INPUT');
+  var i=0; if (elements) while (i<elements.length) {
+    var elt=elements[i++];
+    if ((elt.type=='text') &&
+	((elt.className=='autoprompt') ||
+	 (elt.hasAttribute('prompt'))))
+      if (elt.value=='') {
+	elt.value=fdbGet(elt,'prompt');
+	elt.setAttribute('isempty','yes');}}
+}
+
+// Removes autoprompt text from empty fields
+function fdb_autoprompt_cleanup()
+{
+  var elements=document.getElementsByTagName('INPUT');
+  var i=0; if (elements) while (i<elements.length) {
+    var elt=elements[i++];
+    if (elt.className=='autoprompt_empty')
+      elt.value="";}
+}
+
+/* Tabs */
+
+function fdb_tab_onclick(evt)
+{
+  var elt=evt.target;
+  if (elt) {
+    while (elt)
+      if (elt.hasAttribute("contentid")) break;
+      else elt=elt.parentNode;
+    if (elt==null) return;
+    var select_var=elt.getAttribute('selectvar');
+    var content_id=elt.getAttribute('contentid');
+    var content=document.getElementById(content_id);
+    var select_elt=document.getElementById(select_var+'_INPUT');
+    var parent=elt.parentNode;
+    var sibs=parent.childNodes;
+    if (content==null) {
+      fdbmsg("No content for "+content_id);
+      return;}
+    // evt.preventDefault(); evt.cancelBubble=true;
+    // This lets forms pass tab information along
+    if (select_elt) select_elt.value=content_id;
+    var i=0; while (i<sibs.length) {
+      var node=sibs[i++];
+      if ((node.nodeType==1) && (node.className=='tab')) {
+	var cid=node.getAttribute('contentid');
+	var cdoc=document.getElementById(cid);
+	if (node==elt) {}
+	else if (node.hasAttribute('showtab'))
+	  node.removeAttribute('showtab');
+	if (cdoc==content) {}
+	else if (cdoc) {
+	  // fdbMessage("Hiding "+cdoc+"="+cid);
+	  if (cdoc.hasAttribute('showtab'))
+	    cdoc.removeAttribute('showtab');}}}
+    node.setAttribute('showtab','yes');
+    content.setAttribute('showtab','yes');
+    return false;}
+}
+
+
+
+
