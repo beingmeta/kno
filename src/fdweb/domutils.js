61d50f0bafe05ee32bd09a1fe24de98cf27175d7
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2008-10-31 01:30:31 +0000

    Added domutils.js

---
+++ src/fdweb/domutils.js
@@ -0,0 +1,96 @@
+/* Various utilities for manipulating the dom */
+
+function $(eltarg)
+{
+  if (typeof(eltarg) == 'string')
+    return document.getElementById(eltarg);
+  else return eltarg;
+}
+
+function fdbAddElements(elt,args,i)
+{
+  while (i<args.length) {
+    var arg=args[i++];
+    if (typeof(arg) == 'string')
+      elt.appendChild(document.createTextNode(arg));
+    else elt.appendChild(arg);}
+  return elt;
+}
+
+function fdbAddAttributes(elt,attribs)
+{
+  if (attribs) {
+    for (key in attribs) {
+      elt.setAttribute(key,attribs[key]);}
+    return elt;}
+  else return elt;
+}
+
+function fdbNewElement(tag,class)
+{
+  var elt=document.createElement(tag);
+  elt.className=class;
+  fdbAddElements(elt,arguments,2);
+  return elt;
+}
+
+function fdbNewElementW(tag,class,attribs)
+{
+  var elt=document.createElement(tag);
+  elt.className=class;
+  fdbAddAttributes(elt,attribs);
+  fdbAddElements(elt,arguments,3);
+  return elt;
+}
+
+function fdbSpan(class)
+{
+  var elt=document.createElement('span');
+  elt.className=class;
+  fdbAddElements(elt,arguments,1);
+  return elt;
+}
+
+function fdbSpanW(class,attribs)
+{
+  var elt=document.createElement('span');
+  elt.className=class;
+  fdbAddAttributes(elt,attribs);
+  fdbAddElements(elt,arguments,2);
+  return elt;
+}
+
+function fdbDiv(class)
+{
+  var elt=document.createElement('div');
+  elt.className=class;
+  fdbAddElements(elt,arguments,1);
+  return elt;
+}
+
+function fdbDivW(class,attribs)
+{
+  var elt=document.createElement('div');
+  elt.className=class;
+  fdbAddAttributes(elt,attribs);
+  fdbAddElements(elt,arguments,2);
+  return elt;
+}
+
+function fdbInput(type,name,value,class)
+{
+  var elt=document.createElement('input');
+  elt.type=type; elt.name=name; elt.value=value;
+  if (class) elt.className=class;
+  return elt;
+}
+
+function fdbCheckbox(name,value,checked)
+{
+  var elt=document.createElement('input');
+  elt.type='checkbox'; elt.name=name; elt.value=value;
+  if (checked) elt.checked=true;
+  else elt.checked=false;
+  return elt;
+}
+
