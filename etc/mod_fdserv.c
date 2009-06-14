9391fad153b56da943547651f251b127a47c4d6d
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-06-14 14:17:49 +0000

    Added SVN keywords, use in init messages, etc

--- etc/mod_fdserv.c
+++ etc/mod_fdserv.c
@@ -13,8 +13,8 @@
 
 */
 
-static char versionid[] = "$Id:$";
-static char revisioninfo[] = "$Revision:$";
+static char versionid[] = "$Id$";
+static char revisioninfo[] = "$Revision$";
 
 #include "httpd.h"
 #include "http_config.h"
