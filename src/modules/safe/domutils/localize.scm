b10b717dd0c5668572d69a66f77e51670095120a
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-10-16 02:20:58 +0000

    Fixes to DOM localize and various trace points

--- src/modules/safe/domutils/localize.scm
+++ src/modules/safe/domutils/localize.scm
@@ -20,24 +20,31 @@
 	     filename)))
 
 (define (localref ref urlmap base target read)
-  (try (get urlmap ref)
-       (let ((absref (if (string-starts-with? ref #((isalpha) (isalpha) (isalpha+) ":"))
-			 ref
-			 (mkpath (dirname base) ref)))
-	     (saveto (if (has-suffix target "/") target
-			 (string-append target "/"))))
-	 ;; (%watch ref base target saveto read)
-	 (try (get urlmap absref)
-	      (let* ((name (basename (uribase ref)))
-		     (content (urlcontent absref))
-		     (outfile (chkdir (append saveto ref))))
-		;; This has fragments and queries stripped (uribase)
-		;; and additionally has the 'directory' part of the URI
-		;; removed so that it's a local file name
-		(loginfo "Downloaded " (write outfile) " from " (write absref))
-		(write-file outfile content)
-		(store! urlmap absref outfile)
-		(mkpath read ref))))))
+  (try (tryif (or (empty-string? ref) (has-prefix ref "#")) ref)
+       (tryif (position "#" ref)
+	 (let ((hashpos (position "#" ref)))
+	   (string-append (localref (subseq ref 0 hashpos) urlmap base target read)
+			  (subseq ref hashpos))))
+       (get urlmap ref)
+       (if (string-starts-with? ref #((isalpha) (isalpha) (isalpha+) ":"))
+	   ref
+	   (let ((absref (if (string-starts-with? ref #((isalpha) (isalpha) (isalpha+) ":"))
+			     ref
+			     (mkpath (dirname base) ref)))
+		 (saveto (if (has-suffix target "/") target
+			     (string-append target "/"))))
+	     ;; (%watch ref base target saveto read)
+	     (try (get urlmap absref)
+		  (let* ((name (basename (uribase ref)))
+			 (content (urlcontent absref))
+			 (outfile (chkdir (append saveto ref))))
+		    ;; This has fragments and queries stripped (uribase)
+		    ;; and additionally has the 'directory' part of the URI
+		    ;; removed so that it's a local file name
+		    (loginfo "Downloaded " (write outfile) " from " (write absref))
+		    (write-file outfile content)
+		    (store! urlmap absref outfile)
+		    (mkpath read ref)))))))
 
 (define (dom/localize! dom base write (read))
   (default! read write)
@@ -48,6 +55,10 @@
       (dom/set! node 'href
 		(localref (get node 'href) urlmap base write read)))
     (do-choices (node (pick (dom/find dom "script") 'src))
+      (dom/set! node 'src
+		(localref (get node 'src) urlmap base write read)))
+    (do-choices (node (pick (dom/find dom "a") 'href))
       (dom/set! node 'href
-		(localref (get node 'src) urlmap base write read)))))
+		(localref (get node 'href) urlmap base write read)))))
+
 
