4351901eccdc14a2e0d4064525f1e0aa2c87db3f
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-11-20 16:51:25 +0000

    Fixes to localize to support amalgamates

--- src/modules/safe/domutils/localize.scm
+++ src/modules/safe/domutils/localize.scm
@@ -19,11 +19,13 @@
       (begin (system "mkdir -p " (dirname filename))
 	     filename)))
 
-(define (localref ref urlmap base target read)
+(define (localref ref urlmap base target read amalgamate)
   (try (tryif (or (empty-string? ref) (has-prefix ref "#")) ref)
+       (tryif (exists has-prefix ref amalgamate)
+	 (textsubst ref `(GREEDY ,amalgamate) ""))
        (tryif (position #\# ref)
 	 (let ((hashpos (position #\# ref)))
-	   (string-append (localref (subseq ref 0 hashpos) urlmap base target read)
+	   (string-append (localref (subseq ref 0 hashpos) urlmap base target read (qc amalgamate))
 			  (subseq ref hashpos))))
        (get urlmap ref)
        (if (string-starts-with? ref #((isalpha) (isalpha) (isalpha+) ":"))
@@ -37,28 +39,31 @@
 	     (try (get urlmap absref)
 		  (let* ((name (basename (uribase ref)))
 			 (content (urlcontent absref))
-			 (outfile (chkdir (append saveto ref))))
+			 (outfile (chkdir (append saveto ref)))
+			 (lref (mkpath read ref)))
 		    ;; This has fragments and queries stripped (uribase)
 		    ;; and additionally has the 'directory' part of the URI
 		    ;; removed so that it's a local file name
-		    (loginfo "Downloaded " (write outfile) " from " (write absref))
+		    (loginfo "Downloaded " (write outfile) " from " (write absref) " ref to " lref)
 		    (write-file outfile content)
-		    (store! urlmap absref outfile)
-		    (mkpath read ref)))))))
+		    (store! urlmap absref lref)
+		    lref))))))
 
-(define (dom/localize! dom base write (read))
+(define (dom/localize! dom base write (read) (amalgamate #f))
   (default! read write)
-  (let ((urlmap (make-hashtable)))
+  (let ((urlmap (make-hashtable))
+	(amalgamate (or amalgamate {})))
     (do-choices (node (dom/find dom "img"))
-      (dom/set! node 'src (localref (get node 'src) urlmap base write read)))
+      (dom/set! node 'src (localref (get node 'src) urlmap base write read (qc amalgamate))))
     (do-choices (node (dom/find dom "link"))
       (dom/set! node 'href
-		(localref (get node 'href) urlmap base write read)))
+		(localref (get node 'href) urlmap base write read (qc amalgamate))))
     (do-choices (node (pick (dom/find dom "script") 'src))
       (dom/set! node 'src
-		(localref (get node 'src) urlmap base write read)))
+		(localref (get node 'src) urlmap base write read (qc amalgamate))))
     (do-choices (node (pick (dom/find dom "a") 'href))
       (dom/set! node 'href
-		(localref (get node 'href) urlmap base write read)))))
+		(localref (get node 'href) urlmap base write read (qc amalgamate))))))
+
 
 
