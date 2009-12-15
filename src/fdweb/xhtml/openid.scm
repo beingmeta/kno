ae525dc775d37429bbc44f17d11afe046de31ed1
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-12-15 19:08:41 +0000

    Added OPENID library, first pass

---
+++ src/fdweb/xhtml/openid.scm
@@ -0,0 +1,125 @@
+(in-module 'xhtml/openid)
+
+(use-module '{texttools fdweb domutils varconfig})
+
+(define openid-servers (make-hashtable))
+(define-init openid/optinfo (make-hashtable))
+
+(define reqopts ;; requestopts
+  #["openid.sreg.optional" "nickname,email,fullname,country,postcode"
+    "openid.ns.ui" "http://openid.net/extensions/ui/1.0"
+    "openid.ui.icon" "true"
+    "openid.ns.ax" "http://openid.net/srv/ax/1.0"
+    "openid.ax.mode" "fetch_request"
+    "openid.ax.type.email" "http://axschema.org/contact/email"
+    "openid.ax.type.language" "http://axschema.org/pref/language"
+    "openid.ax.type.firstname" "http://axschema.org/namePerson/first"
+    "openid.ax.type.lastname" "http://axschema.org/namePerson/last"
+    "openid.ax.type.fullname" "http://axschema.org/namePerson"      
+    "openid.ax.type.nickname" "http://axschema.org/namePerson/friendly"
+    "openid.ax.type.homepage" "http://axschema.org/contact/web/default"
+    "openid.ax.type.pic" "http://axschema.org/media/image/default"
+    "openid.ax.type.sqpic" "http://axschema.org/media/image/aspect11"
+    "openid.ax.required" "fullname,nickname,firstname,lastname,homepage,pic,sqpic"])
+(define realm #f)
+(define callback #f)
+
+(varconfig! openid:opts default-opts)
+(varconfig! openid:callback callback)
+(varconfig! openid:realm realm)
+
+(define (openid-url string)
+  (if (has-prefix string "//") (set! string (string-append "http://" string))
+      (unless (or (has-prefix string "http:") (has-prefix string "https:"))
+	(set! string (string-append "http://" string))))
+  (let ((parsed (parseuri string)))
+    (stringout (try (get parsed 'scheme) "http") "://"
+	       (get parsed 'hostname) 
+	       (when (test parsed 'path)
+		 (dolist (elt (get parsed 'path)) (printout "/" elt))
+		 (when (test parsed 'name) (printout "/" (get parsed 'name)))))))
+
+(define (get-openid-server uri)
+  (try ;; (get openid-servers uri)
+       (let ((server (fetch-openid-server uri)))
+	 (when (exists? server) (store! openid-servers uri server))
+	 server)))
+
+(define (fetch-openid-server uri)
+  (let* ((req (urlget uri))
+	 (content (get req '%content))
+	 (xml (xmlparse (get req '%content) 'sloppy))
+	 (links (dom/find xml "link")))
+    (try (cons (try-choices (link links)
+		 (tryif (textsearch '{(WORD "openid.server") (WORD "openid2.provider")}
+				    (get link 'rel))
+		   (get link 'href)))
+	       (try (try-choices (link links)
+		      (tryif (textsearch '{(WORD "openid.delegate") (WORD "openid2.local_id")}
+					 (get link 'rel))
+			(get link 'href)))
+		    #f))
+	 ;; RDS, possibly (should probably check)
+	 (cons (xmlcontent (xmlget xml 'uri)) #f)
+	 (cons uri uri))))
+
+(define (guess-realm uri)
+  (let ((parsed (parseuri uri)))
+    (stringout (get parsed 'scheme) "://" (get parsed 'hostname) "/")))
+
+(define (guess-callback) (cgiget 'script_uri))
+
+(define (openid-redirect url (opts #f))
+  (let* ((server (get-openid-server url))
+	 (opts (or opts (try (get openid/optinfo url)
+			     (get openid/optinfo server)
+			     default-opts)))
+	 (callback (getopt opts 'callback (or callback (guess-callback))))
+	 (realm (getopt opts 'realm (or realm (guess-realm callback)))))
+    (scripturl+ (car server)
+      (try (getopt opts 'reqopts {}) reqopts)
+      "openid.mode" "checkid_setup"
+      "openid.ns" (getopt opts 'ns "http://specs.openid.net/auth/2.0")
+      "openid.identity"
+      (if (and (pair? server) (cdr server)) (cdr server)
+	  "http://specs.openid.net/auth/2.0/identifier_select")
+      "openid.claimed_id"
+      (if (and (pair? server) (cdr server)) (cdr server)
+	  "http://specs.openid.net/auth/2.0/identifier_select")
+      "openid.return_to" callback "openid.realm" realm
+      "openid.trust_root" realm)))
+
+(define (dedotit s) (string-subst s "." "_"))
+(define (redotit s) (string-subst s "_" "."))
+(define (add-openid-prefix s) (string-append "openid." s))
+
+(define (validate openid.identity openid.assoc_handle openid.signed openid.sig)
+  (let* ((provider (car (get-openid-server openid.identity)))
+	 (postdata `#["openid.mode" "check_authentication"
+		      "openid.assoc_handle" ,openid.assoc_handle
+		      "openid.signed" ,openid.signed
+		      "openid.sig" ,openid.sig])
+	 (keys (map add-openid-prefix (segment openid.signed ","))))
+    (doseq (key keys)
+      (unless (equal? key "openid.mode")
+	(store! postdata key (cgiget (string->lisp key)))))
+    (let ((result (urlpost provider #[] postdata)))
+      (and (textsearch #((bol) "is_valid:" (spaces*) "true" (eol))
+		       (get result '%content))
+	   result))))
+
+(define (doredirect uri)
+  ;; (%watch "REDIRECT" uri)
+  (cgiset! 'status 303)
+  (httpheader "Location: " uri))
+
+(define (openidauth (openid.mode #f) (openid.endpoint #f)
+		    (openid.identifier #f) (openid.identity #f))
+  (cond ((equal? openid.mode "id_res")
+	 (or (cgicall validate) (error "OpenID validation failed")))
+	((equal? openid.mode "cancel") #f)
+	((or openid.identifier openid.endpoint)
+	 (doredirect (openid-redirect (or openid.identifier openid.endpoint))))
+	(else #f)))
+
+(module-export! '{get-openid-server openid-url openidauth openid/optinfo})
