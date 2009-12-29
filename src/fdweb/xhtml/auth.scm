b79f96f4fd4de1d8fdda76e30d440173f5a240d5
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-12-29 10:52:01 +0000

    Reorganized AUTH

--- src/fdweb/xhtml/auth.scm
+++ src/fdweb/xhtml/auth.scm
@@ -1,35 +1,75 @@
 (in-module 'xhtml/auth)
 
 (use-module '{fdweb texttools})
-(use-module '{varconfig logger rulesets})
+(use-module '{varconfig logger rulesets ezrecords})
+
+(define %loglevel %notify!)
 
 (module-export! '{auth/getinfo
 		  auth/getuser
-		  auth/authorize!
-		  auth/deauthorize!
-		  auth/validate})
+		  auth/identify!
+		  auth/deauthorize!})
+
+(define (random-signature (length %siglen))
+  (let ((bytes '()))
+    (dotimes (i %siglen) (set! bytes (cons (random 256) bytes)))
+    (->packet bytes)))
 
 ;; The cookie/CGI var used to store the session ID
 (define authid 'AUTH)
-;; The key to use in signing session ids
-(define signature #f)
-;; The key to use when encrypting session ids
-(define encrypt #f)
 (varconfig! auth:id authid)
-(varconfig! auth:signature signature)
-(varconfig! auth:encrypt encrypt)
+
+;; How long a key to use when signing 
+(define %siglen 32)
+;; The key to use in signing session ids
+(define signature (random-signature))
+(define signature-file #f)
+
+(define (signature-config var (val))
+  (if (not (bound? val)) signature
+      (begin
+	(cond ((not val)
+	       (set! signature-file #f)
+	       (set! signature (random-signature)))
+	      ((packet? val)
+	       (set! signature val))
+	      ((number? val)
+	       (if (and (integer? val) (> 4096 val 16))
+		   (set! signature (random-signature val))
+		   (error "Invalid signature length" val)))
+	      ((not (string? val))
+	       (error "Invalid signature key" val))
+	      ((has-prefix val "0x")
+	       (set! signature (base16->packet (subseq val 2))))
+	      ((has-prefix val "0z")
+	       (set! signature (base64->packet (subseq val 2))))
+	      ((has-prefix val "{base16}")
+	       (set! signature (base16->packet (subseq val 8))))
+	      ((has-prefix val "{base64}")
+	       (set! signature (base64->packet (subseq val 8))))
+	      (else (error "Can't interpret signature value" val)))
+	(when signature-file (write-file val signature)))))
+(config-def! 'auth:signature signature-config)
+
+(define (signature-file-config var (val))
+  (cond ((not (bound? val)) signature-file)
+	((not val) (set! signature-file #f))
+	((not (string? val)) (error "Invalid signature file" val))
+	((file-exists? val)
+	 (set! signature-file val)
+	 (set! signature (filedata val)))
+	(else (set! signature-file val)
+	      (write-file val signature))))
+(config-def! 'auth:sigfile signature-file-config)
 
 ;; Various expiration intervals
-(define auth-timeout 3600)
-(define auth-refresh 600)
+(define auth-expiration (* 3600 24 17))
+(define auth-refresh #f)
 (varconfig! auth:expires auth-expiration)
 (varconfig! auth:refresh auth-refresh)
 
-;; Session functions
-(define sessionfn #f)
-(varconfig! auth:sessionfn sessionfn)
-
 ;; Cookie parameters
+(define auth-cookie-expires #f) ;; if #f, use a session cookie
 (define auth-cookie-domain #f)
 (define auth-cookie-path "/")
 (define auth-secure #f)
@@ -37,235 +77,148 @@
 (varconfig! auth:sitepath auth-cookie-path)
 (varconfig! auth:secure auth-secure)
 
-;;; Here's the authorization model:
-;;;  There's one cookie/cgi variable (the AUTHID) indicating authorization
-;;;  Parsed authorization consists of a vector with four elements:
-;;;    the AUTHID
-;;;    the USER
-;;;    a SESSID
-;;;    an expiration timestamp
-;;;    a refresh timestamp
+;; The blacklist
+(define blacklist (make-hashset))
 
-;;; When the AUTHID is a string (how it comes from the web), it is
-;;; unpacked into a parsed authorization.  This authorization process
-;;; optionally decrypts and checks a signature before using it.  The parsed
-;;; form is then cached
-;;; An authorization is valid until its authorization timestamp
-;;;  is passed and the authorization functions automatically extend
-;;;  authorizations older than the refresh timestamp, so an authorization
-;;;  will only be invalidated if there are no transactions between the
-;;;  refresh 
+(define (blacklisted? string)
+  (or (not string) (hashset-get blacklist string)))
+(defslambda (blacklist! string) (hashset-add! blacklist string))
+
+;;; Cookie functions
+
+(define (expire-cookie! cookievar)
+  (set-cookie! cookievar "expired"
+	       auth-cookie-domain auth-cookie-path
+	       (timestamp+ (- (* 7 24 3600)))
+	       auth-secure))
+
+;;; AUTHINFO
+
+(defrecord authinfo realm identity expires)
+
+(define (auth->string auth)
+  (let* ((vec (vector (authinfo-realm auth) (authinfo-identity auth)
+		      (authinfo-expires auth)))
+	 (packet (dtype->packet vec))
+	 (sig (hmac-sha1 packet signature)))
+    (stringout (packet->base64 packet) "|" (packet->base64 sig))))
+
+(define (string->auth authstring (authid authid))
+  (let* ((sigsplit (position #\| authstring))
+	 (payload (and sigsplit (base64->packet (subseq authstring 0 sigsplit))))
+	 (sig (and sigsplit (base64->packet (subseq authstring (1+ sigsplit)))))
+	 (unpacked (and payload (packet->dtype payload))))
+    (unless (equal? sig (hmac-sha1 payload signature))
+      (logwarn "Invalid signature in " authid " authstring " (write authstring)
+	       "\n\tfor " (subseq authstring 0 sigsplit)
+	       "\n\tbased on " signature
+	       "\n\texpecting " sig
+	       "\n\tgetting " (hmac-sha1 payload signature)))
+    (and sig unpacked (equal? sig (hmac-sha1 payload signature))
+	 (eq? (elt unpacked 0) authid)
+	 (cons-authinfo (elt unpacked 0) (elt unpacked 1) (elt unpacked 2)))))
+
+(define (unpack-authinfo authstring (checksig #f))
+  (let* ((sigsplit (position #\| authstring))
+	 (payload (and sigsplit (base64->packet (subseq authstring 0 sigsplit))))
+	 (sig (and sigsplit (base64->packet (subseq authstring (1+ sigsplit)))))
+	 (unpacked (and payload (packet->dtype payload))))
+    (unless (equal? sig (hmac-sha1 payload signature))
+      (logwarn "Invalid signature in " authid " authstring " (write authstring)
+	       "\n\tfor " (subseq authstring 0 sigsplit)
+	       "\n\tbased on " signature
+	       "\n\texpecting " sig
+	       "\n\tgetting " (hmac-sha1 payload signature)))
+    unpacked))
+
+;;; Core functions
+
+(define (auth/identify! identity (duration auth-expiration))
+  (let ((auth (cons-authinfo authid identity (+ (time) duration))))
+    (cgiset! authid auth)
+    (set-cookie! authid (auth->string auth)
+		 auth-cookie-domain auth-cookie-path
+		 (if auth-cookie-expires (timestamp+ auth-cookie-expires) #f)
+		 auth-secure)
+    identity))
+
+(define (auth/ok? auth)
+  (and auth (> (authinfo-expires auth) (time))
+       (not (blacklisted? (auth->string auth)))
+       auth))
+
+(define (auth/refresh! info (authid) (duration auth-expiration))
+  (default! authid (authinfo-realm info))
+  (info%watch "AUTH/REFRESH!" info authid duration)
+  (if (auth/ok? info authid)
+      (let ((identity (authinfo-identity info))
+	    (auth (cons-authinfo (authinfo-realm info) identity (+ (time) duration))))
+	(cgiset! authid auth)
+	(set-cookie! authid (auth->string auth)
+		     auth-cookie-domain auth-cookie-path
+		     (if auth-cookie-expires (timestamp+ auth-cookie-expires) #f)
+		     auth-secure)
+	(blacklist! (auth->string auth))
+	auth)
+      (begin (cgidrop! authid)
+	     (expire-cookie! authid)
+	     (error "Can't refresh an invalid authorization" info))))
+
+;;; Checking authorization
+
+(define (auth/getinfo (authid authid) (authinfo))
+  (default! authinfo (cgiget authid))
+  (debug%watch "AUTH/GETINFO" authid authinfo (cgiget authid))
+  (cond ((fail? authinfo) (fail))
+	;; If the info is a string, we haven't verified it yet
+	;; This is where the real validation happens
+	((not authinfo)
+	 (error "Invalid authorization info" authinfo authid)
+	 (fail))
+	((string? authinfo)
+	 (auth/getinfo authid (string->auth authinfo authid)))
+	;; Check if the info is a valid vector
+	((not (authinfo? authinfo))
+	 (error "Invalid authorization info" authinfo authid)
+	 (expire-cookie! authid)
+	 (cgidrop! authid)
+	 (fail))
+	((not (auth/ok? authinfo))
+	 (logwarn "Expired authorization " authinfo authid)
+	 (expire-cookie! authid)
+	 (cgidrop! authid)
+	 (fail))
+	((< (- (authinfo-expires authinfo) (time))
+	    (if (inexact? auth-refresh)
+		(* auth-expires auth-refresh)
+		auth-refresh))
+	 (auth/refresh! authinfo))
+	(else authinfo)))
 
 ;;; Top level functions
 
-(define (auth/getinfo (authid authid) (authinfo))
-  (default! authinfo (cgiget authid #f))
-  (debug%watch "AUTH/GETINFO" authid authinfo)
-  (cond ((not authinfo) #f)
-	;; If the info is a string, we haven't verified it yet
-	;; This is where the real validation happens
-	((string? authinfo)
-	 (auth/getinfo authid (checkinfo authinfo authid)))
-	;; Check if the info is a valid vector
-	((not (and (vector? authinfo)
-		   (= (length authinfo) 5)
-		   (eq? authid (elt authinfo 0))
-		   (integer? (elt authinfo 3))
-		   (integer? (elt authinfo 4))))
-	 (error "Bad authorization info" authinfo authid)
-	 #f)
-	(else authinfo)))
-
 (define (auth/getuser (authid authid))
-  (elt (auth/getinfo authid (cgiget authid)) 2))
-
-;;; Unpacks an authstring into a parsed representation
-;;;  and updates it if neccesary
-(define (checkinfo authstring authid)
-  (let* ((sigsplit (position #\| authstring))
-	 (payload (if sigsplit
-		      (base64->packet (subseq authstring 0 sigsplit))
-		      (base64->packet authstring)))
-	 (packet (if encrypt (decrypt payload encrypt) payload))
-	 (sigstring (and sigsplit (subseq sigsplit (1+ sigsplit))))
-	 (sigok (or (and (not signature) (not sigstring))
-		    (and sigstring signature
-			 (equal? (base64->packet sigstring)
-				 (hmac-sha1 payload signature)))))
-	 (parsed (packet->dtype packet))
-	 (now (time)))
-    (debug%watch "CHECKINFO" authid authstring sigok parsed (time))
-    (cond ((not sigok)
-	   (bad-signature! parsed authstring authid))
-	  ((not (and (vector? parsed) (= (length parsed) 5)
-		     (eq? (elt parsed 0) authid)
-		     (integer? (elt parsed 3))
-		     (integer? (elt parsed 4))))
-	   (bad-authinfo! parsed authstring authid))
-	  (else
-	   (if (> now (elt parsed 4))
-	       (auth-expired! parsed authstring authid)
-	       (if (> now (elt parsed 3))
-		   (update-authinfo! parsed)
-		   (begin (cgiset! authid parsed)
-			  parsed)))))))
-
-(define (update-authinfo! authinfo)
-  (let* ((authid (elt authinfo 0))
-	 (user (elt authinfo 1))
-	 (cursession (elt authinfo 2))
-	 (session (if sessionfn
-		      (sessionfn user cursession #t)
-		      user))
-	 (expires (elt authinfo 3)))
-    (and session
-	 (let* ((now (time))
-		(authvec (vector authid user session
-				 (if sessionfn (+ now auth-timeout)
-				     expires)
-				 (+ now auth-refresh)))
-		(authdata (if encrypt
-			      (encrypt (dtype->packet authvec) encrypt)
-			      (dtype->packet authvec)))
-		(sig (and signature (hmac-sha1 authdata signature)))
-		(authstring
-		 (if sig (stringout (packet->base64 authdata) "|"
-				    (packet->base64 sig))
-		     (packet->base64 authdata))))
-	   (cgiset! authid authvec)
-	   (set-cookie! authid authstring
-			auth-cookie-domain auth-cookie-path #f
-			auth-secure)
-	   authvec))))
-
-;;;; Error procedures
-
-(define (bad-signature! info authstring authid)
-  (logwarn "Bad signature for " info " from " authid "=" authstring)
-  (clear-cookie! authid)
-  #f)
-(define (no-signature! authstring authid)
-  (logwarn "No signature in " authid "=" authstring)
-  (clear-cookie! authid)
-  #f)
-(define (bad-authinfo! info authstring authid)
-  (logwarn "Malformed authinfo " info " from " authid "=" authstring)
-  (clear-cookie! authid)
-  #f)
-(define (auth-expired! info authstring authid)
-  (loginfo "Expired authinfo " info " from " authid "=" authstring)
-  (clear-cookie! authid)
-  #f)
-
-(define (clear-cookie! id)
-  (set-cookie! id "expired"
-	       auth-cookie-domain auth-cookie-path
-	       (timestamp+ (* 3600 -24))))
+  (try (authinfo-identity (auth/getinfo authid)) #f))
 
 ;;;; Authorize/deauthorize API
 
-(define (auth/authorize! user (authid authid))
-  (let ((now (time)))
-    (update-authinfo! (vector authid user #f
-			      (+ now auth-timeout)
-			      (+ now auth-refresh)))))
-
-(define (auth/deauthorize! (authid authid))
-  (let ((info (if (vector? authid) authid (cgiget authid))))
-    (when (vector? info)
-      (when sessionfn (sessionfn (elt info 1) (elt info 2) #f))
-      (cgidrop! (first info))
-      (set-cookie! (first info) "expired"
-		   auth-cookie-domain auth-cookie-path
-		   (timestamp+ (* 3600 -24))
-		   auth-secure))
-    (when (string? info)
-      (set-cookie! authid "expired"
-		   auth-cookie-domain auth-cookie-path
-		   (timestamp+ (* 3600 -24))
-		   auth-secure))
-    #f))
-
-;;; Simple sessions
-
-;;; These just use a random number as a sessionid and keeps a table
-;;;  of the valid session for each user.
-
-(define sessions-file #f)
-(define sessions #f)
-
-(define (auth/ezsession user session valid)
-  (if valid
-      (if session
-	  (and (test sessions user session) session)
-	  (let ((session (random 6000000)))
-	    (store! sessions user session)
-	    (when session-file (save-ezsessions))
-	    session))
-      (drop! sessions user)))
-
-(defslambda (save-ezsessions)
-  (when session-file (dtype->file sessions session-file)))
-
-(config-def! 'auth:ezsessions
-	     (lambda (var (val))
-	       (if (bound? val)
-		   (cond ((string? val)
-			  (set! sessions
-				(if (file-exists? val)
-				    (file->dtype val)
-				    (make-hashtable)))
-			  (set! sessions-file val)
-			  (set! sessionfn auth/ezsession))
-			 ((not val)
-			  (set! sessions #f)
-			  (set! sessions-file #f)
-			  (set! sessionfn #f))
-			 (else
-			  (set! sessions val)
-			  (set! sessionfn auth/ezsession)))
-		   sessions)))
-
-;;; Validation
-
-;; These are methods which handle respones from various authorization
-;;  sites (OpenID, Facebook, etc)
-;; When a validator (a thunk) returns non-empty/non-false, it
-;;  is taken to indicate that the user has been authorized.
-(define validators '())
-;; This takes the results of validation and makes a user
-;;  who can then be authorized
-(define getuser #f)
-(varconfig! auth:getuser getuser)
-
-(define (auth/validate (authid authid))
-  (debug%watch "AUTH/VALIDATE" authid validators)
-  (or (auth/getinfo authid)
-      (do ((scan validators (cdr scan))
-	   (user #f))
-	  ((or user (null? scan))
-	   (when user
-	     (if getuser
-		 (auth/authorize! (getuser user) authid)
-		 (auth/authorize! user authid)))
-	   user)
-	(set! user (try (if (pair? (car scan))
-			    ((cdr (car scan)))
-			    ((car scan)))
-			#f))
-	(message "Validating " (car scan) " yielded " user))))
-
-(ruleconfig! auth:validate validators)
-
-;;; Logging in
-
-(define (auth/login uri (authid authid))
-  (or (auth/getinfo authid)
-      (let ((valid (auth/valiate authid)))
-	(cond ((and valid (cgitest 'next))
-	       (cgiset! 'status 303)
-	       (httpheader "Location: " (cgiget 'next))
-	       #f)
-	      (else valid)))))
-
-
+(define (auth/deauthorize! (authid authid) (info))
+  ;; Get the arguments sorted
+  (cond ((authinfo? authid)
+	 (set! info authid)
+	 (set! authid (authinfo-realm info)))
+	(else (default! info (cgiget authid))))
+  (cond ((string? info)
+	 ;; Unpack the string to check its expiration
+	 (let ((parsed (unpack-authinfo info)))
+	   ;; If it hasn't expired, blacklist it
+	   (unless (> (time) (elt parsed 2)) (blacklist! info))))
+	((not (authinfo? info))
+	 (logwarn "Can't deauthorize invalid info" info))
+	;; Don't bother
+	((auth/expired? info))
+	(else
+	 (let ((string (auth->string info)))
+	   (unless (hashset-get blacklist string)
+	     (blacklist! string)))))
+  (expire-cookie! authid))
