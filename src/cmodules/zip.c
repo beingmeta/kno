4e67809224f6c6e96e9d0e9b09fc2a3b2259ee44
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2010-03-13 17:12:30 +0000

    Fixed bugs and filled out libzip implementation

--- src/cmodules/zip.c
+++ src/cmodules/zip.c
@@ -17,25 +17,43 @@ static char versionid[] =
 #include "fdb/numbers.h"
 
 #include <libu8/libu8io.h>
+#include <libu8/u8pathfns.h>
 
 #include <sys/types.h>
 #include <zip.h>
 
-static fdtype znumerr(u8_context cxt,int zerrno,fdtype irritant)
+static fd_exception ZipFileError=_("Zip file error");
+
+FD_EXPORT fd_ptr_type fd_zipfile_type;
+fd_ptr_type fd_zipfile_type;
+typedef struct FD_ZIPFILE {
+  FD_CONS_HEADER;
+  u8_string filename; int flags;
+  u8_mutex lock; int closed;
+  struct zip *zip;} FD_ZIPFILE;
+typedef struct FD_ZIPFILE *fd_zipfile;
+
+/* Error messages */
+
+static fdtype znumerr(u8_context cxt,int zerrno,u8_string path)
 {
   u8_byte buf[1024];
   zip_error_to_str(buf,1024,errno,zerrno);
-  return fd_err(ZipFileError,cxt,u8_strdup(buf),irritant);
+  return fd_err(ZipFileError,cxt,
+		u8_mkstring("(%s) %s",path,buf),
+		FD_VOID);
 }
 
 static fdtype ziperr(u8_context cxt,fd_zipfile zf,fdtype irritant)
 {
-  u8_string details=u8_mkstring("(%s) %s",zf->filename,zip_strerror(zf));
-  return fd_err(ZipFileError,cxt,details,irritant);
+  u8_string details=
+    u8_mkstring("(%s) %s",zf->filename,zip_strerror(zf->zip));
+  fd_seterr(ZipFileError,cxt,details,fd_incref(irritant));
+  return FD_ERROR_VALUE;
 }
 
-static fdtype zfilerr(u8_context cxt,fd_zipfile zf,struct zipfile *zfile,
-			 fdtype irritant)
+static fdtype zfilerr(u8_context cxt,fd_zipfile zf,struct zip_file *zfile,
+		      fdtype irritant)
 {
   u8_string details=
     u8_mkstring("(%s) %s",zf->filename,zip_file_strerror(zfile));
@@ -43,21 +61,13 @@ static fdtype zfilerr(u8_context cxt,fd_zipfile zf,struct zipfile *zfile,
   return fd_err(ZipFileError,cxt,details,irritant);
 }
 
-/* Creating zip files */
-
-FD_EXPORT fd_ptr_type fd_zipfile_type;
-fd_ptr_type fd_zipfile_type;
-typedef struct FD_ZIPFILE {
-  FD_CONS_HEADER;
-  u8_string filename; int flags;
-  u8_mutex lock; struct zip *zip;} FD_ZIPFILE;
-typedef struct FD_ZIPFILE *fd_zipfile;
-static fd_exception ZipFileError=_("Zip file error");
+/* Zip file utilities */
 
 static void recycle_zipfile(struct FD_CONS *c)
 {
   struct FD_ZIPFILE *zf=(struct FD_ZIPFILE *)c;
-  zip_close(zf->zip);
+  if (!(zf->closed)) zip_close(zf->zip);
+  zf->closed=1;
   u8_destroy_mutex(&(zf->lock));
   u8_free(zf);
 }
@@ -65,100 +75,163 @@ static void recycle_zipfile(struct FD_CONS *c)
 static int unparse_zipfile(struct U8_OUTPUT *out,fdtype x)
 {
   struct FD_ZIPFILE *zf=(struct FD_ZIPFILE *)x;
-  u8_printf(out,"#<ZIPFILE '%s'>",zf->filename);
+  u8_printf(out,"#<ZIPFILE '%s'%s>",
+	    zf->filename,((zf->closed)?(" closed"):""));
   return 1;
 }
-static fdtype zipopen(u8_string abspath,int zflags)
+
+static fdtype zipreopen(struct FD_ZIPFILE *zf,int locked)
+{
+  if (!(zf->closed)) return FD_FALSE;
+  else {
+    int errflag;
+    struct zip *zip;
+    if (!(locked)) u8_lock_mutex(&(zf->lock));
+    if (!(zf->closed)) {
+      if (!(locked)) u8_unlock_mutex(&(zf->lock));
+      return FD_FALSE;}
+    else zip=zip_open(zf->filename,zf->flags,&errflag);
+    if (!(zip)) {
+      fdtype errval=znumerr("zipreopen",errflag,zf->filename);
+      if (!(locked)) u8_unlock_mutex(&(zf->lock));
+      return errval;}
+    else {
+      zf->zip=zip; zf->closed=0;
+      if (!(locked)) u8_unlock_mutex(&(zf->lock));
+      return FD_TRUE;}}
+}
+
+/* Creating/opening zip files */
+
+static fdtype zipopen(u8_string path,int zflags)
 {
   int errflag=0;
+  u8_string abspath=u8_abspath(path,NULL);
   struct zip *zip=zip_open(abspath,zflags,&errflag);
   if (zip) {
     struct FD_ZIPFILE *zf=u8_alloc(struct FD_ZIPFILE);
     FD_INIT_FRESH_CONS(zf,fd_zipfile_type);
     u8_init_mutex(&(zf->lock));
-    zf->filename=abspath; zf->flags=zflags;
+    zf->filename=abspath; zf->flags=zflags; zf->closed=0;
     zf->zip=zip;
     return FDTYPE_CONS(zf);}
   else {
-    u8_free(abspath);
-    return znumerr("open_zipfile",errflag,filename);}
+    return znumerr("open_zipfile",errflag,abspath);}
 }
-static fdtype zipopen_prim(fdtype filename)
+static fdtype zipopen_prim(fdtype filename,fdtype create)
 {
-  return zipopen(FD_STRDATA(filename),ZIP_CHECKCONS);
+  if ((FD_FALSEP(create))||(FD_VOIDP(create)))
+    return zipopen(FD_STRDATA(filename),ZIP_CHECKCONS);
+  else return zipopen(FD_STRDATA(filename),ZIP_CREATE|ZIP_CHECKCONS);
 }
 static fdtype zipmake_prim(fdtype filename)
 {
   return zipopen(FD_STRDATA(filename),ZIP_CREATE|ZIP_EXCL);
 }
-static fdtype zipuse_prim(fdtype filename)
-{
-  return zipopen(FD_STRDATA(filename),ZIP_CREATE);
-}
 
 static fdtype close_zipfile(fdtype zipfile)
 {
   struct FD_ZIPFILE *zf=FD_GET_CONS(zipfile,fd_zipfile_type,fd_zipfile);
-  int retval=zip_close(zf->zip);
-  if (retval) return zipmerr("close_zipfile",zf,zipfile);
-  else return FD_TRUE;
+  int retval;
+  u8_lock_mutex(&(zf->lock));
+  if (zf->closed) {
+    u8_unlock_mutex(&(zf->lock));
+    return FD_FALSE;}
+  else retval=zip_close(zf->zip);
+  if (retval) {
+    u8_unlock_mutex(&(zf->lock));
+    return ziperr("close_zipfile",zf,zipfile);}
+  else {
+    zf->closed=1;
+    u8_unlock_mutex(&(zf->lock));
+    return FD_TRUE;}
 }
 
 /* Adding to zip files */
-
-#if 0
-static fdtype zipcheckfile(struct zip *z,u8_string name)
-{
-  u8_string buf[1024];
-  u8_byte *start=((name[0]=='/')?(name+1):(name));
-  u8_byte sep=strchr(start,'/');
-  while (sep) {
-    strncpy(buf,start,sep-start); buf[sep-start]='\0';
-    
-  }    
-}
-#endif
 
 static fdtype zipadd
   (struct FD_ZIPFILE *zf,u8_string name,struct zip_source *zsource)
 {
   int index=zip_name_locate(zf->zip,name,0);
-  int retval;
-  if (index<0) retval=zip_add(zf->zip,name,zsource);
-  else retval=zip_replace(zf->zip,index,zsource);
-  if (retval<0) {
-    zip_source_free(zsource);
-    return ziperr("zipadd",zf,(fdtype)zf);}
-  else {
-    zip_source_free(zsource);
-    return FD_VOID;}
+  int retval=((index<0)?(zip_add(zf->zip,name,zsource)):
+	      (zip_replace(zf->zip,index,zsource)));
+  if (retval<0)
+    return ziperr("zipadd",zf,(fdtype)zf);
+  else if (index<0) return FD_TRUE;
+  else return FD_FALSE;
 }
 
 static fdtype zipadd_prim(fdtype zipfile,fdtype filename,fdtype value)
 {
   struct FD_ZIPFILE *zf=FD_GET_CONS(zipfile,fd_zipfile_type,fd_zipfile);
-  struct zip_source *zsource=
-    ((FD_STRINGP(value))?
-     (zip_source_buffer(zf,FD_STRDATA(value),FD_STRLEN(value))):
-     (FD_PACKETP(value))?
-     (zip_source_buffer(zf,FD_PACKET_DATA(value),FD_PACKET_LENGTH(value))):
-     (NULL));
-  if (zsource)
-    return zipadd(zipfile,FD_STRDATA(filename),zsource);
+  unsigned char *data=NULL; size_t datalen=0;
+  struct zip_source *zsource;
+  if (FD_STRINGP(value)) {
+    data=u8_strdup(FD_STRDATA(value));
+    datalen=FD_STRLEN(value);}
+  else if (FD_PACKETP(value)) {
+    datalen=FD_PACKET_LENGTH(value);
+    data=u8_malloc(datalen);
+    memcpy(data,FD_PACKET_DATA(value),datalen);}
   else return fd_type_error("zip source","zipadd_prim",value);
+  u8_lock_mutex(&(zf->lock));
+  if (zf->closed) {
+    fdtype errval=zipreopen(zf,1);
+    if (FD_ABORTP(errval)) {
+      u8_unlock_mutex(&(zf->lock));
+      return errval;}}
+  zsource=zip_source_buffer(zf->zip,data,datalen,1);
+  if (zsource) {
+    fdtype v=zipadd(zf,FD_STRDATA(filename),zsource);
+    u8_unlock_mutex(&(zf->lock));
+    return v;}
+  else return ziperr("zipadd/source",zf,value);
+}
+
+static fdtype zipdrop_prim(fdtype zipfile,fdtype filename)
+{
+  struct FD_ZIPFILE *zf=FD_GET_CONS(zipfile,fd_zipfile_type,fd_zipfile);
+  int index; int retval;
+  u8_lock_mutex(&(zf->lock));
+  if (zf->closed) {
+    fdtype errval=zipreopen(zf,1);
+    if (FD_ABORTP(errval)) {
+      u8_unlock_mutex(&(zf->lock));
+      return errval;}}
+  index=zip_name_locate(zf->zip,FD_STRDATA(filename),0);
+  if (index<0) {
+    u8_unlock_mutex(&(zf->lock));
+    return FD_FALSE;}
+  else retval=zip_delete(zf->zip,index);
+  if (retval<0) {
+    u8_unlock_mutex(&(zf->lock));
+    return ziperr("zipdrop",zf,(fdtype)zf);}
+  else {
+    u8_unlock_mutex(&(zf->lock));
+    return FD_TRUE;}
 }
 
 static fdtype zipget_prim(fdtype zipfile,fdtype filename,fdtype stringp)
 {
   struct FD_ZIPFILE *zf=FD_GET_CONS(zipfile,fd_zipfile_type,fd_zipfile);
   u8_string fname=FD_STRDATA(filename);
-  int index=zip_name_locate(zf->zip,fname,0);
   struct zip_stat zstat; int zret;
-  if (index<0) return FD_FALSE;
-  else if (zret=zip_stat(zf->zip,FD_STRDATA(filename),0,&zstat))
-    return ziperr("zipget_prim/stat",zf,filename);
-  else {
-    struct zip_file *zfile=zip_fopen(zf->zip,FD_STRDATA(filename),0);
+  struct zip_file *zfile;
+  int index;
+  u8_lock_mutex(&(zf->lock));
+  if (zf->closed) {
+    fdtype errval=zipreopen(zf,1);
+    if (FD_ABORTP(errval)) {
+      u8_unlock_mutex(&(zf->lock));
+      return errval;}}
+  index=zip_name_locate(zf->zip,fname,0);
+  if (index<0) {
+    u8_unlock_mutex(&(zf->lock));
+    return FD_FALSE;}
+  else if (zret=zip_stat(zf->zip,FD_STRDATA(filename),0,&zstat)) {
+    u8_unlock_mutex(&(zf->lock));
+    return ziperr("zipget_prim/stat",zf,filename);}
+  else if (zfile=zip_fopen(zf->zip,FD_STRDATA(filename),0)) {
     unsigned char *buf=u8_malloc(zstat.size+1);
     int size=zstat.size, block=0, read=0, togo=size;
     while ((togo>0)&&((block=zip_fread(zfile,buf+read,togo))>0)) {
@@ -166,11 +239,16 @@ static fdtype zipget_prim(fdtype zipfile,fdtype filename,fdtype stringp)
       read=read+block; togo=togo-block;}
     if (togo>0) {
       u8_free(buf);
+      u8_unlock_mutex(&(zf->lock));
       return zfilerr("zipget_prim",zf,zfile,filename);}
-    zip_fclose(zipfile);
+    zip_fclose(zfile);
+    u8_unlock_mutex(&(zf->lock));
     if (FD_TRUEP(stringp))
       return fd_init_packet(NULL,size,buf);
     else return fd_init_string(NULL,size,buf);}
+  else {
+    u8_unlock_mutex(&(zf->lock));
+    return ziperr("zipget_prim/fopen",zf,filename);}
 }
 
 /* Initialization */
@@ -193,16 +271,25 @@ FD_EXPORT int fd_init_zip()
   fd_recyclers[fd_zipfile_type]=recycle_zipfile;
 
   fd_idefn(zip_module,
-	   fd_make_cprim1x("ZIP/OPEN",zipopen_prim,1,fd_string_type,FD_VOID));
+	   fd_make_cprim2x("ZIP/OPEN",zipopen_prim,1,
+			   fd_string_type,FD_VOID,-1,FD_FALSE));
   fd_idefn(zip_module,
 	   fd_make_cprim1x("ZIP/MAKE",zipmake_prim,1,fd_string_type,FD_VOID));
+
   fd_idefn(zip_module,
-	   fd_make_cprim1x("ZIP/USE",zipuse_prim,1,fd_string_type,FD_VOID));
+	   fd_make_cprim1x
+	   ("ZIP/CLOSE",close_zipfile,1,fd_zipfile_type,FD_VOID));
+
   fd_idefn(zip_module,
 	   fd_make_cprim3x("ZIP/ADD!",zipadd_prim,3,
 			   fd_zipfile_type,FD_VOID,
 			   fd_string_type,FD_VOID,
 			   -1,FD_VOID));
+
+  fd_idefn(zip_module,
+	   fd_make_cprim2x("ZIP/DROP!",zipdrop_prim,2,
+			   fd_zipfile_type,FD_VOID,
+			   fd_string_type,FD_VOID));
   fd_idefn(zip_module,
 	   fd_make_cprim3x("ZIP/GET",zipget_prim,2,
 			   fd_zipfile_type,FD_VOID,
