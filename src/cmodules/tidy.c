955bf95e2ccfaa966ede62aa1c6f37726c4bbec5
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2010-08-30 20:24:45 +0000

    Added access to TIDY library

---
+++ src/cmodules/tidy.c
@@ -0,0 +1,105 @@
+/* -*- Mode: C; -*- */
+
+/* Copyright (C) 2007-2010 beingmeta, inc.
+   This file is part of beingmeta's FDB platform and is copyright 
+   and a valuable trade secret of beingmeta, inc.
+*/
+
+static char versionid[] =
+  "$Id: crypto.c 4774 2010-01-15 14:43:07Z haase $";
+
+#include "fdb/dtype.h"
+#include "fdb/eval.h"
+
+#if HAVE_TIDY_H
+#include <tidy.h>
+#include <buffio.h>
+#include <tidyenum.h>
+#elif HAVE_TIDY_TIDY_H
+#include <tidy/tidy.h>
+#include <tidy/buffio.h>
+#include <tidy/tidyenum.h>
+#else
+#define UNTIDY_HEADERS 1
+#endif
+
+#ifndef UNTIDY_HEADERS
+FD_EXPORT int fd_init_tidy(void) FD_LIBINIT_FN;
+fd_exception fd_TidyError=_("Tidy Error");
+static int tidy_init=0;
+
+static fdtype tidy_prim(fdtype string,fdtype opts,fdtype diag)
+{
+  fdtype result=FD_VOID;
+  TidyBuffer outbuf={NULL};
+  TidyBuffer errbuf={NULL};
+  int rc=-1;
+  TidyDoc tdoc=tidyCreate();
+  Bool ok=tidyOptSetBool(tdoc,TidyXhtmlOut,yes);
+  if (ok) {
+    tidyBufInit(&outbuf);
+    tidyBufInit(&errbuf);
+    rc=tidySetErrorBuffer(tdoc,&errbuf);}
+  if (rc<0) {
+    tidyRelease(tdoc);
+    return fd_err(fd_TidyError,"tidy_prim/init",NULL,FD_VOID);}
+  if (FD_VOIDP(opts)) {
+    if (rc>=0) rc=tidyOptSetValue(tdoc,TidyCharEncoding,"utf8");
+    if (rc>=0) rc=tidyOptSetInt(tdoc,TidyDoctypeMode,TidyDoctypeOmit);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyShowWarnings,no);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyQuiet,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMakeBare,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMakeClean,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyDropEmptyParas,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyFixComments,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyXmlSpace,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyEncloseBodyText,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyEncloseBlockText,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyWord2000,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMark,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyJoinClasses,yes);
+    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyNCR,yes);}
+  else {}
+  if (rc<0) result=fd_err(fd_TidyError,"tidy_prim/setopts",errbuf.bp,FD_VOID);
+  rc=tidyParseString(tdoc,FD_STRDATA(string));
+  if ((FD_VOIDP(result))&&(rc<0))
+    result=fd_err(fd_TidyError,"tidy_prim/parse",errbuf.bp,FD_VOID);
+  if (rc>=0) rc=tidyCleanAndRepair(tdoc);
+  if ((FD_VOIDP(result))&&(rc<0))
+    result=fd_err(fd_TidyError,"tidy_prim/clean",errbuf.bp,FD_VOID);
+  if (FD_APPLICABLEP(diag)) {
+    if (rc>=0) rc=tidyRunDiagnostics(tdoc);
+    if ((FD_VOIDP(result))&&(rc<0))
+      result=fd_err(fd_TidyError,"tidy_prim/diag",errbuf.bp,FD_VOID);
+    if (rc>=0) rc=((tidyOptSetBool(tdoc,TidyForceOutput,yes))?(rc):(-1));
+    if ((FD_VOIDP(result))&&(rc<0))
+      result=fd_err(fd_TidyError,"tidy_prim/forceout",errbuf.bp,FD_VOID);}
+  if (rc>=0) rc=tidySaveBuffer(tdoc,&outbuf);
+  if ((FD_VOIDP(result))&&(rc<0))
+    result=fd_err(fd_TidyError,"tidy_prim/save",errbuf.bp,FD_VOID);
+  else if (FD_VOIDP(result))
+    result=fdtype_string(outbuf.bp);
+  else {}
+  tidyBufFree(&outbuf);
+  tidyBufFree(&errbuf);
+  tidyRelease(tdoc);
+  return result;
+}
+
+FD_EXPORT int fd_init_tidy()
+{
+  fdtype tidy_module;
+  if (tidy_init) return;
+  fd_register_source_file(versionid);
+  tidy_init=1;
+  tidy_module=fd_new_module("TIDY",(FD_MODULE_SAFE));
+
+  fd_idefn(tidy_module,
+	   fd_make_cprim3x("TIDY->XHTML",tidy_prim,1,
+			   fd_string_type,FD_VOID,-1,FD_VOID,
+			   -1,FD_VOID));
+
+  fd_finish_module(tidy_module);
+  fd_persist_module(tidy_module);
+}
+#endif /* ndef UNTIDY_HEADERS */
