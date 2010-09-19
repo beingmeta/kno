297ba7dde6cd9025459e6a5eda172ac08782a21a
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2010-09-19 22:16:50 +0000

    Added couchdb module

---
+++ src/modules/couchdb.scm
@@ -0,0 +1,148 @@
+;;; -*- Mode: Scheme; character-encoding: utf-8; -*-
+;;; Copyright (C) 2010 beingmeta, inc. All rights reserved
+
+(in-module 'couchdb)
+
+(define version "$Id: glosses.scm 5530 2010-09-13 16:53:19Z haase $")
+(define revision "$Revision: 5530 $")
+
+(use-module '{fdweb ezrecords extoids})
+(use-module '{texttools logger})
+
+(module-export! '{checkgloss newgloss getgloss glossdb/moveid})
+
+(define %loglevel %notice!)
+
+(defrecord couchdb url (map #[]) (views (make-hashtable)))
+(defrecord couchview db design view path)
+
+(define-init couchdbs (make-hashtable))
+
+(define (couchdb url)
+  (try (get couchdbs url) (new-couchdb url)))
+(module-export! '{couchdb couchdb? couchdb-url couchdb-map})
+
+(defslambda (new-couchdb url)
+  (try (get couchdbs url)
+       (let ((new (cons-couchdb url #[])))
+	 (store! couchdbs url new)
+	 new)))
+
+(define (couchdb/add-map! db . args)
+  (let* ((db (if (string? db) (couchdb db) db))
+	 (map (couchdb-map db)))
+    (when (odd? (length args))
+      (do-choices (key (getkeys (car args)))
+	(store! map key (get (car args) key)))
+      (set! args (cdr args)))
+    (do ((scan args (cddr scan)))
+	((null? scan))
+      (store! map (car scan) (cadr scan)))))
+(module-export! 'couchdb/add-map!)
+
+(define (couchdb/req db path (options #f) . args)
+  (let* ((path (mkpath (couchdb-url db) path))
+	 (call (if (null? args) path (apply scripturl path args))))
+    (jsonparse (get (urlget call  options) '%content)
+	       40 (couchdb-map db))))
+
+(define (couchdb/get db id (options #f))
+  (couchdb/req db (if (oid? id)
+		      (stringout "@" (number->string (oid-addr id) 16))
+		      (if (uuid? id) (uuid->string id)
+			  (if (string? id) id
+			      (stringout ":" id))))
+	       options))
+
+(define (couchdb/save! db value (id))
+  (default! id (get value '_id))
+  (let*  ((idstring (if (oid? id)
+			(stringout "@" (number->string (oid-addr id) 16))
+			(if (uuid? id) (uuid->string id)
+			    (if (string? id) id
+				(stringout ":" id)))))
+	  (r (urlput (mkpath (couchdb-url db) idstring) (->json value)))
+	  (httpcode (get r 'response)))
+    (and (exists? httpcode) (>= httpcode 200) (< httpcode 300))))
+
+(define (couchdb/delete! db id (rev #f))
+  (let ((rev (or rev (get (couchdb/get db id) '_rev))))
+    (couchdb/req db (scripturl (if (oid? id)
+				   (stringout "@" (number->string (oid-addr id) 16))
+				   (if (string? id) id
+				       (stringout ":" id))))
+		 #[METHOD DELETE]
+		 "rev" rev)))
+
+(define (couchdb/store! db id slotid value)
+  (let ((success #f))
+    (until success
+      (let ((cur (couchdb/get db id)))
+	(if (identical? (get cur slotid) value)
+	    (set! success #t)
+	    (begin (store! cur slotid value)
+		   (set! success (couchdb/save! cur))))))))
+(define (couchdb/add! db id slotid value)
+  (let ((success #f))
+    (until success
+      (let ((cur (couchdb/get db id)))
+	(if (test cur slotid value)
+	    (set! success #t)
+	    (begin (add! cur slotid value)
+		   (set! success (couchdb/save! cur))))))))
+(define (couchdb/drop! db id slotid value)
+  (let ((success #f))
+    (until success
+      (let ((cur (couchdb/get db id)))
+	(if (test cur slotid value)
+	    (begin (add! cur slotid value)
+		   (set! success (couchdb/save! cur)))
+	    (set! success #t))))))
+  
+(module-export! '{couchdb/req couchdb/get couchdb/save! couchdb/store! couchdb/add! couchdb/delete! couchdb/drop!})
+
+;;; Getting views
+
+(define (couchdb/view db design view)
+  (if (string? db)
+      (couchdb/view (couchdb db) design view)
+      (try (get (couchdb-views db) (cons design view))
+	   (new-view db design view))))
+    
+(defslambda (new-view db design view)
+  (try (get (couchdb-views db) (cons design view))
+       (let ((new (cons-couchview
+		   db design view
+		   (stringout "_design/" design "/_view/" view))))
+	 (store! (couchdb-views db) (cons design view) new)
+	 new)))
+(module-export! 'couchdb/view)
+
+(define (couchdb/table view (opts #f) (table #f))
+  (let ((response (if opts
+		      (couchdb/req
+		       (couchview-db view)
+		       (scripturl (couchview-path view)
+			 "startkey" (get opts 'start)
+			 "endkey" (get opts 'end)))
+		      (couchdb/req (couchview-db view) (couchview-path view))))
+	(table (or table
+		   (and opts (getopt opts 'output))
+		   (make-hashtable))))
+    (doseq (row (get response 'rows))
+      (add! table (get row 'key) (get row 'value)))
+    table))
+(define (couchdb/list view (opts #f))
+  (let ((response (if opts
+		      (couchdb/req
+		       (couchview-db view)
+		       (scripturl (couchview-path view)
+			 "startkey" (tryif (test opts 'start)
+				      (->json (get opts 'start)))
+			 "endkey" (tryif (test opts 'end)
+				    (->json (get opts 'end)))))
+		      (couchdb/req (couchview-db view) (couchview-path view))))
+	(results {}))
+    (doseq (row (get response 'rows)) (set+! results (get row 'value)))
+    results))
+(module-export! '{couchdb/table couchdb/list})
