8f0a9ec75272946fa32df9683c07b373ea168a35
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2011-02-07 18:09:00 +0000

    Added cache argument to make-extindex

--- src/scheme/dbprims.c
+++ src/scheme/dbprims.c
@@ -426,9 +426,10 @@ static fdtype extpool_setcache(fdtype pool,fdtype oid,fdtype value)
 /* External indices */
 
 static fdtype make_extindex(fdtype label,fdtype fetchfn,fdtype commitfn,
-			    fdtype state)
+			    fdtype state,fdtype usecache)
 {
   fd_index ix=fd_make_extindex(FD_STRDATA(label),fetchfn,commitfn,state);
+  if (FD_FALSEP(usecache)) ix->cache_level=0;
   return fd_index2lisp(ix);
 }
 
@@ -2247,9 +2248,10 @@ FD_EXPORT void fd_init_dbfns_c()
 			   -1,FD_VOID));
 
   fd_idefn(fd_scheme_module,
-	   fd_make_cprim4x("MAKE-EXTINDEX",make_extindex,2,
+	   fd_make_cprim5x("MAKE-EXTINDEX",make_extindex,2,
 			   fd_string_type,FD_VOID,
-			   -1,FD_VOID,-1,FD_VOID,-1,FD_VOID));
+			   -1,FD_VOID,-1,FD_VOID,-1,FD_VOID,
+			   -1,FD_TRUE));
   fd_idefn(fd_scheme_module,
 	   fd_make_cprim2x("EXTINDEX-DECACHE!",extindex_decache,1,
 			   fd_index_type,FD_VOID,-1,FD_VOID));
