1d4903b89976450e2a53e7d3c443638ab8b87eb7
Ken Haase <kh@beingmeta.com>
2011-06-22 09:08:00 -0400

    Updates to oauth 1.0 implementation

--- src/modules/oauth.scm
+++ src/modules/oauth.scm
@@ -10,9 +10,9 @@
 ;;(define %loglevel %debug!)
 ;;(set! %loglevel  %debug!)
 
-(define default-request-endpoint
+(define default-request-uri
   "https://api.twitter.com/oauth/request_token")
-(define default-access-endpoint
+(define default-verify-uri
   "https://api.twitter.com/oauth/access_token")
 (define default-consumer-key
   "eeY4hVUyOSe2IqwhHvNUw")
@@ -26,7 +26,7 @@
     "oauth_signature_method" "HMAC-SHA1"
     "oauth_consumer_key" "GDdmlQH6jhtmLUypg82g"])
 
-(module-export! '{oauth/signature oauth/request})
+(module-export! '{oauth/signature oauth/request oauth/verify oauth/call})
 
 (define (oauth/signature method uri . params)
   (stringout method "&" (uriencode uri) "&"
@@ -50,30 +50,26 @@
 	      (if (string? v) (uriencode v)
 		  (uriencode (stringout v))))))))))
 
-(define (oauth/request service args)
-  (let* ((nonce (getopt args 'nonce (uuid->string (getuuid))))
-	 (endpoint (getopt args 'endpoint
-			   (getopt service 'request_token
-				   default-request-endpoint)))
+(define (oauth/request spec)
+  (let* ((nonce (uuid->string (getuuid)))
+	 (endpoint (getopt spec 'request_uri
+			   default-request-uri))
 	 (callback (uriencode
-		    (getopt args 'callback
-			    (getopt service 'callback
-				    default-callback))))
-	 (ckey (getopt args 'ckey
-		       (getopt service 'ckey default-consumer-key)))
-	 (now (getopt args 'timestamp (time)))
+		    (getopt spec 'callback
+			    default-callback)))
+	 (ckey (getopt spec 'key default-consumer-key))
+	 (now (time))
 	 (sigstring
 	  (oauth/signature
-	   (getopt args 'method "POST") endpoint
+	   (getopt spec 'method "POST") endpoint
 	   "oauth_callback" callback
 	   "oauth_consumer_key" ckey
 	   "oauth_nonce" nonce
 	   "oauth_signature_method" "HMAC-SHA1"
 	   "oauth_timestamp" now
-	   "oauth_version" (getopt service 'version "1.0")))
-	 (sig (hmac-sha1 (stringout (getopt args 'secret
-					    (getopt service 'secret
-						    default-consumer-secret))
+	   "oauth_version" (getopt spec 'version "1.0")))
+	 (sig (hmac-sha1 (stringout
+			   (getopt spec 'secret default-consumer-secret)
 			   "&")
 			 sigstring))
 	 (auth-header
@@ -84,7 +80,79 @@
 	    "oauth_timestamp=\"" now "\", "
 	    "oauth_consumer_key=\"" ckey "\", "
 	    "oauth_signature=\"" (uriencode (packet->base64 sig)) "\", "
-	    "oauth_version=\"1.0\"")))
-    (%watch auth-header)
-    (urlget endpoint (curlopen 'header auth-header 'method 'POST))))
+	    "oauth_version=\"" (getopt spec 'version "1.0") "\""))
+	 (req (urlget endpoint (curlopen 'header auth-header 'method 'POST))))
+    (and (test req 'response 200)
+	 (cons (cgiparse (get req '%content)) spec))))
 
+(define (oauth/verify spec verifier)
+  (let* ((nonce (uuid->string (getuuid)))
+	 (endpoint (getopt spec 'verify_uri default-verify-uri))
+	 (callback (uriencode
+		    (getopt spec 'callback default-callback)))
+	 (ckey (getopt spec 'key default-consumer-key))
+	 (now (time))
+	 (sigstring
+	  (oauth/signature
+	   (getopt spec 'method "POST") endpoint
+	   "oauth_callback" callback
+	   "oauth_consumer_key" ckey
+	   "oauth_token" (getopt spec "oauth_token")
+	   "oauth_verifier" verifier
+	   "oauth_nonce" nonce
+	   "oauth_signature_method" "HMAC-SHA1"
+	   "oauth_timestamp" now
+	   "oauth_version" (getopt spec 'version "1.0")))
+	 (sig (hmac-sha1 (stringout (getopt spec "oauth_token")
+			   (getopt spec 'secret default-consumer-secret)
+			   "&"
+			   (getopt spec "oauth_secret"))
+			 sigstring))
+	 (auth-header
+	  (stringout "Authorization: OAuth "
+	    "oauth_nonce=\"" nonce "\", "
+	    "oauth_token=\"" (getopt spec "oauth_token") "\", "
+	    "oauth_verifier=\"" (getopt spec "oauth_verifier") "\", "
+	    "oauth_signature_method=\"HMAC-SHA1\", "
+	    "oauth_timestamp=\"" now "\", "
+	    "oauth_consumer_key=\"" ckey "\", "
+	    "oauth_signature=\"" (uriencode (packet->base64 sig)) "\", "
+	    "oauth_version=\"" (getopt spec 'version "1.0") "\""))
+	 (req (urlget endpoint (curlopen 'header auth-header 'method 'POST))))
+    (and (test req 'response 200)
+	 (cons (cgiparse (get req '%content)) spec))))
+
+(define (oauth/call spec args (endpoint #f))
+  (let* ((nonce (uuid->string (getuuid)))
+	 (endpoint (or endpoint
+		       (getopt args 'endpoint
+			       (getopt spec 'endpoint
+				       default-endpoint))))
+	 (ckey (getopt spec 'key default-consumer-key))
+	 (now (time))
+	 (sigstring
+	  (oauth/signature
+	   (getopt spec 'method "POST") endpoint
+	   "oauth_consumer_key" ckey
+	   "oauth_token" (getopt spec "oauth_token")
+	   "oauth_nonce" nonce
+	   "oauth_signature_method" "HMAC-SHA1"
+	   "oauth_timestamp" now
+	   "oauth_version" (getopt spec 'version "1.0")
+	   args))
+	 (sig (hmac-sha1 (stringout (getopt spec "oauth_token")
+			   (getopt spec 'secret default-consumer-secret)
+			   "&"
+			   (getopt spec "oauth_secret"))
+			 sigstring))
+	 (auth-header
+	  (stringout "Authorization: OAuth "
+	    "oauth_nonce=\"" nonce "\", "
+	    "oauth_token=\"" (getopt spec "oauth_token") "\", "
+	    "oauth_signature_method=\"HMAC-SHA1\", "
+	    "oauth_timestamp=\"" now "\", "
+	    "oauth_consumer_key=\"" ckey "\", "
+	    "oauth_signature=\"" (uriencode (packet->base64 sig)) "\", "
+	    "oauth_version=\"" (getopt spec 'version "1.0") "\""))
+	 (req (urlget endpoint (curlopen 'header auth-header 'method 'POST))))
+    req))
