1bf70d513f96b748343c2aaf11845029a237c58d
Ken Haase <kh@beingmeta.com>
2012-08-23 19:24:12 +0000

    Minor extensions to logger, mostly synonyms

--- src/safe_modules/logger.scm
+++ src/safe_modules/logger.scm
@@ -12,61 +12,69 @@
  '{logger
    getloglevel %loglevel
    logdetail loginfo lognotice logwarn logdebug
+   logerr logerror logcrit logalert logpanic
    %debug %detail})
 (module-export!
- '{%emergency! %emergency%
+ '{%emergency! %emergency% %panic%
    %alert! %alert%
-   %critical! %danger! %critical% %danger%
+   %critical% %critical! %danger! %danger% %crit% %crit!
    %error! %err! %error% %err%
    %warning! %warn! %warning% %warn%
-   %notice! %notify! %note! %notice% %notify% %note%
-   %informration! %info! %informration% %info%
-   %debug! %detail! %debug% %detail%})
+   %notice% %notice! %notify% %notify! %note% %note!
+   %information% %information! %status% %status! %info% %info!
+   %debug! %debug%
+   %detail% %detail! %details%})
 
 (module-export!
  '{detail%watch debug%watch info%watch notice%watch warn%watch})
 
 (define %nosubst '%loglevel)
 
-(define %emergency! 0)
-(define %alert! 1)
-(define %critical! 2)
-(define %danger! 2)
-(define %error! 3)
-(define %err! 3)
-(define %warning! 4)
-(define %warn! 4)
-(define %notice! 5)
-(define %notify! 5)
-(define %info! 6)
-(define %debug! 7)
-(define %detail! 8)
-
 (define %emergency% 0)
+(define %emergency! 0)
+(define %panic% 0)
+(define %attention% 1)
+(define %attention! 1)
 (define %alert% 1)
+(define %alert! 1)
 (define %critical% 2)
+(define %critical! 2)
 (define %danger% 2)
+(define %danger! 2)
+(define %crit! 2)
+(define %crit! 2)
 (define %error% 3)
 (define %err% 3)
+(define %error! 3)
+(define %err! 3)
 (define %warning% 4)
 (define %warn% 4)
+(define %warning! 4)
+(define %warn! 4)
 (define %notice% 5)
 (define %notify% 5)
+(define %notice! 5)
+(define %notify! 5)
 (define %info% 6)
+(define %info! 6)
 (define %debug% 7)
+(define %debug! 7)
 (define %detail% 8)
+(define %detail! 8)
 
 (define %loglevel 4)
 
 (define loglevel-init-map
   '{(DETAIL . 8) (DETAILS . 8)
-    (DEBUG . 7) (DBG . 7) (INFO . 6)
+    (DEBUG . 7) (DBG . 7)
+    (INFO . 6) (STATUS . 6) (INFORMATION . 6)
     (NOTICE . 5) (NOTE . 5) (NOTIFY . 5)
     (WARN 4) (WARNING 4)
     (ERROR 3) (ERR 3)
     (ERROR 3) (ERR 3)
-    (CRITICAL 2) (CRIT 2)
-    (ALERT 1) (EMERGENCY 0) (EMERG 0)})
+    (CRITICAL 2) (DANGER 2) (CRIT 2)
+    (ALERT 1) (ATTENTION 1)
+    (EMERGENCY 0) (EMERG 0) (PANIC 0)})
 
 (define loglevel-table
   (let ((table (make-hashtable)))
@@ -91,6 +99,17 @@
   (macro expr
     `(logif+ (>= %loglevel ,(cadr expr)) ,(cadr expr) ,@(cddr expr))))
 
+(define logerr
+  (macro expr `(logmsg %error% ,@(cdr expr))))
+(define logcrit
+  (macro expr `(logmsg %critical% ,@(cdr expr))))
+(define logalert
+  (macro expr `(logmsg %alert% ,@(cdr expr))))
+(define logpanic
+  (macro expr `(logmsg %panic% ,@(cdr expr))))
+
+(define logerror logerr)
+
 (define logdetail
   (macro expr `(logif+ (>= %loglevel ,%detail%) 7 ,@(cdr expr))))
 (define logdebug
@@ -107,13 +126,28 @@
   (macro expr `(logif+ (>= %loglevel ,%debug%) 7 ,@(cdr expr))))
 
 (define detail%watch
-  (macro expr `(if (>= %loglevel ,%detail%) (,%watch ,@(cdr expr)) ,(cadr expr))))
+  (macro expr
+    `(if (>= %loglevel ,%detail%)
+	 (,%watch ,@(cdr expr))
+	 ,(cadr expr))))
 (define debug%watch
-  (macro expr `(if (>= %loglevel ,%debug%) (,%watch ,@(cdr expr)) ,(cadr expr))))
+  (macro expr
+    `(if (>= %loglevel ,%debug%)
+	 (,%watch ,@(cdr expr))
+	 ,(cadr expr))))
 (define info%watch
-  (macro expr `(if (>= %loglevel ,%info%) (,%watch ,@(cdr expr)) ,(cadr expr))))
+  (macro expr
+    `(if (>= %loglevel ,%info%)
+	 (,%watch ,@(cdr expr))
+	 ,(cadr expr))))
 (define notice%watch
-  (macro expr `(if (>= %loglevel ,%notice%) (,%watch ,@(cdr expr)) ,(cadr expr))))
+  (macro expr
+    `(if (>= %loglevel ,%notice%)
+	 (,%watch ,@(cdr expr))
+	 ,(cadr expr))))
 (define warn%watch
-  (macro expr `(if (>= %loglevel ,%warn%) (,%watch ,@(cdr expr)) ,(cadr expr))))
+  (macro expr
+    `(if (>= %loglevel ,%warn%)
+	 (,%watch ,@(cdr expr))
+	 ,(cadr expr))))
 
