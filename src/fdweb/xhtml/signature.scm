4df858cd74549a5c3f8ff307e940d2fedf0281e7
Ken Haase <kh@beingmeta.com>
2012-09-17 13:21:28 -0400

    Updated signature implementation

--- src/fdweb/xhtml/signature.scm
+++ src/fdweb/xhtml/signature.scm
@@ -11,12 +11,13 @@
 (define-init %loglevel %notify!)
 ;;(define %loglevel %debug!)
 
-(define (makesigtext . args)
+(defambda (makesigtext . args)
   (let ((table (if (odd? (length args)) (deep-copy (car args)) `#[]))
 	(params (if (odd? (length args)) (cdr args) args)))
     (do ((scan params (cddr scan)))
 	((null? scan))
-      (add! table (car scan) (cadr scan)))
+      (unless (fail? (cadr scan))
+	(add! table (car scan) (cadr scan))))
     (stringout
       (doseq (key (lexsorted (getkeys table)) i)
 	(if (string? key)
@@ -25,21 +26,17 @@
 	    (printout (if (> i 0) "&")
 		      (uriencode (lisp->string key)) "="))
 	(doseq (v (lexsorted (get table key)) j)
-	  (printout (if (> j 0) " ") v))))))
+	  (printout (if (> j 0) "&") v))))))
 
-(define (sig/make key . args)
+(defambda (sig/make key . args)
   (let ((text (apply makesigtext args)))
     (debug%watch (hmac-sha1 text key) "MAKESIG" text key args)))
-(define (sig/check sig key . args)
+(defambda (sig/check sig key . args)
   (let ((text (apply makesigtext args)))
+    (debug%watch "CHECKSIG" args)
     (debug%watch "CHECKSIG" sig key text (hmac-sha1 text key))
     (when (string? sig) (set! sig (base16->packet sig)))
     (and sig (or (equal? sig (hmac-sha1 text key))
-		 (begin (warn%watch "CHECKSIG/FAILED" sig key text (hmac-sha1 text key))
+		 (begin (warn%watch (hmac-sha1 text key)
+				    "CHECKSIG/FAILED" sig key text)
 		   #f)))))
-
-
-
-
-
-
