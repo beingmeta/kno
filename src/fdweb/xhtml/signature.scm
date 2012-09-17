cf4e4adb8a2d0156dd81fad063779b98f0b1b7d4
Ken Haase <kh@beingmeta.com>
2012-09-17 13:29:46 -0400

    Moved signature module into safe_modules, ledgerdermain to keep xhtml/signature working

--- src/fdweb/xhtml/signature.scm
+++ src/fdweb/xhtml/signature.scm
@@ -3,40 +3,10 @@
 
 (in-module 'xhtml/signature)
 
-(use-module '{fdweb texttools})
-(use-module '{varconfig logger rulesets ezrecords})
-
 (module-export! '{sig/make sig/check})
 
-(define-init %loglevel %notify!)
-;;(define %loglevel %debug!)
+(define sigmod (%watch (get-module 'signature)))
 
-(defambda (makesigtext . args)
-  (let ((table (if (odd? (length args)) (deep-copy (car args)) `#[]))
-	(params (if (odd? (length args)) (cdr args) args)))
-    (do ((scan params (cddr scan)))
-	((null? scan))
-      (unless (fail? (cadr scan))
-	(add! table (car scan) (cadr scan))))
-    (stringout
-      (doseq (key (lexsorted (getkeys table)) i)
-	(if (string? key)
-	    (printout (if (> i 0) "&")
-		      (uriencode key) "=")
-	    (printout (if (> i 0) "&")
-		      (uriencode (lisp->string key)) "="))
-	(doseq (v (lexsorted (get table key)) j)
-	  (printout (if (> j 0) "&") v))))))
+(define sig/make (get sigmod 'sig/make))
+(define sig/check (get sigmod 'sig/check))
 
-(defambda (sig/make key . args)
-  (let ((text (apply makesigtext args)))
-    (debug%watch (hmac-sha1 text key) "MAKESIG" text key args)))
-(defambda (sig/check sig key . args)
-  (let ((text (apply makesigtext args)))
-    (debug%watch "CHECKSIG" args)
-    (debug%watch "CHECKSIG" sig key text (hmac-sha1 text key))
-    (when (string? sig) (set! sig (base16->packet sig)))
-    (and sig (or (equal? sig (hmac-sha1 text key))
-		 (begin (warn%watch (hmac-sha1 text key)
-				    "CHECKSIG/FAILED" sig key text)
-		   #f)))))
---
+++ src/safe_modules/signature.scm
@@ -0,0 +1,42 @@
+;;; -*- Mode: Scheme; character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2012 beingmeta, inc. All rights reserved
+
+(in-module 'signature)
+
+(use-module '{fdweb texttools})
+(use-module '{varconfig logger rulesets ezrecords})
+
+(module-export! '{sig/make sig/check})
+
+(define-init %loglevel %notify!)
+;;(define %loglevel %debug!)
+
+(defambda (makesigtext . args)
+  (let ((table (if (odd? (length args)) (deep-copy (car args)) `#[]))
+	(params (if (odd? (length args)) (cdr args) args)))
+    (do ((scan params (cddr scan)))
+	((null? scan))
+      (unless (fail? (cadr scan))
+	(add! table (car scan) (cadr scan))))
+    (stringout
+      (doseq (key (lexsorted (getkeys table)) i)
+	(if (string? key)
+	    (printout (if (> i 0) "&")
+		      (uriencode key) "=")
+	    (printout (if (> i 0) "&")
+		      (uriencode (lisp->string key)) "="))
+	(doseq (v (lexsorted (get table key)) j)
+	  (printout (if (> j 0) "&") v))))))
+
+(defambda (sig/make key . args)
+  (let ((text (apply makesigtext args)))
+    (debug%watch (hmac-sha1 text key) "MAKESIG" text key args)))
+(defambda (sig/check sig key . args)
+  (let ((text (apply makesigtext args)))
+    (debug%watch "CHECKSIG" args)
+    (debug%watch "CHECKSIG" sig key text (hmac-sha1 text key))
+    (when (string? sig) (set! sig (base16->packet sig)))
+    (and sig (or (equal? sig (hmac-sha1 text key))
+		 (begin (warn%watch (hmac-sha1 text key)
+				    "CHECKSIG/FAILED" sig key text)
+		   #f)))))
