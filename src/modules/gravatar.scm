93436566df4965997743805f3c2689dd5b63e06f
Ken Haase <kh@beingmeta.com>
2012-10-03 01:43:49 +0000

    Made gravatars handle foo+xxx emails

--- src/modules/gravatar.scm
+++ src/modules/gravatar.scm
@@ -15,28 +15,48 @@
 (define (gravatar/hash s)
   (downcase (packet->base16 (md5 (downcase (trim-spaces s))))))
 
+(define plus-pat #((not> {"@" "+"}) "+" (not> "@") "@"))
+(define plus-extract
+  #((label id (not> {"@" "+"})) "+" (not> "@") "@"
+    (label host (rest))))
+(define (strip-plus s)
+  (let ((match (text->frame plus-extract s)))
+    (glom (get match 'id) "@" (get match 'host))))
+
 (define (gravatar/image s (size #f) (suffix #f) (canonical) (hash))
   (default! canonical (downcase (trim-spaces s)))
   (default! hash (downcase (packet->base16 (md5 canonical))))
   (try (scripturl (glom (or (get gravatars canonical) {}) (or suffix ""))
 	   's (tryif size size))
-       (let* ((baseurl (glom "http://www.gravatar.com/avatar/" hash))
-	      (httpsurl (glom "https://secure.gravatar.com/avatar/" hash))
-	      (probeurl (scripturl baseurl "d" "default.jpg"))
-	      (head (urlhead probeurl))
-	      (failed (test head 'location "default.jpg")))
-	 (if failed
-	     (store! gravatars canonical #f)
-	     (store! gravatars canonical httpsurl))
-	 (and (not failed)
+       (let ((usehash (probehash hash))
+	     (httpsurl #f))
+	 (when (and (not usehash) (textsearch plus-pat canonical))
+	   (set! canonical (strip-plus canonical))
+	   (set! hash (downcase (packet->base16 (md5 canonical))))
+	   (set! usehash (downcase (packet->base16 (md5 canonical)))))
+	 (when usehash
+	   (set! httpsurl
+		 (glom "https://secure.gravatar.com/avatar/" usehash)))
+	 (if httpsurl
+	     (store! gravatars canonical httpsurl)
+	     (store! gravatars canonical #f))
+	 (and httpsurl
 	      (if (or suffix size)
 		  (if (or suffix size)
 		      (scripturl (glom httpsurl (or suffix ""))
 			  's (tryif size size))
 		      (if suffix (glom httpsurl (or suffix ""))
 			  (scripturl httpsurl 'size size)))
-		  httpsurl)))
-       #f))
+		  httpsurl)))))
+
+(define (probehash hash)
+  (let* ((baseurl (glom "http://www.gravatar.com/avatar/" hash))
+	 (httpsurl (glom "https://secure.gravatar.com/avatar/" hash))
+	 (probeurl (scripturl baseurl "d" "UNlikeLY.jpg"))
+	 (head (urlhead probeurl)))
+    (and (not (equal? (basename (get head 'location))
+		      "UNlikeLY.jpg"))
+	 hash)))
 
 ;;; Profiles
 
