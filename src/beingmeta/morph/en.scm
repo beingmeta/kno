93dd96041853711cc6625fb5aa19712f002e2351
Ken Haase <kh@beingmeta.com>
2013-02-16 16:07:50 -0500

    Added relative frequency data to the morph module (for want of a better place)

--- src/beingmeta/morph/en.scm
+++ src/beingmeta/morph/en.scm
@@ -2,7 +2,7 @@
 
 ;; English Morphology
 ;;  Copyright (C) 2001 Kenneth Haase, All Rights Reserved
-;;  Copyright (C) 2001-2009 beingmeta, inc.
+;;  Copyright (C) 2001-2013 beingmeta, inc.
 
 (in-module 'morph/en)
 
