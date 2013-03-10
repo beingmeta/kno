d80d93119d2ec615ccccb68445c5c65316dc0ada
Ken Haase <kh@beingmeta.com>
2013-03-10 11:18:49 -0400

    Fixed bugs with SQS extraction pattern generators and some API calls

--- src/modules/aws/sqs.scm
+++ src/modules/aws/sqs.scm
@@ -13,30 +13,30 @@
 (define sqs-endpoint "https://sqs.us-east-1.amazonaws.com/")
 (varconfig! sqs:endpoint sqs-endpoint)
 
-(define (sqs-field-pattern name)
+(define (sqs-field-pattern name (label))
+  (default! label (string->lisp name))
   `#(,(glom "<" name ">")
-     (label body ,(parse-arg name) (not> ,(glom "</" name ">")))
-     (glom "</" name ">")))
+     (label ,label (not> ,(glom "</" name ">")))
+     ,(glom "</" name ">")))
 
 (define sqs-fields
-  {#("<Body>" (label body (not> "</Body>")) "</Body>")
-   #("<ReceiptHandle>" (label handle (not> "</ReceiptHandle>")) "</ReceiptHandle>")
-   #("<MessageId>" (label msgid (not> "</MessageId>")) "</MessageId>")
-   #("<RequestId>" (label reqid (not> "</RequestId>")) "</RequestId>")
-   #("<QueueUrl>" (label queue (not> "</QueueUrl>")) "</QueueUrl>")
-   #("<SenderId>" (label sent (not> "</SenderId>") #t) "</SenderId>")
-   #("<SentTimestamp>" (label sent (not> "</SentTimestamp>") #t) "</SentTimestamp>")
-   #("<ApproximateReceiveCount>" (label count (not> "</ApproximateReceiveCount>") #t)
-     "</ApproximateReceiveCount>")
-   #("<ApproximateReceiveTimestamp>" (label first (not> "</ApproximateReceiveTimestamp>") #t)
-     "</ApproximateReceiveTimestamp>")})
+  {(sqs-field-pattern "Body")
+   (sqs-field-pattern "ReceiptHandle" 'handle)
+   (sqs-field-pattern "MessageId" 'msgid)
+   (sqs-field-pattern "RequestId" 'reqid)
+   (sqs-field-pattern "QueueUrl" 'qurl)
+   (sqs-field-pattern "SenderId" 'sender)
+   (sqs-field-pattern "SentTimestamp" 'sent)
+   (sqs-field-pattern "ApproximateReceiveCount")
+   (sqs-field-pattern "ApproximateFirstReceiveTimestamp")})
 
 (define (handle-sqs-response result)
   (debug%watch result)
   (if (<= 200 (getopt result 'response) 299)
-      (let* ((combined (frame-create #f 'queue (getopt result '%queue)))
+      (let* ((combined (frame-create #f 'queue (getopt result '%queue {}) 'received (gmtimestamp)))
 	     (content (getopt result '%content))
-	     (fields (tryif content (text->frames sqs-fields content))))
+	     (fields (tryif content (debug%watch (text->frames sqs-fields content)
+				      sqs-fields content))))
 	(do-choices (field fields)
 	  (do-choices (key (getkeys field))
 	    (add! combined key (get field key))))
@@ -44,20 +44,19 @@
       (error |Bad SQS response| sqs/get (get result 'effective-url)
 	     result)))
 
-(define (sqs/get queue)
-  (handle-sqs-response
-   (aws4/get (frame-create #f '%queue queue) queue
-	     #["Action" "ReceiveMessage"])))
+(define (sqs/get queue (opts #[]) (args `#["Action" "ReceiveMessage" "AttributeName.1" "all"]))
+  (when (getopt opts 'wait) (store! args "WaitTimeSeconds" (getopt opts 'wait)))
+  (when (getopt opts 'reserve) (store! args "VisibilityTimeout" (getopt opts 'reserve)))
+  (handle-sqs-response (aws4/get (frame-create #f '%queue queue) queue args)))
 
-(define (sqs/send queue msg)
-  (handle-sqs-response
-   (aws4/get (frame-create #f '%queue queue) queue
-	     `#["Action" "SendMessage" "MessageBody" ,msg])))
+(define (sqs/send queue msg (opts #[]) (args `#["Action" "SendMessage"]))
+  (store! args "MessageBody" msg)
+  (when (getopt opts 'delay) (store! args "DelaySeconds" (getopt opts 'delay)))
+  (handle-sqs-response (aws4/get (frame-create #f '%queue queue) queue args)))
 
-(define (sqs/list)
-  (handle-sqs-response
-   (aws4/get (frame-create #f) sqs-endpoint
-	     #["Action" "ListQueues"])))
+(define (sqs/list (prefix #f) (args #["Action" "ListQueues"]))
+  (when prefix (set! args `#["Action" "ListQueues" "QueueNamePrefix" ,prefix]))
+  (handle-sqs-response (aws4/get (frame-create #f) sqs-endpoint args)))
 
 (define (sqs/delete message)
   (handle-sqs-response
@@ -71,7 +70,4 @@
 	     `#["Action" "ChangeMessageVisibility"
 		"ReceiptHandle" ,(get message 'handle)
 		"VisibilityTimeout" ,secs])))
-
-
-
 
