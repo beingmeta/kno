355db899152f8a04d821ae9ce458b3bed702d746
Ken Haase <kh@beingmeta.com>
2013-03-17 07:46:04 -0400

    Added LOGCTL config variable

--- src/modules/logctl.scm
+++ src/modules/logctl.scm
@@ -11,11 +11,19 @@
 
 (module-export! 'logctl!)
 
+(define levels {})
+
 (define (convert-log-arg arg)
   (if (and (pair? arg) (eq? (car arg) 'quote) (pair? (cdr arg))
 	      (symbol? (cadr arg)))
       (getloglevel (cadr arg))
       (getloglevel arg)))
+
+(define (record-loglevel! modname level)
+  (let ((entry (pick levels modname)))
+    (if (exists? entry)
+	(set-cdr! entry level)
+	(set+! levels (cons modname level)))))
 
 (define logctl!
   (macro expr
@@ -25,10 +33,22 @@
 	       (pair? (cdr modname)))
 	  (set! modname (cadr modname)))
       (when (fail? loglevel) (error "Bad LOGLEVEL" expr))
-      (if (symbol? modname)
-	  `(within-module ',modname (define %loglevel ,loglevel))
-	  `(within-module ,modname (define %loglevel ,loglevel))))))
+      (let ((modref (if (symbol? modname) `',modname modname)))
+	`(begin (within-module ,modref (define %loglevel ,loglevel))
+	   (,record-loglevel! ,modref ,loglevel))))))
 
+(define (logctl-config var (val))
+  (if (not (bound? val)) levels
+      (if (pair? val)
+	  (eval `(within-module ',(car val) (define %loglevel ,(cdr val))))
+	  (if (string? val)
+	      (let ((split (position #\: val)))
+		(if (not position)
+		    (error "Invalid LOGCTL spec" val)
+		    (let ((module (get-module (string->lisp (slice val 0 split))))
+			  (level (getloglevel (string->lisp (slice val (1+ split))))))
+		      (eval `(within-module ',module (define %loglevel ,level))))))))))
+(config-def! 'logctl logctl-config)
 
 
 
