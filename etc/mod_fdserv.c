8acabf926c2a6867b16accc08f61a1cddb83024f
Ken Haase <kh@beingmeta.com>
2013-04-15 20:26:03 -0400

    Pass gid as RUNGROUP

--- etc/mod_fdserv.c
+++ etc/mod_fdserv.c
@@ -942,7 +942,7 @@ static int spawn_fdservlet /* 2.0 */
   *write_argv++=(char *)sockname;
   
   if (gid>0) {
-    char *gidconfig=apr_psprintf(p,"GROUP=%d",gid);
+    char *gidconfig=apr_psprintf(p,"RUNGROUP=%d",gid);
     *write_argv++=gidconfig;}
 
   if (server_configs) {
