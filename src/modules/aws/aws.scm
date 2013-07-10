d2990253423275e5c4dc59cfbc1f97d4bf7059c7
Ken Haase <kh@beingmeta.com>
2013-07-10 10:45:15 -0400

    Added dropbox module stub

--- src/modules/aws/aws.scm
+++ src/modules/aws/aws.scm
@@ -1,11 +1,9 @@
 ;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
 ;;; Copyright (C) 2005-2013 beingmeta, inc.  All rights reserved.
 
+;;; Core file for accessing Amazon Web Services
 (in-module 'aws)
 
-;;; Core file for accessing Amazon Web Services
-(define version "$Id$")
-(define revision "$Revision$")
 
 (module-export! '{awskey secretawskey awsaccount})
 
