4164d9082c1a20fa3050172ce4443ab8eaaa37fe
Ken Haase <kh@beingmeta.com>
2013-11-06 10:23:39 -0500

    Many improvements to CSS parsing

--- src/modules/domutils/css.scm
+++ src/modules/domutils/css.scm
@@ -6,7 +6,10 @@
 (use-module '{reflection texttools varconfig logger
 	      domutils gpath})
 
-(module-export! '{css-rules css-rule dom/parsecss dom/getcss})
+(module-export! '{css-rules css-rule css/parse dom/getcss
+		  css/selector/parse css/selector/norm
+		  css/match css/matches
+		  css/textout css/htmlout})
 
 (define property-extract
   #((label property #((opt "-") (lword)))
@@ -75,17 +78,79 @@
 			   (gather->list css-rules (get parsed 'rules)))
 		      '())
 	      text ,rule]))
-	(else
-	 (for-choices (ex (text->frames css-rule-extract rule))
-	   (parse-properties (get ex 'props)
-			     (if media
-				 `#[selectors ,(get ex 'selector) media ,media]
-				 `#[selectors ,(get ex 'selector) ]))))))
+	(else (parse-css-rule rule media))))
 
-(define (dom/parsecss string (media #f))
+(define (parse-css-rule rule media)
+  (for-choices (ex (text->frames css-rule-extract rule))
+    (let* ((selectors (get ex 'selector))
+	   (parsed (css/selector/parse selectors))
+	   (matchers
+	    (choice (reject parsed vector?)
+		    (for-choices (v (pick parsed vector?))
+		      (reject (elt v -1) string?))))
+	   (match+ (for-choices (v (pick parsed vector?))
+		     (reject (elts v) string?)))
+	   (seltext (stdspace (slice rule 0 (position #\{ rule))))
+	   (props (get ex 'props))
+	   (base `#[selectors ,selectors parsed ,parsed
+		    matchers ,matchers match+ ,match+ ex ,ex
+		    rule ,rule props ,props sel ,seltext]))
+      (when media (store! base 'media media))
+      (parse-properties props base))))
+
+(define (css/parse string (media #f))
   (map (lambda (x) (parse-rule x media))
        (remove '() (gather->list (qc css-rules) string))))
 
+;;; Parsing selectors
+
+(define selector-extract
+  '(+ {#((bow) (label tag (char-not ".#:[ \t\n")))
+       #("." (label class (char-not ".#:[ \t\n")))
+       #("#" (label id (char-not ".#:[ \t\n")))
+       #("[" (label attrib (not> "]")) "]")
+       #(":" (label pseudo (char-not ".#:[ \t\n")))}))
+(define (css/selector/parse string)
+  (if (not (string? string)) string
+      (let ((clauses (segment string)))
+	(if (= (length clauses) 1)
+	    (text->frame selector-extract string)
+	    (map (lambda (x)
+		   (if (overlaps? x {">" "+"}) x
+		       (text->frame selector-extract x)))
+		 (->vector clauses))))))
+(define (css/selector/norm string)
+  (let ((parsed (if (not (string? string)) string
+		    (text->frame selector-extract string))))
+    (stringout (try (get parsed 'tag) "")
+      (doseq (id (sorted (get parsed 'id))) (printout "#" id))
+      (doseq (class (sorted (get parsed 'class))) (printout "." class))
+      (doseq (attrib (sorted (get parsed 'attrib)))
+	(printout "[" attrib "]"))
+      (doseq (pseudo (sorted (get parsed 'pseudo)))
+	(printout ":" pseudo)))))
+
+;;; Matching selectors
+
+(define (css/match pat sample)
+  (when (string? pat) (set! pat (css/selector/parse pat)))
+  (when (string? sample) (set! sample (css/selector/parse sample)))
+  (and (or (not (test pat 'tag)) (test sample 'tag (get pat 'tag)))
+       (or (not (test pat 'class))
+	   (contains? (get pat 'class) (get sample 'class)))
+       (or (not (test pat 'id)) (test sample 'id (get pat 'id)))
+       (or (not (test pat 'attrib))
+	   (contains? (get pat 'attrib) (get sample 'attrib)))))
+
+(define (css/matches rules pat)
+  (if (string? pat) (set! pat (css/selector/parse pat)))
+  (remove #f (map (lambda (x)
+		    (and (not (string? x))
+			 (exists css/match pat (get x 'matchers))
+			 x))
+		  rules)))
+
+;;; Getting CSS from a parsed DOM
 
 (define (dom/getcss dom source (count 0))
   (remove
@@ -93,7 +158,7 @@
 	     (if (test node '%xmltag 'style)
 		 (cons (try (get node 'id)
 			    (begin (set! count (1+ count)) count))
-		       (dom/parsecss
+		       (css/parse
 			(textsubst
 			 (apply glom (get node '%content))
 			 {"<!--" "-->" "<![CDATA" "]>"} "")
@@ -102,9 +167,33 @@
 		      (let* ((href (get node 'href))
 			     (media (try (get node 'media) #f))
 			     (absref (->gpath href source))
-			     (content (gp/fetch absref)))
+			     (content (onerror (gp/fetch absref)
+					(lambda (x)
+					  (logwarn "Couldn't access " absref)
+					  #f))))
 			(if (and (exists? content) (string? content))
-			    (cons href (dom/parsecss content media))
+			    (cons href (css/parse content media))
 			    (list href))))))
 	   (dom/find->list dom "style,link"))))
+
+;;; Output parsed CSS rule
+
+(define (css/textout entry (norm #t))
+  (if (pair? entry)
+      (doseq (e entry)
+	(when e (css/textout e) (printout "\n")))
+      (begin
+	(do-choices (sel (get entry 'parsed) i)
+	  (printout (if (> i 0) ", ")
+	    (if (vector? sel)
+		(doseq (elt sel i)
+		  (printout (if (> i 0) " ")
+		    (if (string? elt) elt (css/selector/norm elt))))
+		(css/selector/norm sel))))
+	(printout " {")
+	(doseq (prop (get entry 'properties))
+	  (printout "\n\t" prop ": " (get entry prop) ";"))
+	(printout "}"))))
+
+
 
