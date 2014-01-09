29b397a93bf24266998fa8c8822cfe7c0bed6aeb
Ken Haase <kh@beingmeta.com>
2014-01-09 08:27:10 -0500

    First draft of Dropbox API

--- src/modules/dropbox/dropbox.scm
+++ src/modules/dropbox/dropbox.scm
@@ -4,4 +4,48 @@
 ;;; Core file for accessing Dropbox
 (in-module 'dropbox)
 
-(use-module '{fdweb xhtml signature})
+(use-module '{fdweb xhtml signature oauth gpath mimetable ezrecords})
+
+(module-export! '{dropbox/get dropbox/put!})
+
+(defrecord dropbox oauth (root-path #f))
+
+(define-init dropbox-root "sandbox")
+(config-def! dropbox:live
+	     (lambda (var (val))
+	       (if (bound? val)
+		   (if val (set! dropbox-root "dropbox") (set! dropbox-root "sandbox"))
+		   (if (identical? dropbox-root "sandbox") #f
+		       (if (identical? dropbox-root "dropbox") #t
+			   (begin (logwarn "Fixing invalid dropbox-root " dropbox-root)
+			     (set! dropbox-root "sandbox")))))))
+
+(define (dropbox/get oauth path (revision #f))
+  (let ((endpoint (glom "https://content-api.dropbox.com/1/files/"
+		    (getopt oauth 'root
+			    (if (testopt root 'live)
+				(if (getopt root 'live #f) "dropbox" "sandbox")
+				dropbox-root))
+		    "/" path))
+	(result (if revision (oauth/call* oauth "GET" endpoint 'rev revision)
+		    (oauth/call* oauth "GET" endpoint 'rev revision))))
+    result))
+
+(define (dropbox/put! oauth path content (ctype #f) (revision #f))
+  (unless ctype
+    (set! ctype (path->mimetype path (if (packet? content) "application" "text"))))
+  (let ((endpoint
+	 (scripturl
+	     (glom "https://content-api.dropbox.com/1/files_put/"
+	       (getopt oauth 'root
+		       (if (testopt root 'live)
+			   (if (getopt root 'live #f) "dropbox" "sandbox")
+			   dropbox-root))
+	       "/" path)))
+	(result (oauth/call* oauth "PUT" endpoint content ctype)))
+    result))
+
+
+
+
+
