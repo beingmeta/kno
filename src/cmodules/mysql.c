6e04776ff6cb6731cfb899286461252b43ed493e
Ken Haase <kh@beingmeta.com>
2014-11-01 11:32:29 -0400

    Fixed bug in dealing with late version mysql timestamps

--- src/cmodules/mysql.c
+++ src/cmodules/mysql.c
@@ -1140,6 +1140,7 @@ static fdtype applymysqlproc(struct FD_FUNCTION *fn,int n,fdtype *args,
         FD_GET_CONS(arg,fd_timestamp_type,struct FD_TIMESTAMP *);
       MYSQL_TIME *mt=u8_alloc(MYSQL_TIME);
       struct U8_XTIME *xt=&(tm->xtime), gmxtime; time_t tick;
+      memset(mt,0,sizeof(MYSQL_TIME));
       if (n_mstimes<4) mstimes[n_mstimes++]=mt;
       if ((xt->u8_tzoff)||(xt->u8_dstoff)) {
         /* If it's not UTC, we need to convert it. */
