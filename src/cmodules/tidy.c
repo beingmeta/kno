ed6344650ec3d74c3549a157295ad73330241593
Ken Haase <kh@beingmeta.com>
2014-11-06 16:35:48 -0500

    Added more options for TIDY->XHTML

--- src/cmodules/tidy.c
+++ src/cmodules/tidy.c
@@ -31,6 +31,78 @@ FD_EXPORT int fd_init_tidy(void) FD_LIBINIT_FN;
 fd_exception fd_TidyError=_("Tidy Error");
 static int tidy_init=0;
 
+/* Opt setting */
+
+static fdtype getoption(fdtype opts,u8_string optstring,fdtype dflt)
+{
+  if ((FD_FALSEP(opts))||(FD_VOIDP(opts)))
+    return fd_incref(dflt);
+  else return fd_getopt(opts,fd_intern(optstring),dflt);
+}
+
+static int tidySetBoolOpt(TidyDoc tdoc,TidyOptionId optname,fdtype value){
+  if (FD_FALSEP(value))
+    return tidyOptSetBool(tdoc,optname,no);
+  else return tidyOptSetBool(tdoc,optname,yes);}
+static int copyBoolOpt(fdtype opts,TidyDoc tdoc,TidyOptionId optname,
+                       u8_string optstring,fdtype dflt){
+  fdtype value=getoption(opts,optstring,dflt); int rc;
+  if (FD_FALSEP(value))
+    rc=tidyOptSetBool(tdoc,optname,no);
+  else rc=tidyOptSetBool(tdoc,optname,yes);
+  fd_decref(value);
+  return rc;}
+
+static int tidySetIntOpt(TidyDoc tdoc,TidyOptionId optname,fdtype value){
+  if (FD_FIXNUMP(value))
+    return tidyOptSetInt(tdoc,optname,FD_FIX2INT(value));
+  else {
+    fd_incref(value);
+    fd_seterr(fd_TypeError,"tidySetIntOpt",
+              u8_strdup("integer"),value);
+    return -1;}}
+static int copyIntOpt(fdtype opts,TidyDoc tdoc,TidyOptionId optname,
+                      u8_string optstring,int dflt){
+  int rc=-1;
+  fdtype value=getoption(opts,optstring,FD_VOID);
+  if (FD_VOIDP(value))
+    rc=tidyOptSetInt(tdoc,optname,dflt);
+  else if (FD_FIXNUMP(value))
+    rc=tidyOptSetInt(tdoc,optname,FD_FIX2INT(value));
+  else {
+    fd_incref(value);
+    fd_seterr(fd_TypeError,"tidySetIntOpt",
+              u8_strdup("integer"),value);
+    rc=-1;}
+  fd_decref(value);
+  return rc;}
+
+static int tidySetStringOpt(TidyDoc tdoc,TidyOptionId optname,fdtype value){
+  if (FD_STRINGP(value))
+    return tidyOptSetValue(tdoc,optname,FD_STRDATA(value));
+  else {
+    fd_incref(value);
+    fd_seterr(fd_TypeError,"tidySetIntOpt",u8_strdup("string"),value);
+    return -1;}}
+static int copyStringOpt(fdtype opts,TidyDoc tdoc,TidyOptionId optname,
+                         u8_string optstring,u8_string dflt){
+  int rc=-1;
+  fdtype value=getoption(opts,optstring,FD_VOID);
+  if (FD_VOIDP(value))
+    rc=tidyOptSetValue(tdoc,optname,dflt);
+  else if (FD_STRINGP(value))
+    rc=tidyOptSetValue(tdoc,optname,FD_STRDATA(value));
+  else {
+    fd_incref(value);
+    fd_seterr(fd_TypeError,"copyStringOpt(tidy)",
+              u8_strdup("string"),value);
+    return -1;}
+  return rc;}
+
+/* The main primitive */
+
+static fdtype doctype_symbol, dontfix_symbol, wrap_symbol;
+
 static fdtype tidy_prim(fdtype string,fdtype opts,fdtype diag)
 {
   fdtype result=FD_VOID;
@@ -46,29 +118,72 @@ static fdtype tidy_prim(fdtype string,fdtype opts,fdtype diag)
   if (rc<0) {
     tidyRelease(tdoc);
     return fd_err(fd_TidyError,"tidy_prim/init",NULL,FD_VOID);}
-  if ((FD_VOIDP(opts))||(FD_FALSEP(opts))) {
-    if (rc>=0) rc=tidyOptSetValue(tdoc,TidyCharEncoding,"utf8");
-    if (rc>=0) rc=tidyOptSetInt(tdoc,TidyDoctypeMode,TidyDoctypeOmit);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyShowWarnings,no);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyQuiet,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMakeBare,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMakeClean,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyDropEmptyParas,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyFixComments,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyXmlSpace,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyEncloseBodyText,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyEncloseBlockText,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyWord2000,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyMark,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyJoinClasses,yes);
-    if (rc>=0) rc=tidyOptSetBool(tdoc,TidyNCR,yes);}
-  else {}
+  if (rc>=0) rc=copyStringOpt(opts,tdoc,TidyCharEncoding,"ENCODING","utf8");
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyShowWarnings,"WARN",FD_FALSE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyQuiet,"QUIET",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyMakeBare,"BARE",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyMakeClean,"CLEAN",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyDropEmptyParas,"DROPEMPTY",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyFixComments,"FIXCOMMENTS",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyXmlSpace,"XMLSPACE",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyEncloseBodyText,"ENCLOSEBODY",
+                            FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyEncloseBlockText,"ENCLOSEBLOCK",
+                            FD_FALSE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyWord2000,"FIXWORD2000",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyMark,"LEAVEMARK",FD_FALSE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyJoinClasses,"JOINCLASSES",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyJoinStyles,"JOINSTYLES",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyXhtmlOut,"XHTML",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyHtmlOut,"HTML",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyFixUri,"FIXURI",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyNCR,"NUMENTITIES",FD_TRUE);
+  if (rc>=0) rc=copyStringOpt(opts,tdoc,TidyCSSPrefix,"CSSPREFIX","tidy-");
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyQuoteAmpersand,"QUOTEAMP",FD_TRUE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyIndentAttributes,
+                            "INDENTATTRIBS",FD_FALSE);
+  if (rc>=0) rc=copyIntOpt(opts,tdoc,TidyIndentSpaces,"INDENTATION",4);
+  if (rc>=0) rc=copyIntOpt(opts,tdoc,TidyTabSize,"TABSIZE",5);
+  /*
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyMergeSpans,"MERGESPANS",FD_FALSE);
+  if (rc>=0) rc=copyBoolOpt(opts,tdoc,TidyMergeDivs,"MERGEDIVS",FD_FALSE);
+  */
+  if (rc>=0) {
+    fdtype wrap=fd_getopt(opts,wrap_symbol,FD_VOID);
+    if (FD_FIXNUMP(wrap))
+      rc=tidyOptSetInt(tdoc,TidyWrapLen,FD_FIX2INT(wrap));
+    else if (!((FD_FALSEP(wrap))||(FD_VOIDP(wrap))))
+      rc=tidyOptSetInt(tdoc,TidyWrapLen,80);
+    else {}}
+  if (rc>=0) {
+    fdtype indent=fd_getopt(opts,doctype_symbol,FD_VOID);
+    if (FD_FALSEP(indent)) {}
+    else if (FD_FIXNUMP(indent)) {
+      rc=tidyOptSetValue(tdoc,TidyIndentContent,"auto");
+      if (rc>=0)
+        rc=tidyOptSetInt(tdoc,TidyIndentSpaces,FD_FIX2INT(indent));}
+    else rc=tidyOptSetValue(tdoc,TidyIndentContent,"auto");}
+  if (rc>=0) {
+    fdtype doctype=fd_getopt(opts,doctype_symbol,FD_VOID);
+    if (FD_VOIDP(doctype))
+      tidyOptSetInt(tdoc,TidyDoctypeMode,TidyDoctypeAuto);
+    else if (FD_FALSEP(doctype))
+      tidyOptSetInt(tdoc,TidyDoctypeMode,TidyDoctypeAuto);
+    else if (!(FD_STRINGP(doctype)))
+      tidyOptSetValue(tdoc,TidyDoctype,"<!DOCTYPE html>");
+    else {
+      tidyOptSetInt(tdoc,TidyDoctypeMode,TidyDoctypeUser);
+      tidyOptSetValue(tdoc,TidyDoctype,FD_STRDATA(doctype));}
+    fd_decref(doctype);}
   if (rc<0) result=fd_err(fd_TidyError,"tidy_prim/setopts",errbuf.bp,FD_VOID);
   else {
+    fdtype dontfix=fd_getopt(opts,dontfix_symbol,FD_FALSE);
     rc=tidyParseString(tdoc,FD_STRDATA(string));
     if (rc<0)
       result=fd_err(fd_TidyError,"tidy_prim/parse",errbuf.bp,FD_VOID);
-    else rc=tidyCleanAndRepair(tdoc);
+    else if (FD_FALSEP(dontfix))
+      rc=tidyCleanAndRepair(tdoc);
+    else {}
     if (!(FD_VOIDP(result))) {}
     else if (rc<0)
       result=fd_err(fd_TidyError,"tidy_prim/clean",errbuf.bp,FD_VOID);
@@ -79,12 +194,18 @@ static fdtype tidy_prim(fdtype string,fdtype opts,fdtype diag)
     else rc=tidySaveBuffer(tdoc,&outbuf);
     if (!(FD_VOIDP(result))) {}
     else if (rc<0)
-      result=fd_err(fd_TidyError,"tidy_prim/save",errbuf.bp,FD_VOID);
+      result=fd_err(fd_TidyError,"tidy_prim/output",errbuf.bp,FD_VOID);
     else result=fdtype_string(outbuf.bp);
     if ((!((FD_VOIDP(diag))||(FD_FALSEP(diag))))&&((rc>0)||(rc<0))) {
       int drc=tidyRunDiagnostics(tdoc);
-      if (drc>=0) u8_log(LOG_WARNING,"TIDY","%s",errbuf.bp);
-      else u8_log(LOG_CRIT,"TIDY/diagfail","%s",errbuf.bp);}}
+      if (drc<0) u8_log(LOG_CRIT,"TIDY/diagfail","%s",errbuf.bp);
+      else if (FD_APPLICABLEP(diag)) {
+        fdtype arg=fdtype_string(errbuf.bp);
+        fdtype dresult=fd_apply(diag,1,&arg);
+        if (FD_ABORTP(dresult)) {
+          fd_decref(result); result=dresult;}
+        else fd_decref(dresult);}
+      else u8_log(LOG_WARNING,"TIDY","%s",errbuf.bp);}}
   tidyBufFree(&outbuf);
   tidyBufFree(&errbuf);
   tidyRelease(tdoc);
@@ -97,6 +218,9 @@ FD_EXPORT int fd_init_tidy()
   if (tidy_init) return 0;
 
   tidy_init=1;
+  doctype_symbol=fd_intern("DOCTYPE");
+  dontfix_symbol=fd_intern("DONTFIX");
+  wrap_symbol=fd_intern("WRAP");
   tidy_module=fd_new_module("TIDY",(FD_MODULE_SAFE));
 
   fd_idefn(tidy_module,
