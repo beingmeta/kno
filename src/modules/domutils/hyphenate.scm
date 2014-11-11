04cfadcbf61a83e0d540d5678b5171f666e79acc
Ken Haase <kh@beingmeta.com>
2014-11-11 07:27:53 -0500

    Re-encode HTML text after hyphenating, don't hyphenate PRE stuff

--- src/modules/domutils/hyphenate.scm
+++ src/modules/domutils/hyphenate.scm
@@ -11,22 +11,28 @@
 (define-init %loglevel %notice%)
 
 (define (hyphenate-string string)
-  (string-subst (hyphenate (decode-entities string))
-		"­­" "­"))
+  (encode-entities
+   (string-subst (hyphenate (decode-entities string))
+		 "­­" "­")))
 
 (define (dom/hyphenate! node)
   (if (string? node) (hyphenate-string node)
-      (if (test node '%content)
-	  (if (string? (get node '%content))
-	      (begin (store! node '%content
-			     (hyphenate-string (get node '%content)))
-		node)
-	      (if (pair? (get node '%content))
+      (if (not (or (test node '%xmltag 'pre)
+		   (test node 'xml:space "preserve")))
+	  (if (test node '%content)
+	      (if (string? (get node '%content))
 		  (begin (store! node '%content
-				 (map dom/hyphenate! (get node '%content)))
+				 (hyphenate-string (get node '%content)))
 		    node)
-		  node))
+		  (if (pair? (get node '%content))
+		      (begin (store! node '%content
+				     (map dom/hyphenate! (get node '%content)))
+			node)
+		      node))
+	      node)
+	  ;; Spaces matter, so don't bother hyphenating
 	  node)))
+
 
 
 
