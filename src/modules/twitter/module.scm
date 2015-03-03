8ee17f1d160718228defcd3073d342f5c77e3727
Ken Haase <kh@beingmeta.com>
2015-03-02 20:50:43 -0500

    Fixed some bad module links

--- src/modules/twitter/module.scm
+++ src/modules/twitter/module.scm
@@ -1,11 +1,1 @@
-;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2014 beingmeta, inc.  All rights reserved.
-
-(in-module 'twitter)
-
-(use-module '{fdweb texttools reflection varconfig logger})
-(use-module '{xhtml xhtml/auth})
-
-(define %loglevel %notice!)
-;;(define %loglevel %debug!)
-
+twitter.scm
