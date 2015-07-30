a423c0d754f6b959046f3ffc8d74fef10b09201d
Ken Haase <kh@beingmeta.com>
2015-07-30 03:53:22 +0000

    Restored some symlinks

--- src/modules/google/module.scm
+++ src/modules/google/module.scm
@@ -1,8 +1,1 @@
-;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2015 beingmeta, inc.  All rights reserved.
-
-;;; Core file for accessing Amazon Web Services
-(in-module 'google)
-
-(use-module '{fdweb oauth varconfig})
-
+google.scm
