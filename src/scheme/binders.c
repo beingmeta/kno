bff64a6211c429c1c4801cab12c2dc3203c24744
Ken Haase <kh@beingmeta.com>
2015-11-04 10:33:25 -0500

    Added SETFALSE! and made DEFAULT! and BIND-DEFAULT! handle FD_UNBOUND and FD_DEFAULT_VALUE as unbound

--- src/scheme/binders.c
+++ src/scheme/binders.c
@@ -89,7 +89,29 @@ static fdtype set_default_handler(fdtype expr,fd_lispenv env)
     return fd_err(fd_SyntaxError,"set_default_handler",NULL,fd_incref(expr));
   else {
     fdtype val=fd_symeval(symbol,env);
-    if (FD_VOIDP(val)) {
+    if ((FD_VOIDP(val))||(val==FD_UNBOUND)||(val==FD_DEFAULT_VALUE)) {
+      fdtype value=fd_eval(value_expr,env);
+      if (FD_ABORTP(value)) return value;
+      if (fd_set_value(symbol,value,env)==0)
+        fd_bind_value(symbol,value,env);
+      fd_decref(value);
+      return FD_VOID;}
+    else {
+      fd_decref(val); return FD_VOID;}}
+}
+
+static fdtype set_false_handler(fdtype expr,fd_lispenv env)
+{
+  fdtype symbol=fd_get_arg(expr,1);
+  fdtype value_expr=fd_get_arg(expr,2);
+  if (!(FD_SYMBOLP(symbol)))
+    return fd_err(fd_SyntaxError,"set_false_handler",NULL,fd_incref(expr));
+  else if (FD_VOIDP(value_expr))
+    return fd_err(fd_SyntaxError,"set_false_handler",NULL,fd_incref(expr));
+  else {
+    fdtype val=fd_symeval(symbol,env);
+    if ((FD_VOIDP(val))||(FD_FALSEP(val))||
+        (val==FD_UNBOUND)||(val==FD_DEFAULT_VALUE)) {
       fdtype value=fd_eval(value_expr,env);
       if (FD_ABORTP(value)) return value;
       if (fd_set_value(symbol,value,env)==0)
@@ -112,7 +134,7 @@ static fdtype bind_default_handler(fdtype expr,fd_lispenv env)
     return fd_err(fd_SyntaxError,"bind_default_handler",NULL,fd_incref(expr));
   else {
     fdtype val=fd_get(env->bindings,symbol,FD_VOID);
-    if (FD_VOIDP(val)) {
+    if ((FD_VOIDP(val))||(val==FD_UNBOUND)||(val==FD_DEFAULT_VALUE)) {
       fdtype value=fd_eval(value_expr,env);
       if (FD_ABORTP(value)) return value;
       fd_bind_value(symbol,value,env);
@@ -1123,6 +1145,7 @@ FD_EXPORT void fd_init_binders_c()
   fd_defspecial(fd_scheme_module,"DO",do_handler);
 
   fd_defspecial(fd_scheme_module,"DEFAULT!",set_default_handler);
+  fd_defspecial(fd_scheme_module,"SETFALSE!",set_default_handler);
   fd_defspecial(fd_scheme_module,"BIND-DEFAULT!",bind_default_handler);
 
 #if FD_IPEVAL_ENABLED
