89733d2ab22ee60fe8f57be86c94a8e7fe6d651e
Ken Haase <kh@beingmeta.com>
2016-01-19 20:07:28 -0500

    Added xconfig module

---
+++ src/modules/xconfig.scm
@@ -0,0 +1,80 @@
+;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2015 beingmeta, inc.  All rights reserved.
+
+(in-module 'xconfig)
+
+;;; Loading of modules from external (non-file system) sources
+
+(use-module '{gpath varconfig logger})
+
+(module-export! '{load-xconfig xconfig:root xconfig? xconfig/reset!})
+
+(define-init %loglevel %notice%)
+
+(define-init xconfig:root #f)
+(define-init xconfigs {})
+
+(varconfig! xconfig:root xconfig:root)
+
+(define (xconfig? path)
+  (or (overlaps? path xconfigs)
+      (and (gpath? path) 
+	   (overlaps? (gpath->string path xconfig:root) xconfig))
+      (and (string? path)
+	   (onerror
+	       (overlaps? (gpath->string (->gpath path xconfig:root))
+			  xconfig)
+	     (lambda (ex) #f)))))
+
+(define (canonical-gpath path)
+  (if (string? gpath) (gpath->string (->gpath path xconfig:root))
+      (if (gpath? path) (gpath->string path)
+	  (fail))))
+
+(defambda (xconfig/reset! (paths #f))
+  (if (not paths) 
+      (set! xconfigs {})
+      (set! xconfigs (difference xconfigs (canonical-gpath paths)))))
+
+(define (load-xconfig path (err #f))
+  (let* ((gp (->gpath path xconfig:root))
+	 (canonical (and (gpath? gp) (gpath->string gp))))
+    (if (gpath? gp)
+	(unless (overlaps? canonical xconfigs) 
+	  (if (gp/exists? gp)
+	      (begin
+		(lognotice |XConfig| "Loading config from " canonical)
+		(read-config (gp/fetch gp))
+		(set+! xconfigs canonical))
+	      (if err
+		  (irritant canonical |LoadXConfigFailed|)
+		  (logwarn |LoadXConfigFailed| 
+		    "Can't get content for " 
+		    canonical))))
+	(if err
+	    (irritant path |InvalidXConfigPath|)
+	    (logwarn |InvalidXConfigPath| "Can't handle " path)))))
+
+(define (xconfig-handler var (val))
+  (if (not (bound? val)) xconfigs
+      (if (symbol? val) 
+	  (if (and (config val) (string? (config val)))
+	      (load-xconfig (config val) #t)
+	      (irritant val |BadXconfigRef| "Could't resolve XCONFIG "))
+	  (if (or (string? val) (gpath? string))
+	      (load-xconfig val #t)
+	      (irritant val |BadXconfigValue| val)))))
+(config-def! 'xconfig xconfig-handler)
+
+(define (xxconfig-handler var (val))
+  (if (not (bound? val)) xconfigs
+      (if (symbol? val) 
+	  (if (and (config val) (string? (config val)))
+	      (load-xconfig (config val))
+	      (logwarn |BadXconfigRef| "Could't resolve XCONFIG " val))
+	  (if (or (string? val) (gpath? string))
+	      (load-xconfig val)
+	      (logwarn |BadXconfigValue| "Couldn't handle " val)))))
+(config-def! 'xxconfig xxconfig-handler)
+
+
