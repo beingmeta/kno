d79bce9c26b93fbe3f42988edca1ad4bea526948
Ken Haase <kh@beingmeta.com>
2016-03-23 14:27:30 -0400

    Ann an xmlparsing regression testing

---
+++ tests/scheme/xmltest.scm
@@ -0,0 +1,16 @@
+(load-component "common.scm")
+
+(use-module 'fdweb)
+
+(define xml-test-input-1
+  "<p title='check if x > 8'>x is a value.</p>")
+(define xml-test-input-2
+  "<p>x is <?fdeval x?> supposedly.</p>")
+(define xml-test-input-3
+  "<p title='check if x > 8'>x is <?fdeval x?> supposedly.</p>")
+
+
+(applytest #t pair? (xmlparse xml-test-input-1))
+(applytest #t pair? (xmlparse xml-test-input-2))
+(applytest #t pair? (xmlparse xml-test-input-3))
+
