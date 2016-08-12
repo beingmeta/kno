198fcc314726048f0fc68fe330f11b88980edaed
Ken Haase <kh@beingmeta.com>
2016-08-12 11:32:03 -0400

    Add special opcode eval function

--- src/scheme/opcodes.c
+++ src/scheme/opcodes.c
@@ -1,5 +1,7 @@
 #include "framerd/dbprims.h"
 
+static fdtype op_eval(fdtype x,fd_lispenv env);
+
 /* Opcode names */
 
 const u8_string fd_opcode_names[256]={
@@ -100,7 +102,7 @@ static fdtype opcode_special_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
     fdtype test_expr=fd_get_arg(expr,1), test=FD_VOID;
     if (FD_VOIDP(test_expr))
       return fd_err(fd_SyntaxError,"conditional OPCODE test",NULL,expr);
-    test=fd_eval(test_expr,env);
+    test=op_eval(test_expr,env);
     if (FD_ABORTED(test)) return test;
     else if (FD_VOIDP(test))
       return fd_err(fd_VoidArgument,"conditional OPCODE test",NULL,expr);
@@ -119,7 +121,7 @@ static fdtype opcode_special_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
       case FD_WHEN_OPCODE: {
         if (!(FD_FALSEP(test))) {
           FD_DOBODY(tmp,expr,2) {
-            fdtype tmpval=fd_eval(tmp,env);
+            fdtype tmpval=op_eval(tmp,env);
             if (FD_ABORTED(tmpval)) return tmpval;
             fd_decref(tmpval);}}
         fd_decref(test);
@@ -127,7 +129,7 @@ static fdtype opcode_special_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
       case FD_UNLESS_OPCODE: {
         if (FD_FALSEP(test)) {
           FD_DOBODY(tmp,expr,2) {
-            fdtype tmpval=fd_eval(tmp,env);
+            fdtype tmpval=op_eval(tmp,env);
             if (FD_ABORTED(tmpval)) return tmpval;
             fd_decref(tmpval);}
           return FD_VOID;}
@@ -174,7 +176,7 @@ static fdtype opcode_special_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
       fdtype arg1_expr=fd_get_arg(expr,1), arg1;
       if (FD_VOIDP(arg1_expr))
         return fd_err(fd_SyntaxError,"NOT opcode",NULL,expr);
-      else arg1=fd_eval(arg1_expr,env);
+      else arg1=op_eval(arg1_expr,env);
       if (FD_VOIDP(arg1))
         return fd_err(fd_VoidArgument,"NOT opcode",NULL,arg1_expr);
       else if (FD_EMPTY_CHOICEP(arg1)) return arg1;
@@ -213,7 +215,7 @@ static fdtype eval_tail(fdtype expr,int start,enum TAILOP op,
     fdtype *rail_data=FD_RAIL_DATA(expr), final_expr=rail_data[n_butlast];
     int i=2; while (i<n_butlast) {
       fdtype each=rail_data[i++];
-      fdtype each_value=fd_eval(each,env);
+      fdtype each_value=op_eval(each,env);
       if (FD_ABORTED(each_value)) return each_value;
       else if (op==any_op) fd_decref(each_value);
       else if ((op==and_op)&&(FD_FALSEP(each_value)))
@@ -232,7 +234,7 @@ static fdtype eval_tail(fdtype expr,int start,enum TAILOP op,
       exprs=next; next=FD_CDR(exprs); i++;}
     while (FD_PAIRP(next)) {
       fdtype each=FD_CAR(exprs);
-      fdtype each_value=fd_eval(each,env);
+      fdtype each_value=op_eval(each,env);
       if (FD_ABORTED(each_value)) return each_value;
       else if (op==any_op) fd_decref(each_value);
       else if ((op==and_op)&&(FD_FALSEP(each_value)))
@@ -686,7 +688,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
      "advance" the body. */
   arg1_expr=fd_get_arg(expr,1);
   if (!(israil)) body=FD_CDR(body);
-  arg1=fd_eval(arg1_expr,env);
+  arg1=op_eval(arg1_expr,env);
   /* Now, check the result of the first argument expression */
   if (FD_ABORTED(arg1)) return arg1;
   else if (FD_VOIDP(arg1))
@@ -755,7 +757,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
   else if (opcode<FD_NARY_OPCODES)
     /* Binary opcodes all live beneath FD_NARY_OPCODES */ { 
     /* Binary calls start by evaluating the second argument */
-    fdtype arg2=fd_eval(fd_get_arg(expr,2),env);
+    fdtype arg2=op_eval(fd_get_arg(expr,2),env);
     if (FD_ABORTED(arg2)) {
       fd_decref(arg1); return arg2;}
     else if (FD_VOIDP(arg2)) {
@@ -782,7 +784,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
     if (FD_EMPTY_CHOICEP(arg1)) return FD_EMPTY_CHOICE;
     else {
       fdtype slotid_arg=fd_get_arg(expr,2);
-      fdtype slotids=fd_eval(slotid_arg,env), result;
+      fdtype slotids=op_eval(slotid_arg,env), result;
       if (FD_ABORTED(slotids)) return slotids;
       else if (FD_VOIDP(slotids))
 	return fd_err(fd_SyntaxError,"OPCODE fget",NULL,expr);
@@ -793,7 +795,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
         if (FD_VOIDP(dflt_arg))
           result=fd_get(arg1,slotids,FD_EMPTY_CHOICE);
         else {
-          fdtype dflt=fd_eval(dflt_arg,env);
+          fdtype dflt=op_eval(dflt_arg,env);
           if (FD_ABORTED(dflt)) result=dflt;
           else if (FD_VOIDP(dflt)) {
             result=fd_err(fd_VoidArgument,"OPCODE pget",NULL,
@@ -807,7 +809,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
     else {
       fdtype slotid_arg=fd_get_arg(expr,2);
       fdtype values_arg=fd_get_arg(expr,3);
-      fdtype slotids=fd_eval(slotid_arg,env), values, result;
+      fdtype slotids=op_eval(slotid_arg,env), values, result;
       if (FD_ABORTED(slotids)) return slotids;
       else if (FD_VOIDP(slotids))
 	return fd_err(fd_SyntaxError,"OPCODE ftest",NULL,expr);
@@ -815,7 +817,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
         fd_decref(arg1); return FD_FALSE;}
       else if (FD_VOIDP(values_arg))
 	values=FD_VOID;
-      else values=fd_eval(values_arg,env);
+      else values=op_eval(values_arg,env);
       if (FD_ABORTED(values)) {
         fd_decref(arg1); fd_decref(slotids);
         return values;}
@@ -829,7 +831,7 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
   else if ((opcode>=FD_SETOPS_OPCODES) &&
 	   (opcode<=FD_DIFFERENCE_OPCODE)) {
     fdtype arg2expr=fd_get_arg(expr,2), argv[2];
-    fdtype arg2=fd_eval(arg2expr,env);
+    fdtype arg2=op_eval(arg2expr,env);
     fdtype result=FD_ERROR_VALUE;
     argv[0]=arg1; argv[1]=arg2;
     if (FD_ABORTED(arg2)) result=arg2;
@@ -921,6 +923,40 @@ static fdtype opcode_dispatch(fdtype opcode,fdtype expr,fd_lispenv env)
   else {
     fd_decref(arg1);
     return fd_err(fd_SyntaxError,"opcode eval",NULL,expr);}
+}
+
+static fdtype op_eval(fdtype x,fd_lispenv env)
+{
+  switch (FD_PTR_MANIFEST_TYPE(x)) {
+  case fd_oid_ptr_type: case fd_fixnum_ptr_type:
+    return x;
+  case fd_immediate_ptr_type:
+    if (FD_PRIM_TYPEP(x,fd_lexref_type))
+      return fd_lexref(x,env);
+    else if (FD_SYMBOLP(x)) {
+      fdtype val=fd_symeval(x,env);
+      if (FD_EXPECT_FALSE(FD_VOIDP(val)))
+        return fd_err(fd_UnboundIdentifier,"op_eval",FD_SYMBOL_NAME(x),x);
+      else return val;}
+    else return x;
+  case fd_slotmap_type:
+    return fd_deep_copy(x);
+  case fd_cons_ptr_type:
+    if (FD_PTR_TYPEP(x,fd_pair_type))
+      if (FD_PTR_TYPEP(FD_CAR(x),fd_opcode_type))
+        return opcode_dispatch(FD_CAR(x),x,env);
+      else return fd_eval(x,env);
+    else if (FD_PTR_TYPEP(x,fd_rail_type))
+      if (FD_PTR_TYPEP(FD_RAIL_REF(x,0),fd_opcode_type))
+        return opcode_dispatch(FD_RAIL_REF(x,0),x,env);
+      else return fd_eval(x,env);
+    else if ((FD_PTR_TYPEP(x,fd_choice_type)) ||
+             (FD_PTR_TYPEP(x,fd_achoice_type)))
+      return fd_eval(x,env);
+    else return fd_incref(x);
+  default: /* Never reached */
+    return x;
+  }
 }
 
 /* Emacs local variables
