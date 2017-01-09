d8a566d94c1128b827185104e46c448d8d107987
Ken Haase <kh@beingmeta.com>
2017-01-09 07:06:07 -0500

    Add better hash function for pointer locks, add FD_PTR_LOCKBITS base #define

--- include/framerd/defines.h
+++ include/framerd/defines.h
@@ -151,11 +151,11 @@ typedef int fd_size_t;
 #define FD_THREADS_ENABLED 1
 #endif
 
-#if FD_THREADS_ENABLED
-#ifndef FD_N_PTRLOCKS
-#define FD_N_PTRLOCKS 64
+#ifndef FD_PTR_LOCKBITS
+#define FD_PTR_LOCKBITS 6
 #endif
-#endif
+
+#define FD_N_PTRLOCKS (1<<FD_PTR_LOCKBITS)
 
 #if FD_THREADS_ENABLED
 #if HAVE_PTHREAD_H
