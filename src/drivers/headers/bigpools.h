ca031ceed5587dc14abfa450d7cb66a1eac9da4a
Ken Haase <kh@beingmeta.com>
2017-02-27 06:05:21 -0500

    Start adding bigpools

---
+++ src/drivers/headers/bigpools.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; Character-encoding: utf-8; -*- */
+
+/* Copyright (C) 2004-2017 beingmeta, inc.
+   This file is part of beingmeta's FramerD platform and is copyright
+   and a valuable trade secret of beingmeta, inc.
+*/
+
+#include "chunkrefs.h"
+
+#define FD_BIGPOOL_MAGIC_NUMBER ((0x17<<24)|(0x11<<16)|(0x04<<8)|(0x10))
+#define FD_BIGPOOL_TO_RECOVER ((0x11<<24)|(0x09<<16)|(0x04<<8)|(0x12))
+
+/* OID Pools */
+
+#define FD_BIGPOOL_OFFMODE     0x07
+#define FD_BIGPOOL_COMPRESSION 0x78
+#define FD_BIGPOOL_READ_ONLY   0x80
+#define FD_BIGPOOL_DTYPEV2     0x100
+
+typedef struct FD_BIGPOOL {
+  FD_POOL_FIELDS;
+  unsigned int pool_xformat;
+  fd_offset_type pool_offtype;
+  fd_compression_type pool_compression;
+  struct FD_STREAM pool_stream;
+  unsigned int pool_load;
+  unsigned int *pool_offdata, pool_offdata_length;
+  unsigned char *pool_mmap; size_t pool_mmap_size;
+  fdtype *pool_slotids;
+  unsigned int pool_n_slotids, pool_slotids_length;
+  struct FD_HASHTABLE pool_slotid_indices;
+  time_t pool_modtime;
+  U8_MUTEX_DECL(file_lock);} FD_BIGPOOL;
+typedef struct FD_BIGPOOL *fd_bigpool;
+
+
