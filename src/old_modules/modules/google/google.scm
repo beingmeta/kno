76793951d6172eb4f11817c89f681ba4d28945c3
Ken Haase <kh@beingmeta.com>
2017-03-15 17:28:21 -0400

    Removed old modules

--- src/old_modules/modules/google/google.scm
+++
@@ -1,8 +0,0 @@
-;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2016 beingmeta, inc.  All rights reserved.
-
-;;; Core file for accessing Amazon Web Services
-(in-module 'google)
-
-(use-module '{fdweb oauth varconfig})
-
--- src/old_modules/safe_modules/textindex/domtext.scm
+++
@@ -1,6 +0,0 @@
-;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2013 beingmeta, inc.  All rights reserved.
-
-(in-module 'textindex/domtext)
-
-;;; Module for analyzing text in a DOM
