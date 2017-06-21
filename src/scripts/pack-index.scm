829970334083da22b8f0b5813487285ced81a15c
Ken Haase <kh@beingmeta.com>
2017-06-21 18:32:57 -0400

    Make pack-index smarter

--- src/scripts/pack-index.scm
+++ src/scripts/pack-index.scm
@@ -1,7 +1,7 @@
 ;;; -*- Mode: Scheme -*-
 
 (config! 'cachelevel 2)
-(use-module '{optimize mttools logger stringfmts varconfig})
+(use-module '{optimize mttools logger stringfmts fifo varconfig})
 
 (define (getflags)
   (choice->vector
@@ -9,7 +9,6 @@
 	   (tryif (config 'B32 #f) 'B32)
 	   (tryif (config 'B40 #f) 'B40)
 	   (tryif (config 'B64 #f) 'B64)
-	   ;; (tryif (config 'ZLIB #f) 'ZLIB)
 	   )))
 
 ;;; Computing baseoids 
@@ -62,6 +61,11 @@
   
 ;;; Other features
 
+(define (symbolize s)
+  (if (symbol? s) s
+      (if (string? s) (string->symbol (upcase s))
+	  (irritant s |NotStringOrSymbol|))))
+
 (define (get-new-size base)
   (config 'newsize (inexact->exact (* (config 'newload 3) base))))
 
@@ -71,44 +75,67 @@
 (define (get-metadata)
   (and (config 'metadata #f) (file->dtype (config 'metadata #f))))
 
-(define (make-new-index filename old keys)
+(define (make-new-index filename old keys (type))
+  (default! type (symbolize (config 'type 'hashindex)))
   (message "Constructing new index " (write filename) " based on "
 	   (index-source old))
-  (cond ((config 'STDINDEX #f)
-	 (make-index filename 
-		     #[type fileindex
-		       slots ,(max (* 2 (length keys))
-				   (get-new-size (length keys)))]))
-	((config 'HASHINDEX #f)
-	 (make-index filename 
-		     `#[type hashindex
-			slots ,(get-new-size (length keys))
-			slotids ,(compute-slotids keys)
-			baseoids ,(sorted baseoids)
-			metadata ,(get-metadata)]))
-	(else
-	 (make-index filename
-		     `#[type hashindex
-			slots ,(get-new-size (length keys))
-			slotids ,(compute-slotids keys)
-			baseoids ,(sorted baseoids)
-			metadata ,(get-metadata)])))
+  (if (eq? type 'stdindex)
+      (make-index filename 
+		  #[type fileindex
+		    slots ,(max (* 2 (length keys))
+				(get-new-size (length keys)))])
+      (make-index filename 
+		  `#[type hashindex
+		     slots ,(get-new-size (length keys))
+		     slotids ,(compute-slotids keys)
+		     baseoids ,(sorted baseoids)
+		     metadata ,(get-metadata)]))
   (open-index filename))
 
-(define (copy-keys keyv old new)
+(define (copy-loop fifo buckets old new (bucket))
+  (while (fifo-live? fifo)
+    (set! bucket (fifo/pop fifo))
+    (do-choices (key (get buckets bucket))
+      (store! new key (get old key)))))
+
+(defambda (process-batch batch batch-size buckets old new (start (elapsed-time)))
+  (message "Processing a batch of " (choice-size batch) 
+    " buckets covering " batch-size " keys")
+  (do-choices (key (get buckets batch))
+    (store! new key (get old key)))
+  (message "Copied " (choice-size batch) " buckets (" batch-size " keys) "
+    "in " (secs->string (elapsed-time start)) ", committing...")
+  (commit new)
+  (swapout new))
+
+(define (copy-keys keyv old new (chunk-size) (start (elapsed-time)))
+  (default! chunk-size (quotient (length keyv) (config 'nchunks 20)))
   (message "Copying " (length keyv) " keys" 
     " from " (or (index-source old) old)
     " into " (or (index-source new) new))
-  (let* ((prefetcher (lambda (keys done)
-		       (when done (commit) (clearcaches))
-		       (unless done
-			 (prefetch-keys! old keys)))))
-    (do-vector-mt (key keyv (mt/threadcount) prefetcher 
-		       (config 'blocksize (quotient (length keyv)
-						    (config 'nchunks 20)))
-		       (mt/custom-progress "Copying keys"))
-      (unless (or (void? (get old key)) (fail? (get old key)))
-	(add! new key (get old key))))))
+  (let ((buckets (make-hashtable (length keyv)))
+	(total-keys 0)
+	(batch-size 0)
+	(batch {}))
+    (message "Generating a bucket map for " (or (index-source new) new))
+    (doseq (key keyv)
+      (add! buckets (indexctl new 'hash key) key))
+    (do-choices (bucket (getkeys buckets) i)
+      (set+! batch bucket)
+      (set! batch-size (+ batch-size (choice-size (get buckets bucket))))
+      (when (> batch-size chunk-size)
+	(process-batch batch batch-size buckets old new)
+	(set! total-keys (+ total-keys batch-size))
+	(set! batch {})
+	(set! batch-size 0)
+	(message "In total, " (show% total-keys (length keyv)) 
+	  " of the keys (" total-keys ") have been processed in "
+	  (secs->string (elapsed-time start)))
+	(message "At this rate, the copy should finish in about "
+	  (secs->string (- (/  (length keyv) (/ total-keys (elapsed-time start)))
+			   (elapsed-time start)))
+	  ", but you know how these things are.")))
+    (process-batch batch batch-size buckets old new)))
 
 (define (main from (to #f))
   (optimize! copy-keys) (optimize! compute-slotids)
@@ -119,7 +146,7 @@
 	 (message "Can't locate source " from))
 	(else
 	 (when (file-exists? to) (remove-file to))
-	 (let* ((old (open-index from))
+	 (let* ((old (open-index from #[cachelevel 3]))
 		(keyv (index-keysvec old))
 		(new (make-new-index to old keyv)))
 	   (copy-keys keyv old new)))))
