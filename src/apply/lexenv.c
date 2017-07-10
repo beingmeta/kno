7f4d5c7b151dabad5861ec38e13ddd1c9cd155af
Ken Haase <kh@beingmeta.com>
2017-07-10 14:27:02 -0400

    Whitespace

--- src/apply/lexenv.c
+++ src/apply/lexenv.c
@@ -144,8 +144,10 @@ int fd_recycle_lexenv(fd_lexenv env)
     int sproc_count = count_envrefs(env->env_bindings,env,env_recycle_depth);
     if (sproc_count+1==refcount) {
       struct FD_RAW_CONS *envstruct = (struct FD_RAW_CONS *)env;
-      fd_decref(env->env_bindings); fd_decref(env->env_exports);
-      if (env->env_parent) fd_decref((lispval)(env->env_parent));
+      fd_decref(env->env_bindings);
+      fd_decref(env->env_exports);
+      if (env->env_parent)
+	fd_decref((lispval)(env->env_parent));
       envstruct->conshead = (0xFFFFFF80|(env->conshead&0x7F));
       u8_free(env);
       return 1;}
