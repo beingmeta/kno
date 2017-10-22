a0d4e27e0c97aa72637d46aaa7094724cd0fd9d8
Ken Haase <kh@beingmeta.com>
2017-10-22 21:21:59 +0000

    Add more sort options, including smarter libu8 sorting

--- include/framerd/sorting.h
+++ include/framerd/sorting.h
@@ -16,6 +16,51 @@ typedef int (*fd_sortfn)(const void *x,const void *y);
 struct FD_SORT_ENTRY {
   lispval sortval, sortkey;};
 
+static int utf8_strcmp(u8_string s1,u8_string s2)
+{
+  u8_string scan1 = s1, scan2 = s2;
+  while ( (*scan1) && (*scan2) ) {
+    if ( (*scan1 < 0x80 ) && (*scan2 < 0x80 ) ) {
+      if (*scan1 < *scan2) return -1;
+      else if (*scan1 > *scan2) return 1;
+      scan1++;
+      scan2++;}
+    else {
+      int c1 = u8_sgetc(&scan1);
+      int c2 = u8_sgetc(&scan2);
+      if (c1<c2) return -1;
+      else if (c1>c2) return 1;}}
+  if (*scan1)
+    return 1;
+  else if (*scan2)
+    return -1;
+  else return 0;
+}
+static int utf8_strcmp_ci(u8_string s1,u8_string s2)
+{
+  u8_string scan1 = s1, scan2 = s2;
+  while ( (*scan1) && (*scan2) ) {
+    if ( (*scan1 < 0x80 ) && (*scan2 < 0x80 ) ) {
+      int lc1 = u8_tolower(*scan1), lc2 = u8_tolower(*scan2);
+      if (lc1 < lc2) return -1;
+      else if (lc1 > lc2) return 1;
+      scan1++;
+      scan2++;}
+    else {
+      int c1 = u8_sgetc(&scan1);
+      int c2 = u8_sgetc(&scan2);
+      c1 = u8_tolower(c1);
+      c2 = u8_tolower(c2);
+      if (c1<c2) return -1;
+      else if (c1>c2) return 1;}}
+  if (*scan1)
+    return 1;
+  else if (*scan2)
+    return -1;
+  else return 0;
+}
+
+
 static int _fd_sort_helper(const void *vx,const void *vy)
 {
   const struct FD_SORT_ENTRY *sx = (struct FD_SORT_ENTRY *)vx;
@@ -35,16 +80,34 @@ static int _fd_sort_helper(const void *vx,const void *vy)
         if (xval<yval) return -1; else return 1;}
       else return FD_FULL_COMPARE(sx->sortkey,sy->sortkey);
     else if ((xtype == fd_fixnum_type) || (xtype == fd_bigint_type) ||
-              (xtype == fd_flonum_type) || (xtype == fd_rational_type) ||
-              (xtype == fd_complex_type))
-      if ((ytype == fd_fixnum_type) || (ytype == fd_bigint_type) ||
-          (ytype == fd_flonum_type) || (ytype == fd_rational_type) ||
-          (ytype == fd_complex_type)) {
+             (xtype == fd_flonum_type) || (xtype == fd_rational_type) ||
+             (xtype == fd_complex_type)) {
+      lispval x = sx->sortkey, y = sy->sortkey;
+      if ( ((xtype == fd_fixnum_type) && (xtype == fd_bigint_type)) ||
+           ((xtype == fd_bigint_type) && (xtype == fd_fixnum_type)) ) {
+        int xneg = ((xtype == fd_fixnum_type) ? (FD_FIX2INT(x)<0) :
+                    (fd_bigint_negativep((fd_bigint)x)));
+        int yneg = ((ytype == fd_fixnum_type) ? (FD_FIX2INT(y)<0) :
+                    (fd_bigint_negativep((fd_bigint)y)));
+        if ((xneg == 0) && (yneg == 0)) {
+          if (xtype == fd_fixnum_type)
+            return -1;
+          else return 1;}
+        else if ((xneg) && (yneg)){
+          if (xtype == fd_fixnum_type)
+            return 1;
+          else return -11;}
+        else if (xneg)
+          return -1;
+        else return 1;}
+      else if ((ytype == fd_fixnum_type) || (ytype == fd_bigint_type) ||
+               (ytype == fd_flonum_type) || (ytype == fd_rational_type) ||
+               (ytype == fd_complex_type)) {
         int cmp = fd_numcompare(sx->sortkey,sy->sortkey);
         if (cmp) return cmp;
         else if (xtype<ytype) return -1;
         else return 1;}
-      else return -1;
+      else return -1;}
     else if ((ytype == fd_fixnum_type) || (ytype == fd_bigint_type) ||
              (ytype == fd_flonum_type) || (ytype == fd_rational_type) ||
              (ytype == fd_complex_type))
@@ -53,6 +116,17 @@ static int _fd_sort_helper(const void *vx,const void *vy)
     else if (xtype>ytype) return 1;
     /* Never reached */
     else return 0;}
+}
+
+static int _fd_pointer_sort_helper(const void *vx,const void *vy)
+{
+  const struct FD_SORT_ENTRY *sx = (struct FD_SORT_ENTRY *)vx;
+  const struct FD_SORT_ENTRY *sy = (struct FD_SORT_ENTRY *)vy;
+  if (sx->sortkey == sy->sortkey)
+    return 0;
+  else if (sx->sortkey < sy->sortkey)
+    return -1;
+  else return 1;
 }
 
 static int _fd_collate_helper(const void *vx,const void *vy)
@@ -118,11 +192,56 @@ static int _fd_lexsort_helper(const void *vx,const void *vy)
         long long yval = FD_FIX2INT(sy->sortkey);
         if (xval<yval) return -1; else return 1;}
       else if (xtype == fd_string_type)
-        return (strcasecmp(FD_STRDATA(sx->sortkey),
-                           FD_STRDATA(sy->sortkey)));
+        return (utf8_strcmp(FD_STRDATA(sx->sortkey),
+                            FD_STRDATA(sy->sortkey)));
       else if (xtype == fd_symbol_type)
-        return (strcasecmp(FD_XSYMBOL_NAME(sx->sortkey),
-                           FD_XSYMBOL_NAME(sy->sortkey)));
+        return (utf8_strcmp(FD_XSYMBOL_NAME(sx->sortkey),
+                            FD_XSYMBOL_NAME(sy->sortkey)));
+      else return FD_FULL_COMPARE(sx->sortkey,sy->sortkey);
+    else if ((xtype == fd_fixnum_type) || (xtype == fd_bigint_type) ||
+              (xtype == fd_flonum_type) || (xtype == fd_rational_type) ||
+              (xtype == fd_complex_type))
+      if ((ytype == fd_fixnum_type) || (ytype == fd_bigint_type) ||
+          (ytype == fd_flonum_type) || (ytype == fd_rational_type) ||
+          (ytype == fd_complex_type)) {
+        int cmp = fd_numcompare(sx->sortkey,sy->sortkey);
+        if (cmp) return cmp;
+        else if (xtype<ytype) return -1;
+        else return 1;}
+      else return -1;
+    else if ((ytype == fd_fixnum_type) || (ytype == fd_bigint_type) ||
+             (ytype == fd_flonum_type) || (ytype == fd_rational_type) ||
+             (ytype == fd_complex_type))
+      return 1;
+    else if (xtype<ytype) return -1;
+    else if (xtype>ytype) return 1;
+    /* Never reached */
+    else return 0;}
+}
+
+static int _fd_lexsort_ci_helper(const void *vx,const void *vy)
+{
+  const struct FD_SORT_ENTRY *sx = (struct FD_SORT_ENTRY *)vx;
+  const struct FD_SORT_ENTRY *sy = (struct FD_SORT_ENTRY *)vy;
+  if (sx->sortkey == sy->sortkey) return 0;
+  else {
+    fd_ptr_type xtype = FD_PTR_TYPE(sx->sortkey);
+    fd_ptr_type ytype = FD_PTR_TYPE(sy->sortkey);
+    if (xtype == ytype)
+      if (FD_OIDP(sx->sortkey)) {
+        FD_OID xaddr = FD_OID_ADDR(sx->sortkey);
+        FD_OID yaddr = FD_OID_ADDR(sy->sortkey);
+        return FD_OID_COMPARE(xaddr,yaddr);}
+      else if (FD_FIXNUMP(sx->sortkey)) {
+        long long xval = FD_FIX2INT(sx->sortkey);
+        long long yval = FD_FIX2INT(sy->sortkey);
+        if (xval<yval) return -1; else return 1;}
+      else if (xtype == fd_string_type)
+        return (utf8_strcmp_ci(FD_STRDATA(sx->sortkey),
+                               FD_STRDATA(sy->sortkey)));
+      else if (xtype == fd_symbol_type)
+        return (utf8_strcmp_ci(FD_XSYMBOL_NAME(sx->sortkey),
+                               FD_XSYMBOL_NAME(sy->sortkey)));
       else return FD_FULL_COMPARE(sx->sortkey,sy->sortkey);
     else if ((xtype == fd_fixnum_type) || (xtype == fd_bigint_type) ||
               (xtype == fd_flonum_type) || (xtype == fd_rational_type) ||
@@ -147,7 +266,10 @@ static int _fd_lexsort_helper(const void *vx,const void *vy)
 
 static lispval _fd_apply_keyfn(lispval x,lispval keyfn)
 {
-  if ((FD_VOIDP(keyfn)) || (FD_EMPTY_CHOICEP(keyfn)))
+  if ((FD_VOIDP(keyfn)) ||
+      (FD_EMPTY_CHOICEP(keyfn)) ||
+      (FD_FALSEP(keyfn)) ||
+      (FD_DEFAULTP(keyfn)))
     return fd_incref(x);
   else if (FD_OIDP(keyfn)) return fd_frame_get(x,keyfn);
   else if (FD_TABLEP(keyfn)) return fd_get(keyfn,x,FD_EMPTY_CHOICE);
