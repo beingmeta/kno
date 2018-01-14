3d156b5de70224ab0a1ce1e410faa144e482478b
Ken Haase <kh@beingmeta.com>
2018-01-13 20:47:18 -0500

    Rework index management functions, switch to more consistent storage/*indexes naming

---
+++ src/libscm/storage/indexes.scm
@@ -0,0 +1,207 @@
+;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2017 beingmeta, inc.  All rights reserved.
+
+(in-module 'storage/indexes)
+
+(use-module '{fifo engine stringfmts storage/hashindexes logger varconfig})
+
+(define-init %loglevel %notice%)
+
+(module-export! '{index/copy! index/pack!
+		  index/copy-keys!
+		  index/install!})
+
+;;; Top level functions
+
+(define (index/copy! from to (opts #f))
+  (let ((in (if (index? from) from
+		(if (string? from)
+		    (open-index from (get-read-opts opts))
+		    (irritant from |NotIndex| index/copy!))))
+	(out (if (index? to) to
+		 (if (string? to)
+		     (open-index to (get-write-opts opts))
+		     (irritant from |NotIndex| index/copy!)))))
+    (if (equal? (indexctl in 'metadata 'type) "hashindex")
+	(hashindex/copy-keys! in out opts)
+	(index/copy-keys! in out opts))))
+
+(define (index/pack! from (to #f) (opts #f))
+  (let* ((in (if (index? from) from
+		 (if (string? from)
+		     (open-index from (get-read-opts opts))
+		     (irritant from |NotIndex| index/copy!))))
+	 (infile (if (string? from) from (index-source in)))
+	 (outfile (or to (index-source in)))
+	 (rarefile (getopt opts 'rarefile))
+	 (uniquefile (getopt opts 'uniquefile))
+	 (inplace (and (file-exists? outfile)
+		       (equal? (realpath infile) (realpath outfile))))
+	 (out (if (and (file-exists? outfile) (not inplace)
+		       (not (getopt opts 'overwrite (config 'OVERWRITE #f))))
+		  (irritant outfile |OutputAlreadyExists|)
+		  (get-new-index outfile in opts)))
+	 (rare (and rarefile (get-new-index rarefile in opts)))
+	 (unique (and uniquefile (get-new-index uniquefile in opts)))
+	 (copy-opts `(#[rare ,rare unique ,unique] . ,opts))
+	 (copier (cond ((testopt opts 'copier 'generic) index/copy-keys!)
+		       ((getopt opts 'copier) (getopt opts 'copier))
+		       ((equal? (indexctl in 'metadata 'type) "hashindex")
+			hashindex/copy-keys!)
+		       (else index/copy-keys!))))
+    (copier in out copy-opts)
+    (index/install! out outfile)
+    (when rare (index/install! rare rarefile))
+    (when unique (index/install! unique uniquefile))))
+
+(define (index/install! index into)
+  (close-index index)
+  (when (file-exists? into)
+    (if (config 'unsafe #f)
+	(remove-file into)
+	(move-file! into into (glom into ".bak"))))
+  (onerror
+      (move-file! (index-source index) into)
+      (lambda (x)
+	(logwarn |RenameFailed|
+	  "Couldn't rename " (index-source index) " to " into ", using shell")
+	(exec/cmd "mv" (index-source index) into))))
+
+;;;; Support functions
+
+(define default-index-size 31415)
+(define default-minsize 100)
+(varconfig! INDEX:MINSIZE default-minsize)
+(varconfig! INDEX:SIZE default-index-size)
+
+(define (get-keycount index)
+  (let ((v (indexctl index 'keycount)))
+    (unless (and (exists? v) v)
+      (set! v (indexctl index 'load)))
+    (unless (and (exists? v) v)
+      (set! v (indexctl index 'metadata 'keys)))
+    (if (and (exists? v) v) v #f)))
+
+(define (get-new-size old opts (minimum))
+  (default! minimum (getopt opts 'minsize (config 'MINSIZE 100)))
+  (let ((specified (getopt opts 'newsize (config 'newsize 2.0))))
+    (if (not (number? specified)) (irritant newsize |BadNewIndexSize|))
+    (max
+     (if (and (exact? specified) (> specified 42))
+	 specified
+	 (let ((keycount (or (get-keycount old) default-index-size)))
+	   (->exact (ceiling (* keycount specified)))))
+     minimum)))
+
+(define (get-new-index filename old opts)
+  (if (file-exists? filename)
+      (begin (logwarn |Existing|
+	       "Using existing output file index " filename)
+	(open-index filename (get-write-opts opts)))
+      (let* ((n-keys (indexctl old 'keycount))
+	     (size (get-new-size old opts))
+	     (type (getopt opts 'type (config 'NEWTYPE (CONFIG 'TYPE 'hashindex))))
+	     (new-opts (frame-create #f
+			 'type type 'size size
+			 'register (getopt opts 'register #t)
+			 'keyslot (indexctl old 'metadata 'keyslot)))
+	     (new (make-index (glom filename ".part") (cons new-opts opts))))
+	(logwarn |NewIndex|
+	  "Created new " type " " filename "(.part) "
+	  "with size " ($num size))
+	new)))
+
+(define (get-read-opts opts)
+  `(#[readonly #t
+      cachelevel ,(getopt opts 'cachelevel (config 'force:cachelevel 2))
+      register ,(getopt opts 'register #t)]
+    . ,opts))
+(define (get-write-opts opts)
+  `(#[readonly #f
+      cachelevel ,(getopt opts 'cachelevel (config 'force:cachelevel 2))
+      register ,(getopt opts 'register #t)]
+    . ,opts))
+
+;;; Default key copier, uses fetchn
+
+(define (key-copier keys batch-state loop-state task-state)
+  (let* ((minval (get loop-state 'minvals))
+	 (maxval (get loop-state 'maxvals))
+	 (unique (try (get loop-state 'unique) #f))
+	 (rare   (try (get loop-state 'rare) #f))
+	 (input  (get loop-state 'input))
+	 (output (get loop-state 'output))
+	 (outhash (make-hashtable (choice-size keys)))
+	 (uniquehash (and unique (make-hashtable (choice-size keys))))
+	 (rarehash (and rare (make-hashtable (choice-size keys))))
+	 (stopkeys (indexctl output 'metadata 'stopkeys))
+	 (copy-count 0)
+	 (unique-count 0)
+	 (rare-count 0)
+	 (value-count 0))
+    (let* ((keyvec (choice->vector (difference keys stopkeys)))
+	   (keyvals (index/fetchn input keyvec)))
+      (if (and (not minval) (not maxval) (not unique) (not rare))
+	  (let ((i 0) (nkeys (length keyvec)))
+	    (while (< i nkeys)
+	      (add! output (elt keyvec i) (elt keyvals i))
+	      (set! value-count (+ value-count (choice-size (elt keyvals i))))
+	      (set! i (1+ i))))
+	  (let ((i 0) (nkeys (length keyvec))
+		(key #f) (vals #f) (nvals 0) (copied #f))
+	    (while (< i nkeys)
+	      (set! key (elt keyvec i))
+	      (set! vals (elt keyvals i))
+	      (set! nvals (choice-size vals))
+	      (set! copied #t)
+	      (if (= nvals 1)
+		  (if uniquehash
+		      (add! uniquehash key vals)
+		      (if (and minval (< minval 1))
+			  (begin (if rarehash (add! rarehash key vals))
+			    (set! rare-count (1+ rare-count)))
+			  (if (or (not minval) (< minval 1))
+			      (add! outhash key vals)
+			      (set! copied #f))))
+		  (if (and minval (< nvals minval))
+		      (begin (if rarehash (add! rarehash key vals))
+			(set! rare-count (1+ rare-count)))
+		      (if (and (or (not minval) (>= nvals minval))
+			       (or (not maxval) (<= nvals maxval)))
+			  (add! outhash key vals)
+			  (set! copied #f))))
+	      (when copied
+		(when (= nvals 1) (set! unique-count (1+ unique-count)))
+		(set! value-count (+ value-count nvals))
+		(set! copy-count (1+ copy-count)))
+	      (set! i (1+ i))))))
+    (index-merge! output outhash)
+    (when uniquehash (index-merge! unique outhash))
+    (when rarehash (index-merge! rare rarehash))
+    (table-increment! batch-state 'copied copy-count)
+    (table-increment! batch-state 'rarekeys rare-count)
+    (table-increment! batch-state 'uniquekeys unique-count)
+    (table-increment! batch-state 'values value-count)))
+
+(define (index/copy-keys! in out (opts #f))
+  (let ((started (elapsed-time))
+	(rare (getopt opts 'rare {}))
+	(unique (getopt opts 'unique {}))
+	(keys (getkeys in)))
+    (lognotice |Copying| (choice-size keys) " keys from " in " to " out)
+    (engine/run key-copier keys
+		`#[loop #[input ,in output ,out
+			  rare ,(getopt opts 'rare)
+			  unique ,(getopt opts 'unique)
+			  maxvals ,(getopt opts 'maxvals)
+			  minvals ,(getopt opts 'minvals)]
+		   counters {copied rarekeys uniquekeys values}
+		   logrates {copied rarekeys uniquekeys values}
+		   batchsize ,(getopt opts 'batchsize (config 'BATCHSIZE 10000))
+		   batchrange ,(getopt opts 'batchrange (config 'BATCHRANGE 8))
+		   nthreads ,(getopt opts 'nthreads (config 'NTHREADS (rusage 'ncpus)))
+		   checktests ,(engine/interval (getopt opts 'savefreq (config 'savefreq 60)))
+		   checkpoint {,out ,unique ,rare}
+		   logfreq ,(getopt opts 'logfreq (config 'LOGFREQ 30))
+		   checkfreq ,(getopt opts 'checkfreq (config 'checkfreq 15))
+		   started ,started])))
--- src/scripts/pack-index.scm
+++ src/scripts/pack-index.scm
@@ -1,312 +1,46 @@
 ;;; -*- Mode: Scheme -*-
 
-(use-module '{optimize mttools logger stringfmts fifo varconfig storage/hashindex})
-
 (config! 'cachelevel 2)
-(config! 'optlevel 4)
-(config! 'logthreadinfo #t)
-(config! 'thread:logexit #f)
+(use-module '{optimize varconfig logger mttools})
+(use-module '{storage/indexes})
 
-(define (getflags)
-  (choice->vector
-   (choice (tryif (config 'dtypev2 #t) 'dtypev2)
-	   (tryif (config 'B32 #f) 'B32)
-	   (tryif (config 'B40 #f) 'B40)
-	   (tryif (config 'B64 #f) 'B64)
-	   )))
+(define (overwriting file)
+  (when (file-exists? file)
+    (cond ((config 'unsafe)
+	   (remove-file! file)
+	   (logwarn |FileExists| "Removed existing file " (write file)))
+	  (else
+	   (onerror
+	       (move-file! file (glom file ".bak"))
+	       (lambda (ex)
+		 (logwarn |FileExists|
+		   "Couldn't back up " file " to " (glom file ".bak"))
+		 (exit)))
+	   (logwarn |FileExists|
+	     "Moved existing file " file " " "to " (glom file ".bak"))))))
 
-(config! 'logprocinfo #t)
-(config! 'logthreadinfo #t)
+(define (main in (out #f))
+  (let ((overwrite (config 'overwrite #f))
+	(opts (frame-create #f
+		'newsize (config 'newsize {})
+		'keyslot (string->symbol (upcase (config 'keyslot {})))
+		'rarefile (config 'rare {})
+		'uniquefile (config 'unique {}))))
+    (when (and out (file-exists? out) (not overwrite))
+      (logwarn |FileExists|
+	"The output file " (write out) " already exists.\n  "
+	"Specify OVERWRITE=yes to remove.")
+      (exit))
+    (when (and overwrite (getopt opts 'unique)
+	       (file-exists? (getopt opts 'unique)))
+      (overwriting (getopt opts 'unique)))
+    (when (and overwrite (getopt opts 'rare)
+	       (file-exists? (getopt opts 'rare)))
+      (overwriting (getopt opts 'rare)))
+    (index/pack! in out opts)))
 
-(define validate-oids #f)
-(varconfig! VALIDATE validate-oids config:boolean)
+(when (config 'optimize #t)
+  (optimize! '{storage/indexes storage/hashindexes ezrecords fifo engine})
+  (optimize!))
 
-(define prefetch-keys #t)
-(varconfig! PREFECTCH prefetch-keys)
-
-(define %loglevel %notice%)
-
-(define last-log (elapsed-time))
-(define log-freq 30)
-(defslambda (dolog?)
-  (cond ((< (elapsed-time last-log) log-freq) #f)
-	(else (set! last-log (elapsed-time)) #t)))
-
-(define (domove from to)
-  (onerror (move-file from to)
-      (lambda (ex) (system "mv " from " " to))))
-
-;;; Computing baseoids 
-
-(define (get-baseoids-from-pool pool)
-  (get-baseoids-from-base (pool-base pool) (pool-capacity pool)))
-
-(define (get-baseoids-from-base base capacity)
-  (choice base
-	  (tryif (> capacity 0)
-		 (get-baseoids-from-base (oid-plus base capacity)
-					 (- capacity (* 1024 1024))))))
-
-(define baseoids {})
-
-(config-def! 'baseoid (slambda (var val)
-			(if (bound? val)
-			    (set+! baseoids val)
-			    baseoids)))
-(config-def! 'pool (slambda (var val)
-		     (if (bound? val)
-			 (set+! baseoids (get-baseoids-from-pool val))
-			 (fail))))
-
-;;; Computing slotids
-
-(define slotids #f)
-
-(config-def! 'slotid
-	     (slambda (var val)
-	       (if (bound? val)
-		   (if (or (symbol? val) (oid? val))
-		       (if slotids
-			   (set! slotids (append slotids (list val)))
-			   (set! slotids (list val)))
-		       (logwarn |BadSlotID| "Can't slotcode " val))
-		   slotids)))
-
-#|
-(define (count-slotid key table)
-  (when (pair? key) (hashtable-increment! table (car key))))
-
-(define (compute-slotids keyvec)
-  (let ((slotids-config (config 'slotids #f)))
-    (if (and (string? slotids-config) (file-exists? slotids-config))
-	(append (if slotids (->vector slotids) #())
-		(file->dtype slotids-config))
-	(let ((table (make-hashtable))
-	      (start (elapsed-time)))
-	  (loginfo |PackIndex/slotids|
-	    "Computing slotids based on " ($num (length keyvec)) " keys")
-	  (doseq (key keyvec)
-	    (when (pair? key) (hashtable-increment! table (car key))))
-	  (lognotice |PackIndex/slotids|
-	    "Found " ($num (choice-size (getkeys table)))
-	    " slotids across " ($num (length keyvec)) " keys in "
-	    (secs->string (elapsed-time start)))
-	  (if slotids (drop! table (elts slotids)))
-	  (append (if slotids (->vector slotids) #())
-		  (rsorted (getkeys table) table))))))
-|#
-
-(define (extend-slotids old keyvec)
-  (let* ((old (or (indexctl old 'slotids) #()))
-	 (filed (if (and (config 'slotids #f)
-			 (file-exists? (config 'slotids #f)))
-		    (file->dtype (config 'slotids #f))
-		    #()))
-	 (config (if slotids (->vector slotids) #()))
-	 (seen (make-hashset)))
-    (remove #f (map (lambda (slotid) 
-		      (if (hashset-get seen slotid) #f
-			  (begin (hashset-add! seen slotid) slotid)))
-		    (append old filed config)))))
-
-;;; Other features
-
-(define (symbolize s)
-  (if (symbol? s) s
-      (if (string? s) (string->symbol (upcase s))
-	  (irritant s |NotStringOrSymbol|))))
-
-(define (get-new-size index nkeys (minsize (config 'minsize 8000)))
-  (when (config 'keepsize config:boolean)
-    (when (number? (indexctl index 'hash))
-      (set! minsize (indexctl index 'hash))))
-  (config 'newsize 
-	  (max (inexact->exact (* (config 'loadfactor 2) nkeys))
-	       minsize)))
-
-(define (get-metadata old)
-  (or (and (config 'metadata #f)
-	   (file->dtype (config 'metadata #f)))
-      (indexctl old 'metadata)))
-
-(define (make-new-index filename old keyvec (size) (type) (keyslot))
-  (default! type (symbolize (config 'type 'hashindex)))
-  (default! size (get-new-size old (length keyvec)))
-  (default! keyslot (->lisp (config 'keyslot (indexctl old 'keyslot))))
-  (lognotice |NewIndex|
-    "Constructing new index " (write filename) " based on "
-    (index-source old))
-  (if (eq? type 'stdindex)
-      (make-index filename `#[type fileindex size ,size
-			      keyslot ,keyslot
-			      metadata ,(get-metadata old)])
-      (make-index filename 
-		  `#[type hashindex size ,size
-		     keyslot ,keyslot
-		     slotids ,(and (config 'codeslots) (extend-slotids old keyvec))
-		     baseoids
-		     ,(and (config 'codeoids (vector? (indexctl old 'baseoids)) config:boolean)
-			   (rsorted baseoids oid-addr))
-		     metadata ,(get-metadata old)]))
-  (open-index filename))
-
-(defambda (copy-all-keys keyv old new (opts #f) (start (elapsed-time)))
-  (let ((batchsize #f)
-	(n-batches #f)
-	(n (length keyv))
-	(modfn (getopt opts 'modfn #f))
-	(changed 0)
-	(removed 0)
-	(count 0))
-
-    (cond ((getopt opts 'batchsize) 
-	   (set! batchsize (getopt opts 'batchsize)))
-	  ((getopt opts 'nbatches) 
-	   (set! n-batches (getopt opts 'nbatches)))
-	  ((config 'batchsize)
-	   (set! batchsize (config 'batchsize)))
-	  ((config 'nbatches)
-	   (set! n-batches (config 'nbatches)))
-	  (else (set! batchsize 100000)))
-
-    (cond (batchsize
-	   (set! n-batches 
-	     (+ (quotient n batchsize)
-		(if (zero? (remainder n batchsize)) 0 1))))
-	  (n-batches
-	   (set! batchsize
-	     (+ (quotient n n-batches)
-		(if (zero? (remainder n n-batches)) 0 1)))))
-
-    (let ((batches (make-vector n-batches #f))
-	  (default-nthreads
-	    (CONFIG 'NTHREADS (max 1 (min (rusage 'ncpus) (quotient n-batches 3)))))
-	  (counter (slambda (delta (modified 0) (dropped 0))
-		     (set! count (+ count delta))
-		     (set! changed (+ changed modified))
-		     (set! removed (+ removed dropped))
-		     `#[count ,count changed ,changed removed ,removed
-			nkeys ,n time ,(elapsed-time start)])))
-
-      (dotimes (i n-batches)
-	(vector-set! batches i 
-		     (qc (elts keyv (* i batchsize) 
-			       (min (* (1+ i) batchsize) n)))))
-
-      (let ((fifo (fifo/make batches `#[fillfn ,fifo/exhausted!]))
-	    (n-threads (mt/threadcount (getopt opts 'nthreads default-nthreads))))
-
-	(lognotice |BatchCopy|
-	  "Copying " ($num n-batches) " batches of "
-	  "~" ($num batchsize) " keys using "
-	  (or n-threads "no") " threads")
-	    
-	(if (and (number? n-threads) (> n-threads 1))
-	    (let ((threads {}))
-	      (dotimes (i n-threads)
-		(set+! threads (thread/call copy-batches old new fifo modfn counter)))
-	      (thread/wait threads))
-	    (copy-batches old new fifo modfn counter))))))
-
-(define (copy-batches from to fifo modfn counter)
-  (let ((batch (fifo/pop fifo))
-	(batch-start (elapsed-time))
-	(modified 0)
-	(dropped 0))
-    (while (exists? batch)
-      (let ((table (make-hashtable (* 2 (choice-size batch)))))
-	(when prefetch-keys (prefetch-keys! from batch))
-	(do-choices (key batch) 
-	  (let ((v (if validate-oids
-		       (pick (get from key) valid-oid?)
-		       (get from key))))
-	    (cond ((fail? v) (set! dropped (1+ dropped)))
-		  ((not modfn) (store! table key (get from key)))
-		  (else (let ((newv (modfn key (qc v))))
-			  (cond ((identical? v newv))
-				((exists? newv)
-				 (set! modified (1+ modified))
-				 (store! table key newv))
-				(else (set! dropped (1+ dropped)))))))))
-	(merge+save! to table)
-	(swapout from batch))
-      (let ((status (counter (choice-size batch) modified dropped)))
-	(loginfo |Batch|
-	  "Copied " ($num (- (choice-size batch) dropped)) " keys "
-	  "in " (secs->string (elapsed-time batch-start))
-	  (unless (zero? dropped)
-	    (printout ", dropping " ($num dropped) " keys"))
-	  (unless (zero? modified)
-	    (printout ", modifying " ($num modified) " keys")))
-	(when (and (> (elapsed-time last-log) log-freq) (dolog?))
-	  (let ((count (get status 'count))
-		(dropped (try (get status 'dropped) 0))
-		(modified (try (get status 'modified) 0))
-		(time (get status 'time))
-		(n (get status 'nkeys)))
-	    (lognotice |Overall|
-	      "Processed " ($num count) " keys" " (" (show% count n) ") "
-	      "in " (secs->string (get status 'time))
-	      (unless (zero? (- count dropped modified))
-		(printout ", copying " ($num (- count dropped modified)) " keys"))
-	      (unless (zero? dropped)
-		(printout ", dropping " ($num dropped) " keys"))))
-	  (unless (zero? modified)
-	    (printout ", modifying " ($count modified) " keys")))
-	(set! batch (fifo/pop fifo))
-	(set! batch-start (elapsed-time))
-	(set! modified 0)
-	(set! dropped 0)))))
-
-(defslambda (merge+save! index table (start (elapsed-time)))
-  (logdebug |Merge+Save|
-    "Merging " (table-size table) " into " index)
-  (index-merge! index table)
-  (logdebug |Merge+Save|
-    "Saving " (table-size table) " merged keys in " index)
-  (commit index)
-  (swapout index)
-  (loginfo |Merge+Save|
-    "Merged and saved " (table-size table) " for " index
-    " in " (secs->string (elapsed-time start))))
-
-(define (main from (to #f))
-  (config! 'appid (glom "pack(" (basename from) ")"))
-  (cond ((not to) (repack-index from))
-	((not (file-exists? from))
-	 (logcrit |PackIndex| "Can't locate source " from))
-	((and (file-exists? to) (not (config 'OVERWRITE #f)))
-	 (logcrit |PackIndex| "Not overwriting " to))
-	(else
-	 (when (file-exists? to) (remove-file to))
-	 (lognotice |PackIndex| "Copying index " from " into " to)
-	 (let* ((old (open-index from `#[repair ,(config 'repair #f config:boolean)]))
-		(part (glom to ".part"))
-		(keyv (index-keysvec old))
-		(new (make-new-index part old keyv)))
-	   (copy-all-keys keyv old new #f)
-	   (commit new)
-	   (domove part to)
-	   new))))
-
-(define (repack-index from)
-  (let* ((base (basename from))
-	 (tmpfile (or (config 'TMPFILE #f)
-		      (and (config 'TMPDIR #f)
-			   (mkpath (config 'TMPDIR) (glom base ".part")))
-		      (glom base ".part")))
-	 (bakfile (or (config 'BAKFILE #f)
-		      (string-append base ".bak"))))
-    (lognotice |PackIndex| 
-      "Repacking the index " from " using " tmpfile 
-      " and saving original in " bakfile)
-    (let* ((old (open-index from `#[repair ,(config 'repair #f config:boolean)]))
-	   (keyv (index-keysvec old))
-	   (new (make-new-index tmpfile old keyv)))
-      (copy-all-keys keyv old new)
-      (commit new))
-    (domove from bakfile)
-    (domove tmpfile from)))
-
-(optimize!)
 
