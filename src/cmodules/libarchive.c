0444d0a80e09d6914802d412632380670d16b84a
Ken Haase <kh@beingmeta.com>
2018-02-23 12:05:05 -0500

    Start implementing libarchive module

--- src/cmodules/libarchive.c
+++ src/cmodules/libarchive.c
@@ -32,6 +32,31 @@ FD_EXPORT int fd_init_libarchive(void) FD_LIBINIT_FN;
 
 static long long int libarchive_initialized = 0;
 
+struct FD_ARCHIVE {
+  FD_CONS_HEADER;
+  u8_string spec;
+  struct archive *fd_archive;
+  u8_mutex archive_lock;
+  lispval archive_opts;} *fd_archive;
+
+#if 0
+static lispval open_archive(lispval spec,lispval opts)
+{
+  struct FD_ARCHIVE *a = u8_alloc(struct FD_ARCHIVE);
+  FD_INIT_FRESH_CONS(a,fd_libarchive_type);
+  if (FD_STRINGP(spec)) {
+    lispval foo;}
+  else if (FD_PACKETP(spec)) {}
+  else {
+    u8_free(a);
+    fd_seterr("InvalidArchiveSpec","open_archive",NULL,spec);
+    return FD_ERROR;}
+  u8_init_mutex(&(a->archive_lock));
+  a->archive_opts = opts; fd_incref(opts);
+  return (lispval) a;
+}
+#endif
+
 FD_EXPORT void fd_init_libarchive_c()
 {
   if (libarchive_initialized)
