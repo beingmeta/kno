02cef00ef841975da3b94306266db6234574392e
Ken Haase <kh@beingmeta.com>
2018-02-24 15:04:42 -0500

    Add readfile, which tries to be smart about whether a data file is a DTYPE or lisp data

---
+++ src/libscm/readfile.scm
@@ -0,0 +1,26 @@
+;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2018 beingmeta, inc.  All rights reserved.
+
+(in-module 'readfile)
+
+(module-export! '{readfile})
+
+(define (readfile file)
+  (and (file-exists? file)
+       (let ((header (onerror (read-bytes (open-dtype-input file) 4) #f)))
+	 (if (and header
+		  (or (< (elt header 0) 0x20)
+		      (and (> (elt header 0) 0x80)
+			   (<= (elt header 0) 0xc0))
+		      (and (= (elt header 0) 0x42) (>= (elt header 1) 0x80 ))
+		      (and (= (elt header 0) 0x41)
+			   (or (< (elt header 1) 0x10)
+			       (>= 0xbF (elt header 1) 0x80)
+			       (and (>= 0x47 (elt header 1) 0x40)
+				    (< (elt header 2) 0x20))))
+		      (and (= (elt header 0) 0x40)
+			   (or (< (elt header 1) 0x10)
+			       (and (<= 0x42 (elt header 1) 0x45)
+				    (< (elt header 2) 0x20))))))
+	     (file->dtype file)
+	     (read (open-input-file file))))))
