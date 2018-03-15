80932a1425d7191a954328d669afb686c6853b2c
Ken Haase <kh@beingmeta.com>
2018-03-15 09:21:24 -0400

    Initial bugjar/servlet

--- src/libscm/bugjar/html.scm
+++ src/libscm/bugjar/html.scm
@@ -1,14 +1,17 @@
 ;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-
-;;; Copyright (C) 2005-2017 beingmeta, inc. All rights reserved
+;;; Copyright (C) 2005-2018 beingmeta, inc. All rights reserved
 
 (in-module 'bugjar/html)
 
-(use-module '{fdweb xhtml})
+(use-module '{fdweb xhtml bugjar condense})
 
-(module-export! '{exception->html})
+(module-export! '{exception/preamble
+		  exception/header 
+		  exception/body
+		  exception->html
+		  exception/page})
 
-(define (exception->html exception)
+(define (exception/preamble exception)
   (htmlheader
    (xmlblock STYLE ((type "text/css"))
      "\n"
@@ -17,6 +20,7 @@
      "div.exception_header span.floatright { float: right; font-size: 80%; clear: right;}\n"
      "div.exception_header h1 { margin-top: 0px; margin-bottom: 0px; }\n"
      "div.exception_header p { margin-left: 2em;  }\n"
+     "div.exception_messages {padding-left: 2em; padding-right: 2em; margin-bottom: 1em; background-color: ivory; color: purple; padding-bottom: 1ex; border-bottom: solid black 1ex;}\n"
      "div#BODY {margin-left: 5vw;}\n"
      "div#BODY div {margin-right: 2em;}\n"
      "h2 { text-align: left; margin-left: -3vw; margin-right: 2em; border-top: double black 2px; font-variant: small-caps; margin-bottom: 1px;}\n"
@@ -25,15 +29,16 @@
      "div#BODY div.stack { margin-left: -2vw; }\n"
      "div.stack { border-top: solid black 1px; margin-bottom: 1em; }\n"
      "div.stack:first-child { border-top: none; }\n"
-     "div.stack pre { display: inline-block; }\n"
+     "div.stack pre { display: inline; margin-bottom: 0px; }\n"
+     "div.stack li pre { display: block; }\n"
      "div.stack span.info { font-size: 70%; float: right; margin-left: 2em; }\n"
      "div.stack span.info span.type { font-weight: bold; ;}\n"
      "div.stack span.label { font-size: 120%; font-weight: bold; padding-right: 1em; ;}\n"
      "div.stack span.op { white-space: nowrap; font-size: 80%; }\n"
      "div.stack div.bindings { margin-left: 2vw; }\n"
-     "div.stack div.bindings .binding { white-space: nowrap; padding-right: 1ex; }\n"
+     "div.stack div.bindings .binding { white-space: nowrap; padding-right: 1ex; display: inline-block; }\n"
      "div.stack div.bindings .binding .name { font-weight: bold; }\n"
-     "div.stack div.bindings .binding .value { display: inline-block; }\n"
+     "div.stack div.bindings .binding .value { display: inline; }\n"
      "div.stack div.bindings .binding .status { font-style: italic; }\n"
      "p,li,ul,ol {text-align: left; margin-right: 2em; margin-top: 1px; margin-bottom: 1px;}\n"))
   (title! (exception-condition exception) " "
@@ -41,6 +46,9 @@
 	       'iso) " "
 	       (when (exception-caller exception)
 		 (printout "- " (exception-caller exception))))
+  (bodyclass! "errorpage"))
+
+(define (exception/header exception)
   (div ((class "exception_header"))
     (when (exception-caller exception)
       (span ((class "floatright caller")) (->string (exception-caller exception))))
@@ -51,11 +59,15 @@
     (h1 (tt (exception-condition exception)))
     (when (exception-details exception)
       (p* ((class "details")) (xmlout (exception-details exception)))))
-  (do-choices (msg (req/get 'messages {}))
-    (cond ((and (string? msg) (position #\< msg)) (xhtml msg))
-	  ((string? msg) (p* ((class "message")) msg))
-	  ((applicable? msg) (msg))
-	  (else (p* ((class "message")) msg))))
+  (when (req/get 'messages)
+    (div ((class "exception_messages"))
+      (do-choices (msg (req/get 'messages {}))
+	(cond ((and (string? msg) (position #\< msg)) (xhtml msg))
+	      ((string? msg) (p* ((class "message")) msg))
+	      ((applicable? msg) (msg))
+	      (else (p* ((class "message")) msg)))))))
+
+(define (exception/body exception)
   (div ((id "BODY"))
     (when (exception-irritant? exception)
       (h2 "Irritant")
@@ -64,14 +76,17 @@
     (do-choices (context (pick (exception-context exception) {slotmap? schemap?}))
       (do-choices (key (getkeys context))
 	(h2 (if (string? key) key (capitalize (downcase key))))
-	(xmlblock "PRE" ((class key))
-	  (stringout (void (pprint (get context key)))))))
+	(let ((value (get context key)))
+	  (if (string? value)
+	      (div ((class "string")) value)
+	      (xmlblock "PRE" ((class key))
+		(stringout (void (pprint value))))))))
     (do-choices (context (reject (exception-context exception) {slotmap? schemap?}))
       (h2 "Context")
       (xmlblock "PRE" ((class "context"))
 	(stringout (void (pprint context)))))
     (h2 "Backtrace")
-    (div ((class "backtrace"))
+    (div ((class "exception_backtrace"))
       (doseq (stack (exception-stack exception))
 	(div ((class "stack"))
 	  (span ((class "info"))
@@ -90,7 +105,7 @@
 				  (compound-ref (stack-env stack) 0)
 				  #f)))))
 	    (when  (and args (sequence? args)  (> (length args) 0))
-	      (xmlblock ul ((class "args")) 
+	      (xmlblock ol ((class "args")) 
 		(doseq (arg args i) (li (output-vals arg)))))
 	    (when env
 	      (div ((class "bindings"))
@@ -103,6 +118,17 @@
 			(output-vals (get env key))))))))
 	  ;;(if (stack-env stack))
 	  )))))
+
+(define (exception->html exception)
+  (exception/preamble exception)
+  (div ((class "exception_body"))
+    (exception/header exception)
+    (exception/body exception)))
+
+(define (exception/page exception)
+  (req/set! 'content-type "text/html")
+  (bodyclass! "errorpage")
+  (exception->html exception))
 
 (defambda (output-vals vals)
   (cond ((fail? vals) 
---
+++ src/libscm/bugjar/servlet.scm
@@ -0,0 +1,53 @@
+;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2018 beingmeta, inc. All rights reserved
+
+(in-module 'bugjar/servlet)
+
+(use-module '{fdweb xhtml texttools})
+(use-module '{varconfig logger})
+(use-module '{gpath condense})
+(use-module '{bugjar bugjar/html})
+(use-module '{aws aws/s3})
+
+(define %loglevel %debug%)
+
+(loginfo |Bugjar/Servlet| "Loading")
+
+(module-export! '{main webmain get-bugroot})
+
+(define bug-buckets
+  #["us-east" "beingmeta-us-east"
+    "us-east-1" "beingmeta-us-east"
+    "us-east-2" "beingmeta-us-east-2"	
+    "US-west" "beingmeta-us-west"
+    "us-northwest" "beingmeta-us-northwest"
+    "eu-east" "beingmeta-eu-east"
+    "eu-west" "beingmeta-eu-west"
+    "asia-south" "beingmeta-asia-south"
+    "asia-japan" "beingmeta-asia-japan"])
+
+(define default-bug-bucket
+  "beingmeta-us-east")
+
+(define (get-bugroot hostname)
+  (if (string-starts-with? 
+       hostname {#({"local" "site"} ".bugs.")
+		 #("bugs." {"local" "site"} ".")})
+      bugjar/saveroot
+      (let ((region (text->frames
+		     #{#((bos) (label (not> ".") region) ".bugs.")
+		       #((bos) "bugs." (label (not> ".") region) ".")})))
+	(s3/loc (try (get bug-buckets (get region 'region)) 
+		     default-bug-bucket)))))
+
+(define (main (http_host (req/get 'http_host))
+	      (request_uri (req/get 'request_uri)))
+  (debug%watch "MAIN" REQUEST_URI)
+  (let* ((root (%wc get-bugroot http_host))
+	 (bugpath (%wc gp/mkpath root (strip-prefix request_uri "/")))
+	 (bugdata (gpath->dtype bugpath))
+	 (expanded (condense/expand bugdata)))
+    (debug%watch "MAIN/OUTPUT" bugpath root bugdata expanded request_uri)
+    (exception/page expanded)))
+
+(define webmain main)
