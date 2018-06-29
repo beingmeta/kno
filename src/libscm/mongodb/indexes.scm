3d4e49e057ddf6b40fe8c4160c26b0f43ca00465
Ken Haase <kh@beingmeta.com>
2018-06-29 10:02:42 -0400

    Add mongodb collection indexes

--- src/libscm/mongodb/indexes.scm
+++ src/libscm/mongodb/indexes.scm
@@ -5,3 +5,15 @@
 
 (use-module '{mongodb logger})
 
+(module-export! '{mongodb/index})
+
+(define (collection-index-fetchfn key.value collection)
+  (get (mongodb/find collection `#[,(car key.value) ,(cdr key.value)]
+	 `#[return #[_id #t]])
+       '_id))
+
+(define (mongodb/index collection (opts #f))
+  (cons-extindex 
+   (glom "index-" (collection/name collection) "@" (mongodb/spec collection))
+   collection-index-fetchfn #f collection #t (opts+ opts 'register #t)))
+
