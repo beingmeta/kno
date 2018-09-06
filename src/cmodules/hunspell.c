3b531791dec2ae8ba0cd474f37d4636182fc7a77
Ken Haase <kh@beingmeta.com>
2018-09-06 11:26:31 -0400

    Fix bad recycle call for hunspell

--- src/cmodules/hunspell.c
+++ src/cmodules/hunspell.c
@@ -27,19 +27,17 @@ FD_EXPORT int fd_init_hunspell(void) FD_LIBINIT_FN;
 
 static u8_string hunspell_dictionaries = "/usr/share/hunspell";
 
-static u8_mutex hunspell_lock;
-
-#define HUNSPELL_LOCK() u8_lock_mutex(&hunspell_lock)
-#define HUNSPELL_UNLOCK() u8_unlock_mutex(&hunspell_lock)
+static void free_suggestions(Hunhandle *hh,char ***suggestions,int n)
+{
+  Hunspell_free_list(hh,suggestions,n);
+}
 
 static lispval hunspell_symbol;
 
 static void recycle_hunspell(void *rawptrval)
 {
-  struct FD_RAWPTR *rawptr = (fd_rawptr) rawptrval;
-  Hunhandle *hh = (Hunhandle *) rawptr->ptrval;
+  Hunhandle *hh = (Hunhandle *) rawptrval;
   if (hh) Hunspell_destroy(hh);
-  rawptr->ptrval = NULL;
 }
 
 static Hunhandle *gethunspell(lispval x)
@@ -61,26 +59,26 @@ static lispval hunspell_open(lispval path,lispval keyboard)
     u8_string try_path = u8_mkpath(hunspell_dictionaries,aff_path);
     if (u8_file_existsp(try_path)) {
       u8_string try_dic = u8_mkpath(hunspell_dictionaries,dic_path);
-      u8_free(aff_path); u8_free(dic_path);
+      u8_free(aff_path);
+      u8_free(dic_path);
       aff_path = try_path;
       dic_path = try_dic;}
     else {
       fd_seterr("NoHunspellDictionaries","hunspell_open",base,FD_VOID);
       return FD_ERROR_VALUE;}}
-  HUNSPELL_LOCK();
   Hunhandle *hh = (FD_VOIDP(keyboard)) ?
     (Hunspell_create(aff_path,dic_path)) :
     (Hunspell_create_key(aff_path,dic_path,FD_CSTRING(keyboard)));
-  HUNSPELL_UNLOCK();
   if (hh == NULL) {
     if (errno) u8_graberrno("hunspell_open",u8_strdup(dic_path));
     u8_free(aff_path); u8_free(dic_path);
     return fd_err("HunspellOpenFailed","hunspell_open",NULL,FD_VOID);}
   lispval result =
     fd_wrap_pointer(hh,recycle_hunspell,
-		    hunspell_symbol,
-		    dic_path);
-  u8_free(aff_path); u8_free(dic_path);
+                    hunspell_symbol,
+                    dic_path);
+  u8_free(aff_path);
+  u8_free(dic_path);
   return result;
 }
 
@@ -88,9 +86,8 @@ static lispval hunspell_open(lispval path,lispval keyboard)
 static lispval hunspell_add_dictionary(lispval h,lispval dictpath)
 {
   Hunhandle *hh = gethunspell(h);
-  HUNSPELL_LOCK();
+  if (hh == NULL) return FD_ERROR;
   int status = Hunspell_add_dic(hh,FD_CSTRING(dictpath));
-  HUNSPELL_UNLOCK();
   if (status == 1)
     return FD_FALSE;
   else return fd_incref(h);
@@ -102,9 +99,7 @@ static lispval hunspell_check(lispval word,lispval hunspell)
 {
   Hunhandle *hh = gethunspell(hunspell);
   if (hh == NULL) return FD_ERROR_VALUE;
-  HUNSPELL_LOCK();
   int rv = Hunspell_spell(hh,FD_CSTRING(word));
-  HUNSPELL_UNLOCK();
   if (rv)
     return FD_TRUE;
   else return FD_FALSE;
@@ -116,9 +111,7 @@ static lispval hunspell_suggest(lispval word,lispval hunspell)
   if (hh == NULL) return FD_ERROR_VALUE;
   char **suggestions = NULL;
   int n = 0;
-  HUNSPELL_LOCK();
-  if ( (n=(Hunspell_suggest(hh,&suggestions,FD_CSTRING(word)))) ) {
-    HUNSPELL_UNLOCK();
+  if ( (n=(Hunspell_suggest(hh,&suggestions,FD_CSTRING(word)))) > 0) {
     if (n == 0) return FD_FALSE;
     int i = 0;
     lispval result = fd_make_vector(n,NULL);
@@ -128,30 +121,27 @@ static lispval hunspell_suggest(lispval word,lispval hunspell)
       lispval string = fd_make_string(NULL,-1,suggestion);
       FD_VECTOR_SET(result,i,string);
       i++; scan++;}
-    HUNSPELL_LOCK();
-    Hunspell_free_list(hh,&suggestions,n);
-    HUNSPELL_UNLOCK();
+    free_suggestions(hh,&suggestions,n);
+    suggestions = NULL;
     return result;}
-  else return FD_FALSE;
+  return fd_empty_vector(0);
 }
 
 static lispval hunspell_add_word(lispval h,lispval word,lispval example)
 {
   Hunhandle *hh = gethunspell(h);
-  HUNSPELL_LOCK();
+  if (hh == NULL) return FD_ERROR;
   int status = (FD_VOIDP(example)) ?
     (Hunspell_add(hh,FD_CSTRING(word))) :
     (Hunspell_add_with_affix(hh,FD_CSTRING(word),FD_CSTRING(example)));
-  HUNSPELL_UNLOCK();
   return FD_INT(status);
 }
 
 static lispval hunspell_remove_word(lispval h,lispval word)
 {
   Hunhandle *hh = gethunspell(h);
-  HUNSPELL_LOCK();
+  if (hh == NULL) return FD_ERROR;
   int status = Hunspell_remove(hh,FD_CSTRING(word));
-  HUNSPELL_UNLOCK();
   return FD_INT(status);
 }
 
@@ -161,48 +151,46 @@ static int hunspell_init = 0;
 
 FD_EXPORT int fd_init_hunspell()
 {
-  if (hunspell_init) 
+  if (hunspell_init)
     return 0;
   else hunspell_init = u8_millitime();
   lispval hunspell_module =
     fd_new_cmodule("HUNSPELL",(FD_MODULE_SAFE),fd_init_hunspell);;
-  
-  u8_init_mutex(&hunspell_lock);
 
   fd_idefn2(hunspell_module,"HUNSPELL/OPEN",hunspell_open,1,
-	    "(hunspell/open *affixes* *dictionary* [*key*]) "
-	    "Opens (creates) a spellchecker",
-	    fd_string_type,FD_VOID,fd_string_type,FD_VOID);
-  
+            "(hunspell/open *affixes* *dictionary* [*key*]) "
+            "Opens (creates) a spellchecker",
+            fd_string_type,FD_VOID,fd_string_type,FD_VOID);
+
 #if HAVE_HUNSPELL_ADD_DIC
   fd_idefn2(hunspell_module,"HUNSPELL/DICTIONARY!",hunspell_add_dictionary,1,
-	    "(hunspell/dictionary! *hunspeller* *dictionary*) "
-	    "Opens (creates) a spellchecker",
-	    fd_rawptr_type,FD_VOID,fd_string_type,FD_VOID);
+            "(hunspell/dictionary! *hunspeller* *dictionary*) "
+            "Opens (creates) a spellchecker",
+            fd_rawptr_type,FD_VOID,fd_string_type,FD_VOID);
 #endif
-  
+
   fd_idefn2(hunspell_module,"HUNSPELL/CHECK",hunspell_check,2,
-	    "(hunspell/check *hunspell* *word*) "
-	    "Opens (creates) a spellchecker",
-	    fd_string_type,FD_VOID,
-	    fd_rawptr_type,FD_VOID);
-  
+            "(hunspell/check *hunspell* *word*) "
+            "Opens (creates) a spellchecker",
+            fd_string_type,FD_VOID,
+            fd_rawptr_type,FD_VOID);
+
   fd_idefn2(hunspell_module,"HUNSPELL/SUGGEST",hunspell_suggest,2,
-	    "(hunspell/check *hunspell* *word*) "
-	    "Opens (creates) a spellchecker",
-	    fd_string_type,FD_VOID,
-	    fd_rawptr_type,FD_VOID);
-  
+            "(hunspell/check *hunspell* *word*) "
+            "Opens (creates) a spellchecker",
+            fd_string_type,FD_VOID,
+            fd_rawptr_type,FD_VOID);
+
   fd_idefn3(hunspell_module,"HUNSPELL/ADD!",hunspell_add_word,2,
-	    "(hunspell/add! *hunspell* *word* *justlike*) ",
-	    fd_rawptr_type,FD_VOID,
-	    fd_string_type,FD_VOID,
-	    fd_string_type,FD_VOID);
+            "(hunspell/add! *hunspell* *word* *justlike*) ",
+            fd_rawptr_type,FD_VOID,
+            fd_string_type,FD_VOID,
+            fd_string_type,FD_VOID);
 
   fd_idefn2(hunspell_module,"HUNSPELL/REMOVE!",hunspell_remove_word,2,
-	    "(hunspell/remove! *hunspell* *word*) ",
-	    fd_rawptr_type,FD_VOID,
-	    fd_string_type,FD_VOID);
+            "(hunspell/remove! *hunspell* *word*) ",
+            fd_rawptr_type,FD_VOID,
+            fd_string_type,FD_VOID);
 
   hunspell_symbol = fd_intern("HUNSPELL");
 
@@ -212,3 +200,10 @@ FD_EXPORT int fd_init_hunspell()
 
   return 1;
 }
+
+/* Emacs local variables
+   ;;;  Local variables: ***
+   ;;;  compile-command: "make -C ../.. debugging;" ***
+   ;;;  indent-tabs-mode: nil ***
+   ;;;  End: ***
+*/
