8f7b4681168d7a98a476f653a3c69720de0a5595
Ken Haase <kh@beingmeta.com>
2019-03-11 12:40:10 -0400

    Add U8_MAYBE_UNUSED to type signature defs

--- include/framerd/cprims.h
+++ include/framerd/cprims.h
@@ -33,84 +33,84 @@
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__),                         \
     docstring, ((flags)&0x7f), flags};                                  \
-  static int *cname ## _typeinfo = NULL;                          \
-  static lispval *cname ## _defaults = NULL
+  static U8_MAYBE_UNUSED int *cname ## _typeinfo = NULL;                          \
+  static U8_MAYBE_UNUSED lispval *cname ## _defaults = NULL
 
 #define FD_DEFPRIM(pname,cname,flags,docstring)                         \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__),                         \
     docstring, ((flags)&0x7f), flags};                                  \
-  static int *cname ## _typeinfo = NULL;                          \
-  static lispval *cname ## _defaults = NULL;                      \
+  static U8_MAYBE_UNUSED int *cname ## _typeinfo = NULL;                          \
+  static U8_MAYBE_UNUSED lispval *cname ## _defaults = NULL;                      \
   static lispval cname
 
 #define FD_DEFPRIM1(pname,cname,flags,docstring,t1,d1)                  \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 1, flags};   \
-  static int cname ## _typeinfo[1] = { t1 };                      \
-  static lispval cname ## _defaults[1] = { d1 };                  \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[1] = { t1 };                      \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[1] = { d1 };                  \
   static lispval cname
 
 #define FD_DEFPRIM2(pname,cname,flags,docstring,t1,d1,t2,d2)            \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 2, flags};   \
-  static int cname ## _typeinfo[2] = { t1, t2 };                  \
-  static lispval cname ## _defaults[2] = { d1, d2 };              \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[2] = { t1, t2 };                  \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[2] = { d1, d2 };              \
   static lispval cname
 
 #define FD_DEFPRIM3(pname,cname,flags,docstring,t1,d1,t2,d2,t3,d3)      \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 3, flags};   \
-  static int cname ## _typeinfo[3] = { t1, t2, t3 };              \
-  static lispval cname ## _defaults[3] = { d1, d2, d3 };          \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[3] = { t1, t2, t3 };              \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[3] = { d1, d2, d3 };          \
   static lispval cname
 
 #define FD_DEFPRIM4(pname,cname,flags,docstring,t1,d1,t2,d2,t3,d3,t4,d4) \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 4, flags};   \
-  static int cname ## _typeinfo[4] = { t1, t2, t3, t4 };          \
-  static lispval cname ## _defaults[4] = { d1, d2, d3, d4 };      \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[4] = { t1, t2, t3, t4 };          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[4] = { d1, d2, d3, d4 };      \
   static lispval cname
 
 #define FD_DEFPRIM5(pname,cname,flags,docstring,                        \
                     t1,d1,t2,d2,t3,d3,t4,d4,t5,d5)                      \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 5, flags};   \
-  static int cname ## _typeinfo[5] = { t1, t2, t3, t4, t5 };      \
-  static lispval cname ## _defaults[5] = { d1, d2, d3, d4, d5 };  \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[5] = { t1, t2, t3, t4, t5 };      \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[5] = { d1, d2, d3, d4, d5 };  \
   static lispval cname
 
 #define FD_DEFPRIM6(pname,cname,flags,docstring,                        \
                     t1,d1,t2,d2,t3,d3,t4,d4,t5,d5,t6,d6)                \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 6, flags};   \
-  static int cname ## _typeinfo[6] = { t1, t2, t3, t4, t5, t6 };  \
-  static lispval cname ## _defaults[6] = { d1, d2, d3, d4, d5, d6 }; \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[6] = { t1, t2, t3, t4, t5, t6 };  \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[6] = { d1, d2, d3, d4, d5, d6 }; \
   static lispval cname
 
 #define FD_DEFPRIM7(pname,cname,flags,docstring,t1,d1,t2,d2,t3,d3,      \
                     t4,d4,t5,d5,t6,d6,t7,d7)                            \
   static struct FD_CPRIM_INFO cname ## _info = {                        \
     pname const, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 7, flags}; \
-  static int cname ## _typeinfo[7] = { t1, t2, t3, t4, t5, t6, t7 }; \
-  static lispval cname ## _defaults[7] = { d1, d2, d3, d4, d5, d6, d7 }; \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[7] = { t1, t2, t3, t4, t5, t6, t7 }; \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] = { d1, d2, d3, d4, d5, d6, d7 }; \
   static lispval cname
 
 #define FD_DEFPRIM8(pname,cname,flags,docstring,t1,d1,t2,d2,      \
                     t3,d3,t4,d4,t5,d5,t6,d6,t7,d7,t8,d8)                \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     const, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 8, flags};   \
-  static int cname ## _typeinfo[8] = { t1, t2, t3, t4, t5, t6, t7, t8 }; \
-  static lispval cname ## _defaults[8] = { d1, d2, d3, d4, d5, d6, d7, d8 }; \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[8] = { t1, t2, t3, t4, t5, t6, t7, t8 }; \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[8] = { d1, d2, d3, d4, d5, d6, d7, d8 }; \
   static lispval cname
 
 #define FD_DEFPRIM9(pname,cname,flags,docstring,t1,d1,t2,d2,      \
                     t3,d3,t4,d4,t5,d5,t6,d6,t7,d7,t8,d8,t9,d9)          \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 9, flags};   \
-  static int cname ## _typeinfo[9] =                              \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[9] =                              \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9 };                             \
-  static lispval cname ## _defaults[9] =                                \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[9] =                                \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9 };                             \
   static lispval cname
 
@@ -118,9 +118,9 @@
                      t3,d3,t4,d4,t5,d5,t6,d6,t7,d7,t8,d8,t9,d9,t10,d10) \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 10, flags};  \
-  static int cname ## _typeinfo[10] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[10] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10 };                        \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10 };                        \
   static lispval cname
 
@@ -129,9 +129,9 @@
                      t10,d10,t11,d11)                                   \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 11, flags};  \
-  static int cname ## _typeinfo[11] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[11] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11 };                   \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11 };                   \
   static lispval cname
 
@@ -140,9 +140,9 @@
                      t10,d10,t11,d11,t12,d12)                           \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 12, flags};  \
-  static int cname ## _typeinfo[12] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[12] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12 };              \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12 };              \
   static lispval cname
 
@@ -151,9 +151,9 @@
                      t10,d10,t11,d11,t12,d12,t13,d13)                   \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 13, flags};  \
-  static int cname ## _typeinfo[12] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[12] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13 };         \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13 };         \
   static lispval cname
 
@@ -162,9 +162,9 @@
                      t10,d10,t11,d11,t12,d12,t13,d13,t14,d14)           \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 14, flags};  \
-  static int cname ## _typeinfo[12] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[12] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14 };    \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14 };    \
   static lispval cname
 
@@ -173,9 +173,9 @@
                      t10,d10,t11,d11,t12,d12,t13,d13,t14,d14,t15,d15)   \
   static struct FD_CPRIM_INFO cname ## _info = {                  \
     pname, _FILEINFO " L#" STRINGIFY(__LINE__), docstring, 15, flags};  \
-  static int cname ## _typeinfo[12] =                             \
+  static U8_MAYBE_UNUSED int cname## _typeinfo[12] =                             \
     { t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15 }; \
-  static lispval cname ## _defaults[7] =                          \
+  static U8_MAYBE_UNUSED lispval cname ## _defaults[7] =                          \
     { d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15 }; \
   static lispval cname
 
