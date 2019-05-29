6a2461c69eb97e3b2b4a6dca513ddd98b88dec52
Ken Haase <kh@beingmeta.com>
2019-05-29 14:53:21 -0400

    Add startup/appenv/config tests

---
+++ tests/startup.scm
@@ -0,0 +1,27 @@
+;;; -*- Mode: Scheme; -*-
+
+(load-component "common.scm")
+
+(define-init foo #f)
+(define-init bar #f)
+
+(applytest #t timestamp? foo)
+(applytest #t timestamp? bar)
+(applytest #t time>? bar foo)
+
+(applytest #t timestamp? (config 'startup.cfg))
+(applytest (get (timestamp) 'year) config 'startup.cfg.year)
+
+(evaltest #t (bound? gpath?))
+(evaltest #t (bound? $size))
+(evaltest #t (bound? gather))
+
+(applytest (getenv "USER") config 'envuser)
+(applytest (getenv "USER") config 'envuser1)
+(applytest #t number? (config 'loadpid))
+(applytest (abspath "r4rs.scm") config 'r4rs)
+
+(applytest #t environment? (%appenv))
+(applytest #t test (%appenv) 'foo)
+
+(message "Finished startup/config testing")
