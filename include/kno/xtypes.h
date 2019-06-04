7ca4648f2969ddbc25c439bc7c807aa04593cec5
Ken Haase <kh@beingmeta.com>
2019-06-04 12:06:41 -0400

    Updated xtypes a little

--- include/kno/xtypes.h
+++ include/kno/xtypes.h
@@ -32,74 +32,68 @@ typedef enum xt_type_code {
   xt_default = 0x05,
   xt_void = 0x06,
   xt_qempty = 0x07,
-  /* String types */
-  xt_utf8_b = 0x10,
-  xt_utf8_bb = 0x1,
-  xt_utf8_bbbb = 0x12,
-  xt_utf8_v = 0x13,
-  xt_packet_b = 0x14,
-  xt_packet_bb = 0x15,
-  xt_packet_bbbb = 0x16,
-  xt_packet_v = 0x17,
-  xt_secret_b = 0x18,
-  xt_secret_bb = 0x19,
-  xt_secret_bbbb = 0x1a,
-  xt_secret_v = 0x1b,
-  xt_u8symbol_b = 0x18,
-  xt_u8symbol_bb = 0x19,
-  xt_u8symbol_bbbb = 0x1a,
-  xt_u8symbol_v = 0x1b,
   /* Scalars */
-  xt_fixnum_b = 0x20,
-  xt_fixnum_bb = 0x21,
-  xt_fixnum_bbbb = 0x22,
-  xt_fixnum_v = 0x23,
-  xt_bigint_b = 0x24,
-  xt_bigint_bb = 0x25,
-  xt_bigint_bbbb = 0x26,
-  xt_flonum_bbbb = 0x27,
-  xt_flonum_bbbbbbbb = 0x28,
-  xt_character_b = 0x2c,
-  xt_character_bb = 0x2d,
-  xt_character_bbbb = 0x2e,
-  xt_character_v = 0x2f,
-  /* Pair types */
-  xt_pair = 0x30,
-  xt_rational = 0x31,
-  xt_complex = 0x32,
-  xt_xycoord = 0x33,
-  xt_tagged = 0x34,
-  xt_compressed = 0x35,
-  xt_encrypted = 0x36,
-  /* Repeated types */
-  xt_vector_b = 0x40,
-  xt_vector_bb = 0x41,
-  xt_vector_bbbb = 0x42,
-  xt_vector_v = 0x43,
-  xt_choice_b = 0x40,
-  xt_choice_bb = 0x41,
-  xt_choice_bbbb = 0x42,
-  xt_choice_v = 0x43,
-  /* Tables */
-  xt_table_b = 0x50,
-  xt_table_bb = 0x51,
-  xt_table_bbbb = 0x52,
-  xt_table_v = 0x53,
+  xt_fixnum_b = 0x10,
+  xt_fixnum_bb = 0x11,
+  xt_fixnum_bbb = 0x12,
+  xt_fixnum_bbbb = 0x13,
+  xt_posint_v = 0x14,
+  xt_negint_v = 0x15,
+  xt_flonum_bbbb = 0x16,
+  xt_flonum_bbbbbbbb = 0x17,
+  xt_character_b = 0x18,
+  xt_character_bb = 0x19,
+  xt_character_bbbb = 0x1a,
+  xt_character_v = 0x1b,
   /* Refs */
-  xt_ref_b = 0x60,
-  xt_ref_bb = 0x61,
-  xt_ref_bbb = 0x62,
-  xt_ref_bbbb = 0x63,
-  xt_ref_bbbbb = 0x64,
-  xt_ref_bbbbbb = 0x65,
-  xt_ref_bbbbbbb = 0x66,
-  xt_ref_bbbbbbbb = 0x67,
+  xt_ref_b = 0x20,
+  xt_ref_bb = 0x21,
+  xt_ref_bbb = 0x22,
+  xt_ref_v = 0x23,
+  /* String types */
+  xt_utf8_b = 0x30,
+  xt_utf8_bb = 0x3,
+  xt_utf8_bbbb = 0x32,
+  xt_utf8_v = 0x33,
+  xt_packet_b = 0x34,
+  xt_packet_bb = 0x35,
+  xt_packet_bbbb = 0x36,
+  xt_packet_v = 0x37,
+  xt_secret_b = 0x38,
+  xt_secret_bb = 0x39,
+  xt_secret_bbbb = 0x3a,
+  xt_secret_v = 0x3b,
+  xt_u8symbol_b = 0x38,
+  xt_u8symbol_bb = 0x39,
+  xt_u8symbol_bbbb = 0x3a,
+  xt_u8symbol_v = 0x3b,
+  /* Pair types */
+  xt_pair = 0x40,
+  xt_rational = 0x41,
+  xt_complex = 0x42,
+  xt_tagged = 0x43,
+  xt_compressed = 0x44,
+  xt_encrypted = 0x45,
+  xt_media = 0x46,
+  /* Repeated types */
+  xt_vector_b = 0x50,
+  xt_vector_bb = 0x51,
+  xt_vector_bbb = 0x52,
+  xt_vector_v = 0x53,
+  xt_choice_b = 0x54,
+  xt_choice_bb = 0x55,
+  xt_choice_bbb = 0x56,
+  xt_choice_v = 0x57,
+  xt_table_b = 0x58,
+  xt_table_bb = 0x59,
+  xt_table_bbb = 0x5a,
+  xt_table_v = 0x5b,
   /* OID refs */
-  xt_oid = 0x68, /* 8 bytes */
-  xt_oid_b = 0x69, /* has form base (xtype) + off (bytes) */
-  xt_oid_bb = 0x6a, /* ... */
-  xt_oid_bbb = 0x6b, /* ... */
-  xt_oid_bbbb = 0x6c, /* ... */
+  xt_oid = 0x60, /* 8 bytes */
+  xt_poolref_b = 0x64, /* has form off (bytes) + base (xtype) */
+  xt_poolref_bb = 0x65, /* ... */
+  xt_poolref_bbb = 0x66, /* ... */
+  xt_poolrev_bbbb = 0x67, /* ... */
   /* Type codes */
   xt_type_short16_vec = 0x70,
   xt_type_int32_vec = 0x71,
@@ -117,6 +111,13 @@ typedef struct XTYPE_REFS {
   struct KNO_HASHTABLE xt_lookup;} XTYPE_REFS;
 typedef struct XTYPE_REFS *xtype_refs;
 
+/* XTYPE flags */
+
+#define XTYPE_FLAGS_BASE      (KNO_DTYPEIO_FLAGS << 1)
+#define XTYPE_WRITE_OPAQUE    (XTYPE_FLAGS_BASE)
+#define XTYPE_NATSORT_VALUES  (XTYPE_FLAGS_BASE << 1)
+#define XTYPE_USE_XTREFS      (XTYPE_FLAGS_BASE << 2)
+#define XTYPE_ADD_XTREFS      (XTYPE_FLAGS_BASE << 3)
 
 /* The top level functions */
 
