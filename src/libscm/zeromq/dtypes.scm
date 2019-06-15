32a20047ff1b10635bd6aee866d85e02c733ead2
Ken Haase <kh@beingmeta.com>
2019-06-15 18:27:15 -0400

    Added initial dtype server framework for zeromq. Sweet!

---
+++ src/libscm/zeromq/dtypes.scm
@@ -0,0 +1,23 @@
+;;; -*- Mode: Scheme; -*-
+
+(in-module 'zeromq/dtypes)
+
+(use-module '{zeromq logger varconfig})
+
+(module-export! '{zmq/dtserver zmq/dtclient zmq/dteval})
+
+(define (zmq/dtserver addr)
+  (let* ((socket (zmq/listen addr 'reply))
+	 (packet (zmq/recv socket)))
+    (while packet
+      (let* ((expr (packet->dtype packet))
+	     (result (eval expr)))
+	(zmq/send! socket (dtype->packet result) ))
+      (set! packet (zmq/recv socket)))))
+
+(define (zmq/dtclient addr)
+  (zmq/open addr 'request))
+
+(define (zmq/dteval expr client)
+  (zmq/send! client (dtype->packet expr))
+  (packet->dtype (zmq/recv client)))
