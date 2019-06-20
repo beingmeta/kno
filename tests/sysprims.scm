356934689fdd52bb161b30260419d39df1cc9277
Ken Haase <kh@beingmeta.com>
2019-06-20 16:10:31 -0400

    Improve coverage

--- tests/sysprims.scm
+++ tests/sysprims.scm
@@ -16,11 +16,13 @@
 	    ((inexact? v)
 	     (applytest #t inexact? cmp))
 	    ((vector? v)
-	     (applytest #t vector? cmp))))))
+	     (applytest #t vector? cmp)))))
+  (applytest flonum? cpusage usage))
 
 (applytest #t table? (uname))
 (applytest #t string? (get (uname) 'osname))
 (applytest #t fixnum? (getpid))
+(applytest #t fixnum? (getppid))
 (applytest #t integer? (stacksize))
 (applytest #t integer? (threadid))
 (applytest #t string? (procstring))
@@ -31,5 +33,17 @@
 (applytest #t inexact? (usertime))
 (applytest #t inexact? (systime))
 (applytest #t inexact? (cpusage))
+
+(errtest (sleep -5))
+(errtest (sleep -5.0))
+
+(define triplet #((isdigit) (opt (isdigit)) (opt (isdigit))))
+(define ipv4 (vector triplet triplet triplet triplet))
+
+(define (ip-addr? string)
+  (and (string? string) (textmatch ipv4 string)))
+
+(applytest-pred string? hostname)
+(applytest {} hostaddrs "beingmeta.cox")
 
 (test-finished "SYSPRIMS")
