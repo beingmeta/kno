406c2431b1ee6dbb3e48cff3b7645e54029c65f2
Ken Haase <kh@beingmeta.com>
2019-06-20 08:49:58 -0400

    Add support for more capi testing, tests of modules.c

--- src/cmodules/testcapi.c
+++ src/cmodules/testcapi.c
@@ -16,13 +16,15 @@
 #include "kno/cprims.h"
 #include "kno/knoregex.h"
 
+#include "libu8/u8logging.h"
+
 KNO_EXPORT int kno_init_testcapi(void) KNO_LIBINIT_FN;
 
 static long long int testcapi_init = 0;
 
 KNO_DCLPRIM("REGEX/TESTCAPI",regex_testcapi,MAX_ARGS(0)|MIN_ARGS(0),
-	    "Run various tests of the C API which are difficult to do "
-	    "directly from scheme")
+	    "Run various tests of the C API for regexes "
+	    "which are difficult to do directly from scheme")
 static lispval regex_testcapi()
 {
   lispval pattern = knostring("[0123456789]+");
@@ -49,6 +51,87 @@ static lispval regex_testcapi()
   return VOID;
 }
 
+static int check_error(u8_string cxt,lispval result)
+{
+  int rv = 1;
+  if (KNO_ABORTP(result)) {
+    u8_exception ex = u8_erreify();
+    lispval errobj = kno_get_exception(ex);
+    u8_log(LOGNOTICE,cxt,"Expected error %q",errobj);
+    u8_free_exception(ex,0);
+    rv = 0;}
+  else u8_log(LOG_ERROR,cxt,
+	      "Test failed to generate expected error, returned %q",
+	      result);
+  kno_decref(result);
+  return rv;
+}
+
+typedef void (*ptr_freefn)(void *);
+#define freefn(x) ((ptr_freefn)(x))
+
+static int check_null(u8_string cxt,void *result,ptr_freefn fn)
+{
+  int rv = 1;
+  if (result==NULL) {
+    u8_exception ex = u8_erreify();
+    lispval errobj = kno_get_exception(ex);
+    u8_log(LOGNOTICE,cxt,"Expected error %q",errobj);
+    u8_free_exception(ex,0);
+    rv = 0;}
+  else u8_log(LOG_ERROR,cxt,"Test failed to generate expected error",result);
+  if (fn) fn(result);
+  return rv;
+}
+
+static int check_result(u8_string cxt,lispval result,lispval expected)
+{
+  int rv = 1;
+  if (KNO_EQUALP(result,expected)) {
+    u8_log(LOGNOTICE,cxt,"Got expected result");
+    rv = 0;}
+  else u8_log(LOGERR,cxt,"Unexpected result %q",result);
+  kno_decref(result);
+  return rv;
+}
+
+typedef int (*testp)(lispval);
+
+static int check_resultp(u8_string cxt,lispval result,testp tester)
+{
+  int rv = 1;
+  if (tester(result)) {
+    u8_log(LOGNOTICE,cxt,"Got expected result");
+    rv = 0;}
+  else u8_log(LOGERR,cxt,"Unexpected result %q",result);
+  kno_decref(result);
+  return rv;
+}
+
+static int lexenvp(lispval x) { return (KNO_LEXENVP(x)); }
+
+KNO_DCLPRIM("MODULES/TESTCAPI",modules_testcapi,MAX_ARGS(0)|MIN_ARGS(0),
+	    "Run various tests of the module C API which are "
+	    "difficult to do directly from scheme")
+static lispval modules_testcapi()
+{
+  int errors = 0;
+  lispval astring = knostring("some string");
+  errors += check_null("kno_make_env(#f)",kno_make_env(KNO_FALSE,NULL),
+		       freefn(kno_free_lexenv));
+  errors += check_null("kno_make_env(string)",kno_make_env(astring,NULL),
+		       freefn(kno_free_lexenv));
+  errors += check_null("kno_make_export_env(#f)",
+		       kno_make_export_env(KNO_FALSE,NULL),
+		       freefn(kno_free_lexenv));
+  errors += check_null("kno_make_export_env(#f)",
+		       kno_make_export_env(astring,NULL),
+		       freefn(kno_free_lexenv));
+  if (errors == 0)
+    return KNO_FALSE;
+  else return KNO_INT(errors);
+}
+
 KNO_EXPORT int kno_init_testcapi()
 {
   if (testcapi_init) return 0;
@@ -56,6 +139,7 @@ KNO_EXPORT int kno_init_testcapi()
   /* u8_register_source_file(_FILEINFO); */
 
   KNO_DECL_PRIM(regex_testcapi,0,testcapi_module);
+  KNO_DECL_PRIM(modules_testcapi,0,testcapi_module);
 
   u8_register_source_file(_FILEINFO);
 
