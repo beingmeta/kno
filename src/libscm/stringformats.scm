514d8ffa72906eea719d3cf1f1e5459131a58cba
Ken Haase <kh@beingmeta.com>
2019-07-01 20:13:05 -0400

    Add stringformats

---
+++ src/libscm/stringformats.scm
@@ -0,0 +1,8 @@
+;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
+;;; Copyright (C) 2005-2013 beingmeta, inc.  All rights reserved.
+
+(in-module 'stringformats)
+
+;;; Generation of strings from various other kinds of values
+
+(export-alias! 'stringfmts)
