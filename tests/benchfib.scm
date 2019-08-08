b8f31acc48a864a7142be8717898c7b382153dd9
Ken Haase <kh@beingmeta.com>
2019-08-08 19:35:54 -0400

    Add more flexibility to benchfib, including command line arguments

--- tests/benchfib.scm
+++ tests/benchfib.scm
@@ -1,21 +1,27 @@
 ;;; -*- Mode: Scheme; -*-
 
 (use-module '{bench/miscfns optimize stringfmts})
+(config! 'optimize:keepsource #f)
+
 (optimize! 'bench/miscfns)
 
-(define (fibtest n cycles)
+(define (fibtest (n 50) (cycles 5000) . more)
   (let ((start (elapsed-time))
 	(runtime #f))
     (dotimes (i cycles) (fibi n))
     (set! runtime (elapsed-time start))
-    (message "(fibi " n ") x " cycles " in " (secs->string runtime #f))))
+    (message "(fibi " n ") x " cycles " in " (secs->string runtime #f))
+    (while (and (pair? more)  (pair? (cdr more)))
+      (let ((n (car more)) (cycles (cadr more)))
+	(set! more (cddr more))
+	(set! start (elapsed-time))
+	(dotimes (i cycles) (fibi n))
+	(set! runtime (elapsed-time start))
+	(message "(fibi " n ") x " cycles " in " (secs->string runtime #f))
+	))))
 
 (optimize!)
 
-(define (main)
-  (fibtest 10 5000)
-  (fibtest 100 5000)
-  (fibtest 200 5000)
-  (fibtest 500 5000))
+(define main fibtest)
 
 
