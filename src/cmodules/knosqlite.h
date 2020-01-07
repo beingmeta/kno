e22a4441f54d5cdc1498d8ec081385419ca5e5e3
Ken Haase <kh@beingmeta.com>
2020-01-07 19:38:17 +0000

    Remove left over knosqlite.h

--- src/cmodules/knosqlite.h
+++
@@ -1,8 +0,0 @@
-#include <sqlite.h>
-
-typedef struct KNO_SQLITE {
-  KNO_CONS_HEADER; sqlite *db;} KNO_SQLITE;
-typedef struct KNO_SQLITE *kno_sqlite;
-
-KNO_EXPORT kno_lisp_type kno_sqlite_type;
-
