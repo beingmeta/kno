78dcff15b720de772fcad3ad730e9fc86d79b542
Ken Haase <kh@beingmeta.com>
2020-06-09 11:50:54 -0400

    Include varconfig for config:boolean

--- src/libscm/kno/autosession.scm
+++ src/libscm/kno/autosession.scm
@@ -2,11 +2,11 @@
 
 (in-module 'kno/autosession)
 
-(use-module '{kno/sessions})
+(use-module '{kno/sessions varconfig})
 
 ;;;; This loads a session defined in the current directory
 
-(unless (or *session* (config 'quick) (config 'session)
+(unless (or *session* (config 'quick)
 	    (not (config 'autosession #t config:boolean)))
   (cond ((file-directory? (abspath ".knoc"))
 	 (config! 'session (abspath ".knoc")))
