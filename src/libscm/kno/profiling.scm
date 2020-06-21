c31d6513887a183a3413bba26750dfaca91773b6
Ken Haase <kh@beingmeta.com>
2020-06-21 16:37:49 -0400

    Add profile/report and other enhancements to kno/profiling
    Add explicit fcns arg to profile/report

--- src/libscm/kno/profiling.scm
+++ src/libscm/kno/profiling.scm
@@ -2,17 +2,23 @@
 
 (in-module 'kno/profiling)
 
-(use-module '{logger reflection})
+(use-module '{logger reflection stringfmts varconfig})
 
 (define %loglevel %notice%)
 
-(module-export! '{profile-module! module-getprocs module-getcalls})
+(module-export! '{profile!
+		  profile-module!
+		  module-getprocs module-getcalls
+		  profile/report})
+
+(define-init profile-root #f)
+(varconfig! profile:root profile-root)
 
 (define (module-getprocs module)
   (for-choices (sym (module-bindings (get-module module)))
     (pick (importvar module sym) applicable?)))
 
-(define sortfns
+(define profilefns
   [nsecs profile/nsecs
    time profile/nsecs
    stime profile/stime
@@ -29,12 +35,51 @@
   (if sort
       (rsorted (module-getcalls module #f)
 	       (try (tryif (applicable? sort) sort)
-		    (get sortfns sort)
+		    (get profilefns sort)
 		    (irritant sort |BadCallSortFn|)))
       (reject (profile/getcalls (module-getprocs module)) profile/ncalls 0)))
 
 (define (profile-module! module)
   (let ((procs (module-getprocs module)))
     (config! 'profiled procs)
+    (lognotice |Profile| "Profiling " ($count (|| procs) "procedure") " in " module)
     (|| procs)))
 
+(defambda (profile/report (root profile-root) (fcns (config 'profiled))
+			  (profilefn profile/time) . morefns)
+  (when (and (symbol? profilefn) (test profilefns profilefn))
+    (set! profilefn (get profilefns profilefn)))
+  (when (and root (not (profile/getcalls root)))
+    (logwarn |Profiling|
+      "The " (if (eq? root profile-root) "default ") "root " root
+      " was not profiled! Ignoring it.")
+    (set! root #f))
+  (let* ((ranks (make-hashtable))
+	 (rootinfo (and root (profile/getcalls root)))
+	 (rootval (and rootinfo (profilefn rootinfo)))
+	 (rootvals and (pair? morefns) #[]))
+    (when rootvals 
+      (dolist (fn morefns)
+	(if (and (symbol? fn) (get profilefns fn))
+	    (store! rootvals fn ((get profilefns fn) rootinfo))
+	    (store! rootvals fn (fn rootinfo)))))
+    (do-choices (fcn (config 'profiled))
+      (let* ((info (profile/getcalls fcn))
+	     (val (profilefn info)))
+	(unless (or (fail? val) (not (number? val)) (zero? val))
+	  (store! ranks fcn val))))
+    (lineout "================================================================")
+    (doseq (fcn (rsorted (getkeys ranks) ranks))
+      (let ((info (profile/getcalls fcn)))
+	(if rootval
+	    (lineout (show% (profilefn info) rootval) 
+	      "\t" (procedure-name fcn) " ("  (profilefn info) ")"
+	      (dolist (fn morefns)
+		(when (test rootvals fn)
+		  (printout "\t" (procedure-name fn) "=" (show% (fn info) (get rootvals fn)) 
+		    " (" (fn info) ")"))))
+	    (lineout (profilefn info) "\t" (procedure-name fcn)
+	      (dolist (fn morefns) (printout "\t" (fn info)))))))))
+
+(define (profile! fcn)
+  (config! 'profiled fcn))
