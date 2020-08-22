7d548aa820a77706ad28b441f843e85fb4862a07
Ken Haase <kh@beingmeta.com>
2020-08-22 09:07:47 -0400

    Add extmods .dir-local, new tests

---
+++ src/extmods/.dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((indent-tabs-mode . t) (fill-column . 100)))
+ (c-mode . ((c-file-style . "gnu")
+	    (fill-column . 100)
+	    (indent-tabs-mode . t)
+	    (compile-command . "make -C ../.. debugging;"))))
