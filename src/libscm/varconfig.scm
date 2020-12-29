0dc22e4398c221ad2fa2307d319b940b9c95a070
Ken Haase <kh@beingmeta.com>
2020-12-29 20:46:06 +0000

    Update some boilerplate

--- src/libscm/varconfig.scm
+++ src/libscm/varconfig.scm
@@ -1,5 +1,5 @@
 ;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
-;;; Copyright (C) 2005-2013 beingmeta, inc.  All rights reserved.
+;;; Copyright (C) 2005-2020 beingmeta, inc.  All rights reserved.
 
 ;;; DON'T EDIT THIS FILE !!!
 ;;;
