3c8b1562457494aa86038b9b0953e42bef42a274
Ken Haase <kh@beingmeta.com>
2021-02-05 17:39:47 -0500

    Try to be more atomic in writing out state files

--- src/libscm/io/filestream.scm
+++ src/libscm/io/filestream.scm
@@ -32,6 +32,15 @@
   (difftime (filestream/batch-end batch) (filestream/batch-start batch)))
 (define (batch-count batch) (filestream/batch-itemcount batch))
 
+(define (inner-write-state-file file state)
+  (let ((temp-file (glom file ".part")))
+    (fileout (open-output-file temp-file "w*")
+      (void (printout state)))
+    (move-file temp-file file)))
+(define-init write-state-file
+  (defsync (write-state-file file state)
+    (inner-write-state-file file state)))
+
 (define (filestream/open filename (opts #f))
   (let* ((statefile (glom filename ".state"))
 	 (archive (and (has-suffix filename {".bz" ".gz" ".bz2" ".Z" ".tar"})
@@ -67,7 +76,7 @@
 	       ;;  items which have been read in the past
 	       (dotimes (i (getopt state 'itemcount 0)) (readfn port)))))
       (store! state 'updated (timestamp))
-      (with-lock (filestream-lock stream) (fileout statefile (void (pprint state))))
+      (with-lock (filestream-lock stream) (write-state-file statefile state))
       stream)))
 
 (define (filestream/read stream (extra #f))
@@ -105,7 +114,7 @@
       (when (filestream-filepos stream)
 	(store! state 'filepos (filestream-filepos stream)))
       (add! state 'batches batch)
-      (fileout (filestream-statefile stream) (void (pprint state))))))
+      (write-state-file (filestream-statefile stream) state))))
 
 (define (filestream/log! stream (opts #f) (%loglevel %notice%))
   (let* ((count (filestream-itemcount stream))
