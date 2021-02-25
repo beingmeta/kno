ec6366e375206fbb075aa76db887e1827b58f05c
Ken Haase <kh@beingmeta.com>
2021-02-25 03:17:43 +0000

    Add new copyright notices, etc

--- src/libscm/knodb/adjuncts.scm
+++ src/libscm/knodb/adjuncts.scm
@@ -1,6 +1,6 @@
 ;;; -*- Mode: Scheme; Character-encoding: utf-8; -*-
 ;;; Copyright (C) 2005-2020 beingmeta, inc.  All rights reserved.
-Copyright (C) 2020-2021 beingmeta, LLC
+;;; Copyright (C) 2020-2021 beingmeta, LLC
 
 (in-module 'knodb/adjuncts)
 
